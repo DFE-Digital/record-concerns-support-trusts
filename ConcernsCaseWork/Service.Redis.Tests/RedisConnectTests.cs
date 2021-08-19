b2041f262dea31c1cb81fefd2add71034bf90550
PauloLancao <paulolancao@hotmail.com>
2021-08-19 08:39:51 +0100

    Testing redis dev env

--- ConcernsCaseWork/Service.Redis.Tests/RedisConnectTests.cs
+++ ConcernsCaseWork/Service.Redis.Tests/RedisConnectTests.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using StackExchange.Redis;
 
 namespace Service.Redis.Tests
 {
@@ -13,23 +12,7 @@ namespace Service.Redis.Tests
 		[Test]
 		public void RedisConnectSuccessTest()
 		{
-			/*var server = "master.cf-a7ls4an6embbm.b5xrdh.euw2.cache.amazonaws.com";
-			var port = "6379";
-			
-			ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
-				new ConfigurationOptions {
-					Password = "iIssk9ZxDWIsiHifgMCoyZZzByblP4Mj",
-					EndPoints = { $"{server}:{port}" },
-					Ssl = false
-				});
-			
-			var cache = redis.GetDatabase();
 
-			string expectedStringData = "Hello world";
-			cache.SetAdd("key003", System.Text.Encoding.UTF8.GetBytes(expectedStringData));
-			var dataFromCache = cache.StringGet("key003");
-			var actualCachedStringData = System.Text.Encoding.UTF8.GetString(dataFromCache);
-			Assert.AreEqual(expectedStringData, actualCachedStringData);*/
 		}
 	}
 }
