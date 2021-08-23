f9474ddc7f00e44a150e3a0aa53bf0d85c668b5f
PauloLancao <paulolancao@hotmail.com>
2021-08-23 12:18:12 +0100

    Add unit tests to cache provider moqs

--- ConcernsCaseWork/Service.Redis/Services/ActiveDirectoryService.cs
+++ ConcernsCaseWork/Service.Redis/Services/ActiveDirectoryService.cs
@@ -17,7 +17,7 @@ namespace Service.Redis.Services
 		/// <returns></returns>
 		public Task<UserClaims> GetUserAsync(UserCredentials userCredentials)
 		{
-			return Task.FromResult(new UserClaims { Email = "paulo.lancao@education.gov.uk", Id = "User:AD:ID" });
+			return Task.FromResult(new UserClaims { Email = userCredentials.Email, Id = userCredentials.Password });
 		}
 	}
 }
