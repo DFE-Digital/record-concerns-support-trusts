bc259a16863f18c5e663960f84898fce454ec5a0
PauloLancao <paulolancao@hotmail.com>
2021-08-24 18:03:10 +0100

    Add trust cache to project

--- ConcernsCaseWork/Service.Redis.Tests/Trusts/TrustCachedServiceTests.cs
+++ ConcernsCaseWork/Service.Redis.Tests/Trusts/TrustCachedServiceTests.cs
@@ -8,6 +8,7 @@ using Service.TRAMS.Models;
 using Service.TRAMS.Trusts;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Service.Redis.Tests.Trusts
@@ -16,7 +17,7 @@ namespace Service.Redis.Tests.Trusts
 	public class TrustCachedServiceTests
 	{
 		[Test]
-		public async Task WhenGetTrustsCached_ReturnsTrustFromTrams()
+		public async Task WhenGetTrustsCached_ReturnsTrustsFromTrams()
 		{
 			// arrange
 			var mockTrustService = new Mock<ITrustService>();
@@ -40,6 +41,51 @@ namespace Service.Redis.Tests.Trusts
 			Assert.That(trusts, Is.Not.Null);
 			Assert.That(trusts.Count, Is.EqualTo(expectedTrusts.Count));
 
+			foreach (var trust in trusts)
+			{
+				foreach (var expectedTrust in expectedTrusts)
+				{
+					Assert.That(trust.Establishments.Count, Is.EqualTo(expectedTrust.Establishments.Count));
+					Assert.That(trust.Urn, Is.EqualTo(expectedTrust.Urn));
+					Assert.That(trust.GroupName, Is.EqualTo(expectedTrust.GroupName));
+					Assert.That(trust.UkPrn, Is.EqualTo(expectedTrust.UkPrn));
+					Assert.That(trust.CompaniesHouseNumber, Is.EqualTo(expectedTrust.CompaniesHouseNumber));
+
+					foreach (var establishment in trust.Establishments)
+					{
+						foreach (var expectedEstablishment in expectedTrust.Establishments)
+						{
+							Assert.That(establishment.Name, Is.EqualTo(expectedEstablishment.Name));
+							Assert.That(establishment.Urn, Is.EqualTo(expectedEstablishment.Urn));
+							Assert.That(establishment.UkPrn, Is.EqualTo(expectedEstablishment.UkPrn));
+						}
+					}
+				}
+			}
+		}
+
+		[Test]
+		public async Task WhenGetTrustsCached_ReturnsTrustsFromCache()
+		{
+			// arrange
+			var mockTrustService = new Mock<ITrustService>();
+			var mockCacheProvider = new Mock<ICacheProvider>();
+			var mockLogger = new Mock<ILogger<TrustCachedService>>();
+	
+			var expectedTrusts = TrustDtoFactory.CreateListTrustDto();
+
+			mockCacheProvider.Setup(c => c.GetFromCache<List<TrustDto>>(It.IsAny<string>()))
+				.Returns(Task.FromResult(expectedTrusts.ToList()));
+			
+			var trustCachedService = new TrustCachedService(mockTrustService.Object, mockCacheProvider.Object, mockLogger.Object);
+			
+			// act
+			var trusts = await trustCachedService.GetTrustsCached();
+			
+			// assert
+			Assert.That(trusts, Is.Not.Null);
+			Assert.That(trusts.Count, Is.EqualTo(expectedTrusts.Count));
+			
 			foreach (var trust in trusts)
 			{
 				foreach (var expectedTrust in expectedTrusts)
