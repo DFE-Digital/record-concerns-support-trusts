6e84ea71596cd5566c869a83a778bee9d5372667
PauloLancao <paulolancao@hotmail.com>
2021-08-27 09:42:09 +0100

    Move back button to main layout to render on the top page and add exclusions on pages that shouldn't show.

---
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/BackModelTests.cs
@@ -0,0 +1,28 @@
+﻿using ConcernsCaseWork.Pages.Partials;
+using NUnit.Framework;
+
+namespace ConcernsCaseWork.Tests.Pages
+{
+	[Parallelizable(ParallelScope.All)]
+	public class BackModelTests
+	{
+		[TestCase("", false)]
+		[TestCase("/", false)]
+		[TestCase("login", false)]
+		[TestCase("/login", false)]
+		[TestCase("logout", false)]
+		[TestCase("/logout", false)]
+		[TestCase("case", true)]
+		[TestCase("/case", true)]
+		[TestCase("home", false)]
+		[TestCase("/home", false)]
+		public void WhenCanRender_ReturnsExpected(string requestPath, bool expected)
+		{
+			// act
+			var canRender = BackModel.CanRender(requestPath);
+
+			// assert
+			Assert.That(canRender, Is.EqualTo(expected));
+		}
+	}
+}
--- ConcernsCaseWork/ConcernsCaseWork/Pages/Partials/Back.cshtml.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Pages/Partials/Back.cshtml.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConcernsCaseWork.Pages.Partials
 {
@@ -8,5 +10,15 @@ namespace ConcernsCaseWork.Pages.Partials
 		/// Store here all the go back options
 		/// </summary>
 		public const string BrowserBackPage = "javascript: history.back()";
+		
+		/// <summary>
+		/// Don't render component for this paths
+		/// </summary>
+		private static readonly IList<string> PathExclusions = new List<string> { "home", "login", "logout" };
+
+		public static bool CanRender(string requestPath)
+		{
+			return !string.IsNullOrEmpty(requestPath) && !requestPath.Equals("/") && PathExclusions.All(exclude => !requestPath.Contains(exclude));
+		}
 	}
 }
