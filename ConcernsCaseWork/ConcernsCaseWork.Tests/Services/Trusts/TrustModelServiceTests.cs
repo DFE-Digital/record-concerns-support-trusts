ae343b43d0a4ab53f813d0d58377164e3deb2100
PauloLancao <paulolancao@hotmail.com>
2021-08-31 13:52:34 +0100

    Add unit tests to TrustModelService

--- ConcernsCaseWork/ConcernsCaseWork.Tests/Services/Trusts/TrustModelServiceTests.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Services/Trusts/TrustModelServiceTests.cs
@@ -1,4 +1,16 @@
-﻿using NUnit.Framework;
+﻿using AutoMapper;
+using ConcernsCaseWork.Mappers;
+using ConcernsCaseWork.Models;
+using ConcernsCaseWork.Services.Trusts;
+using ConcernsCaseWork.Shared.Tests.Factory;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using Service.TRAMS.Models;
+using Service.TRAMS.Trusts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ConcernsCaseWork.Tests.Services.Trusts
@@ -10,17 +22,63 @@ namespace ConcernsCaseWork.Tests.Services.Trusts
 		public async Task WhenGetTrustsBySearchCriteria_ReturnsTrusts()
 		{
 			// arrange
-			
-			
-			
+			var mockTrustService = new Mock<ITrustSearchService>();
+			var mockLogger = new Mock<ILogger<TrustModelService>>();
+			var config = new MapperConfiguration(cfg => cfg.AddProfile<AutoMapping>());
+			var mapper = config.CreateMapper();
+			var trustSummaryDto = TrustDtoFactory.CreateListTrustSummaryDto();
+
+			mockTrustService.Setup(ts => ts.GetTrustsBySearchCriteria(It.IsAny<TrustSearch>())).ReturnsAsync(trustSummaryDto);
+
 			// act
-			
-			
-			
+			var trustModelService = new TrustModelService(mockTrustService.Object, mapper, mockLogger.Object);
+			var trustsSummaryModel = await trustModelService.GetTrustsBySearchCriteria(It.IsAny<TrustSearch>());
+
 			// assert
-			
-			
+			Assert.IsAssignableFrom<List<TrustSummaryModel>>(trustsSummaryModel);
+			Assert.That(trustsSummaryModel.Count, Is.EqualTo(trustSummaryDto.Count));
+			foreach (var expected in trustsSummaryModel)
+			{
+				foreach (var actual in trustSummaryDto.Where(actual => expected.UkPrn.Equals(actual.UkPrn)))
+				{
+					Assert.That(expected.Establishments.Count, Is.EqualTo(actual.Establishments.Count));
+					Assert.That(expected.Urn, Is.EqualTo(actual.Urn));
+					Assert.That(expected.UkPrn, Is.EqualTo(actual.UkPrn));
+					Assert.That(expected.GroupName, Is.EqualTo(actual.GroupName));
+					Assert.That(expected.CompaniesHouseNumber, Is.EqualTo(actual.CompaniesHouseNumber));
+					Assert.That(expected.DisplayName, Is.EqualTo(actual.GroupName + "," + actual.Urn + "," + actual.CompaniesHouseNumber));
+					
+					foreach (var establishment in actual.Establishments)
+					{
+						foreach (var expectedEstablishment in expected.Establishments)
+						{
+							Assert.That(establishment.Name, Is.EqualTo(expectedEstablishment.Name));
+							Assert.That(establishment.Urn, Is.EqualTo(expectedEstablishment.Urn));
+							Assert.That(establishment.UkPrn, Is.EqualTo(expectedEstablishment.UkPrn));
+						}
+					}
+				}
+			}
 		}
 
+		[Test]
+		public async Task WhenGetTrustsBySearchCriteria_ThrowsException_ReturnEmptyTrusts()
+		{
+			// arrange
+			var mockTrustService = new Mock<ITrustSearchService>();
+			var mockLogger = new Mock<ILogger<TrustModelService>>();
+			var config = new MapperConfiguration(cfg => cfg.AddProfile<AutoMapping>());
+			var mapper = config.CreateMapper();
+
+			mockTrustService.Setup(ts => ts.GetTrustsBySearchCriteria(It.IsAny<TrustSearch>())).ReturnsAsync(Array.Empty<TrustSummaryDto>());
+
+			// act
+			var trustModelService = new TrustModelService(mockTrustService.Object, mapper, mockLogger.Object);
+			var trustsSummaryModel = await trustModelService.GetTrustsBySearchCriteria(It.IsAny<TrustSearch>());
+
+			// assert
+			Assert.IsAssignableFrom<List<TrustSummaryModel>>(trustsSummaryModel.ToList());
+			Assert.That(trustsSummaryModel.Count, Is.EqualTo(0));
+		}
 	}
 }
