89577c346b7e04a4eb6a4e9606cb9050c253c90a
PauloLancao <paulolancao@hotmail.com>
2021-09-17 10:39:33 +0100

    Release

---
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Mappers/HomeMappingTests.cs
@@ -0,0 +1,36 @@
+﻿using ConcernsCaseWork.Mappers;
+using ConcernsCaseWork.Models;
+using ConcernsCaseWork.Shared.Tests.Factory;
+using NUnit.Framework;
+using Service.TRAMS.Status;
+using System.Collections.Generic;
+
+namespace ConcernsCaseWork.Tests.Mappers
+{
+	[Parallelizable(ParallelScope.All)]
+	public class HomeMappingTests
+	{
+		[Test]
+		public void WhenMapCases_To_HomeModels()
+		{
+			// arrange
+			var casesDto = CaseFactory.BuildListCaseDto();
+			var trustsDetailsDto = TrustFactory.BuildListTrustDetailsDto();
+			var recordsDto = RecordFactory.BuildListRecordDto();
+			var ratingsDto = RatingFactory.BuildListRatingDto();
+			var typesDto = TypeFactory.BuildListTypeDto();
+			var statusLiveDto = StatusFactory.BuildStatusDto(Status.Live.ToString(), 1);
+			var statusMonitoringDto = StatusFactory.BuildStatusDto(Status.Monitoring.ToString(), 2);
+
+			// act
+			(IList<HomeModel> activeCases, IList<HomeModel> monitoringCases) = HomeMapping.Map(casesDto, trustsDetailsDto,
+				recordsDto, ratingsDto, typesDto, statusLiveDto, statusMonitoringDto);
+			
+			// assert
+			Assert.That(activeCases, Is.Not.Null);
+			Assert.That(monitoringCases, Is.Not.Null);
+			Assert.That(activeCases.Count, Is.EqualTo(2));
+			Assert.That(monitoringCases.Count, Is.EqualTo(2));
+		}
+	}
+}
