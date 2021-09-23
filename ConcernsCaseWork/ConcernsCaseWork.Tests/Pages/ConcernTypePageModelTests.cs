86fb58835e76e8664f0976fa6dbb9bfda1b4bd69
PauloLancao <paulolancao@hotmail.com>
2021-09-23 16:51:21 +0100

    rename some properties on case details page

--- ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/ConcernTypePageModelTests.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/ConcernTypePageModelTests.cs
@@ -1,4 +1,20 @@
-﻿using NUnit.Framework;
+﻿using ConcernsCaseWork.Pages.Case;
+using ConcernsCaseWork.Services.Trusts;
+using ConcernsCaseWork.Services.Type;
+using ConcernsCaseWork.Shared.Tests.Factory;
+using ConcernsCaseWork.Shared.Tests.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using Service.Redis.Base;
+using Service.Redis.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ConcernsCaseWork.Tests.Pages
 {
@@ -9,42 +25,44 @@ namespace ConcernsCaseWork.Tests.Pages
 		public async Task WhenOnGetAsync_ReturnsModel()
 		{
 			// arrange
-			var mockCaseModelService = new Mock<ICaseModelService>();
-			var mockLogger = new Mock<ILogger<DetailsPageModel>>();
+			var mockLogger = new Mock<ILogger<ConcernTypePageModel>>();
 			var mockTrustModelService = new Mock<ITrustModelService>();
-			var mockCasesCachedService = new Mock<ICachedService>();
+			var mockCachedService = new Mock<ICachedService>();
 			var mockTypeModelService = new Mock<ITypeModelService>();
 			var expected = TrustFactory.BuildTrustDetailsModel();
 
 			mockTypeModelService.Setup(t => t.GetTypes()).ReturnsAsync(new Dictionary<string, IList<string>>());
-			mockCasesCachedService.Setup(c => c.GetData<UserState>(It.IsAny<string>())).ReturnsAsync(new UserState { TrustUkPrn = "trust-ukprn" });
+			mockCachedService.Setup(c => c.GetData<UserState>(It.IsAny<string>())).ReturnsAsync(new UserState { TrustUkPrn = "trust-ukprn" });
 			mockTrustModelService.Setup(s => s.GetTrustByUkPrn(It.IsAny<string>())).ReturnsAsync(expected);
 			
-			var pageModel = SetupDetailsModel(mockCaseModelService.Object, mockCasesCachedService.Object, mockLogger.Object, true);
+			var pageModel = SetupConcernTypePageModel(mockTrustModelService.Object, mockCachedService.Object, 
+				mockTypeModelService.Object, mockLogger.Object, true);
 			
 			// act
 			await pageModel.OnGetAsync();
-			var createCaseModel = pageModel.CreateCaseModel;
+			var trustDetailsModel = pageModel.TrustDetailsModel;
+			var typesDictionary = pageModel.TypesDictionary;
 
 			// assert
 			Assert.That(pageModel.TempData["Error.Message"], Is.Null);
-			Assert.IsAssignableFrom<CreateCaseModel>(createCaseModel);
+			Assert.IsAssignableFrom<CreateCaseModel>(trustDetailsModel);
+			Assert.IsAssignableFrom<Dictionary<string, IList<string>>>(typesDictionary);
 
 			Assert.That(typesDictionary, Is.Not.Null);
-			Assert.That(createCaseModel, Is.Not.Null);
-			Assert.That(createCaseModel.GiasData, Is.Not.Null);
-			Assert.That(createCaseModel.GiasData.GroupId, Is.EqualTo(expected.GiasData.GroupId));
-			Assert.That(createCaseModel.GiasData.GroupName, Is.EqualTo(expected.GiasData.GroupName));
-			Assert.That(createCaseModel.GiasData.UkPrn, Is.EqualTo(expected.GiasData.UkPrn));
-			Assert.That(createCaseModel.GiasData.CompaniesHouseNumber, Is.EqualTo(expected.GiasData.CompaniesHouseNumber));
-			Assert.That(createCaseModel.GiasData.GroupContactAddress, Is.Not.Null);
-			Assert.That(createCaseModel.GiasData.GroupContactAddress.County, Is.EqualTo(expected.GiasData.GroupContactAddress.County));
-			Assert.That(createCaseModel.GiasData.GroupContactAddress.Locality, Is.EqualTo(expected.GiasData.GroupContactAddress.Locality));
-			Assert.That(createCaseModel.GiasData.GroupContactAddress.Postcode, Is.EqualTo(expected.GiasData.GroupContactAddress.Postcode));
-			Assert.That(createCaseModel.GiasData.GroupContactAddress.Street, Is.EqualTo(expected.GiasData.GroupContactAddress.Street));
-			Assert.That(createCaseModel.GiasData.GroupContactAddress.Town, Is.EqualTo(expected.GiasData.GroupContactAddress.Town));
-			Assert.That(createCaseModel.GiasData.GroupContactAddress.AdditionalLine, Is.EqualTo(expected.GiasData.GroupContactAddress.AdditionalLine));
-			Assert.That(createCaseModel.GiasData.GroupContactAddress.DisplayAddress, Is.EqualTo(SharedBuilder.BuildDisplayAddress(expected.GiasData.GroupContactAddress)));
+			Assert.That(trustDetailsModel, Is.Not.Null);
+			Assert.That(trustDetailsModel.GiasData, Is.Not.Null);
+			Assert.That(trustDetailsModel.GiasData.GroupId, Is.EqualTo(expected.GiasData.GroupId));
+			Assert.That(trustDetailsModel.GiasData.GroupName, Is.EqualTo(expected.GiasData.GroupName));
+			Assert.That(trustDetailsModel.GiasData.UkPrn, Is.EqualTo(expected.GiasData.UkPrn));
+			Assert.That(trustDetailsModel.GiasData.CompaniesHouseNumber, Is.EqualTo(expected.GiasData.CompaniesHouseNumber));
+			Assert.That(trustDetailsModel.GiasData.GroupContactAddress, Is.Not.Null);
+			Assert.That(trustDetailsModel.GiasData.GroupContactAddress.County, Is.EqualTo(expected.GiasData.GroupContactAddress.County));
+			Assert.That(trustDetailsModel.GiasData.GroupContactAddress.Locality, Is.EqualTo(expected.GiasData.GroupContactAddress.Locality));
+			Assert.That(trustDetailsModel.GiasData.GroupContactAddress.Postcode, Is.EqualTo(expected.GiasData.GroupContactAddress.Postcode));
+			Assert.That(trustDetailsModel.GiasData.GroupContactAddress.Street, Is.EqualTo(expected.GiasData.GroupContactAddress.Street));
+			Assert.That(trustDetailsModel.GiasData.GroupContactAddress.Town, Is.EqualTo(expected.GiasData.GroupContactAddress.Town));
+			Assert.That(trustDetailsModel.GiasData.GroupContactAddress.AdditionalLine, Is.EqualTo(expected.GiasData.GroupContactAddress.AdditionalLine));
+			Assert.That(trustDetailsModel.GiasData.GroupContactAddress.DisplayAddress, Is.EqualTo(SharedBuilder.BuildDisplayAddress(expected.GiasData.GroupContactAddress)));
 			
 			// Verify ILogger
 			mockLogger.Verify(
@@ -57,13 +75,13 @@ namespace ConcernsCaseWork.Tests.Pages
 				Times.Once);
 		}
 		
-		private static DetailsPageModel SetupDetailsModel(
-			ICaseModelService mockCaseModelService, ICachedService mockCachedService, 
-			ILogger<DetailsPageModel> mockLogger, bool isAuthenticated = false)
+		private static ConcernTypePageModel SetupConcernTypePageModel(
+			ITrustModelService mockTrustModelService, ICachedService mockCachedService, ITypeModelService mockTypeModelService, 
+			ILogger<ConcernTypePageModel> mockLogger, bool isAuthenticated = false)
 		{
 			(PageContext pageContext, TempDataDictionary tempData, ActionContext actionContext) = PageContextFactory.PageContextBuilder(isAuthenticated);
 			
-			return new DetailsPageModel(mockCaseModelService, mockCachedService, mockLogger)
+			return new ConcernTypePageModel(mockTrustModelService, mockCachedService, mockTypeModelService, mockLogger)
 			{
 				PageContext = pageContext,
 				TempData = tempData,
