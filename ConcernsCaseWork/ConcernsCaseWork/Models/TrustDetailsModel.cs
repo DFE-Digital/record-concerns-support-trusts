c3bc6795281e1df27963851648b879fdf7e1e26b
Elijah Aremu <Elijah.AREMU@EDUCATION.GOV.UK>
2021-10-19 15:46:09 +0100

    Add Total pupils, Total pupil capacity and Pupil capacity percentage to trust overview model

--- ConcernsCaseWork/ConcernsCaseWork/Models/TrustDetailsModel.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Models/TrustDetailsModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ConcernsCaseWork.Models
 {
@@ -9,8 +10,48 @@ namespace ConcernsCaseWork.Models
 	{
 		public GiasDataModel GiasData { get; }
 		
-		public List<EstablishmentModel> Establishments { get; } 
-		
+		public List<EstablishmentModel> Establishments { get; }
+
+
+		public double TotalPupilCapacity { get { return CalculateTotalPupilCapacity(); } }
+
+		public double TotalPupils { get { return CalculateTotalPupils(); } }
+
+		public double PupilCapacityPercentage { get { return CalculatePupilCapacityPercentage(); } }
+
+
+		private int CalculateTotalPupilCapacity()
+		{
+			int totalCapacity = 0;
+
+			this.Establishments.ForEach(establishment =>
+			{
+				totalCapacity += Int32.Parse(establishment.SchoolCapacity);
+
+			});
+
+			return totalCapacity;
+		}
+
+		private int CalculateTotalPupils()
+		{
+			int totalPupils = 0;
+
+			this.Establishments.ForEach(establishment =>
+			{
+				totalPupils += Int32.Parse(establishment.Census.NumberOfPupils);
+
+			});
+
+			return totalPupils;
+		}
+
+		private double CalculatePupilCapacityPercentage()
+		{
+			return Math.Round((this.TotalPupils / this.TotalPupilCapacity), 2) * 100;
+		}
+
+
 		public TrustDetailsModel(GiasDataModel giasData, List<EstablishmentModel> establishments) => 
 			(GiasData, Establishments) = (giasData, establishments);
 	}
