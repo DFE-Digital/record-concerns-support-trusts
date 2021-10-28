dfc613399b00417b3474e0a73d44ec025ebf41df
PauloLancao <paulolancao@hotmail.com>
2021-10-28 12:33:20 +0100

    Add coverage to missing logic on cases service model

---
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Models/TrustCasesModelTests.cs
@@ -0,0 +1,41 @@
+﻿using ConcernsCaseWork.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace ConcernsCaseWork.Tests.Models
+{
+	[Parallelizable(ParallelScope.All)]
+	public class TrustCasesModelTests
+	{
+		[TestCase("live", "Open")]
+		[TestCase("close", "Closed")]
+		public void WhenBuildTrustCasesModel_ReturnsValidLogic(string actualStatus, string expectedStatus)
+		{
+			// arrange
+			var createdAtDate = DateTimeOffset.Now;
+			var closeDate = DateTimeOffset.Now;
+			
+			var trustCaseModel = new TrustCasesModel(
+				1,
+				"case-type",
+				"case-sub-type",
+				new Tuple<int, IList<string>>(1, new List<string> { "red" }),
+				new List<string>{ "red" },
+				createdAtDate, 
+				closeDate, 
+				actualStatus
+			);
+			
+			// assert
+			Assert.That(trustCaseModel.CaseUrn, Is.EqualTo(1));
+			Assert.That(trustCaseModel.Closed, Is.EqualTo(closeDate.ToString("dd-MM-yyyy")));
+			Assert.That(trustCaseModel.RagRating, Is.Not.Null);
+			Assert.That(trustCaseModel.RagRating.Item1, Is.EqualTo(1));
+			Assert.That(trustCaseModel.RagRating.Item2, Is.EqualTo(new List<string> { "red" }));
+			Assert.That(trustCaseModel.StatusDescription, Is.EqualTo(expectedStatus));
+			Assert.That(trustCaseModel.CaseTypeDescription, Is.EqualTo("case-type: case-sub-type"));
+			Assert.That(trustCaseModel.RagRatingCss, Is.EqualTo(new List<string>{ "red" }));
+		}
+	}
+}
