6d8d785e76a6a027365dc285b26b5bf9ae58fa5e
PauloLancao <paulolancao@hotmail.com>
2021-11-15 10:43:24 +0000

    Add unit tests to case history service

--- ConcernsCaseWork/Service.TRAMS.Tests/Cases/CaseHistoryServiceTests.cs
+++ ConcernsCaseWork/Service.TRAMS.Tests/Cases/CaseHistoryServiceTests.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 using Service.TRAMS.Base;
 using Service.TRAMS.Cases;
 using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text.Json;
@@ -113,6 +114,83 @@ namespace Service.TRAMS.Tests.Cases
 			
 			// act
 			Assert.ThrowsAsync<Exception>(() => caseHistoryService.PostCaseHistory(CaseFactory.BuildCreateCaseHistoryDto()));
+		}
+		
+		[Test]
+		public async Task WhenGetCasesHistory_ReturnsCasesHistory()
+		{
+			// arrange
+			var expectedCasesHistory = CaseFactory.BuildListCasesHistoryDto();
+			var expectedCasesHistoryWrap = new ApiListWrapper<CaseHistoryDto>(expectedCasesHistory, null);
+			
+			var configuration = new ConfigurationBuilder().ConfigurationUserSecretsBuilder().Build();
+			var tramsApiEndpoint = configuration["trams:api_endpoint"];
+			
+			var logger = new Mock<ILogger<CaseHistoryService>>();
+			var httpClientFactory = new Mock<IHttpClientFactory>();
+			var mockMessageHandler = new Mock<HttpMessageHandler>();
+			mockMessageHandler.Protected()
+				.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+				.ReturnsAsync(new HttpResponseMessage
+				{
+					StatusCode = HttpStatusCode.OK,
+					Content = new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(expectedCasesHistoryWrap))
+				});
+
+			var httpClient = new HttpClient(mockMessageHandler.Object);
+			httpClient.BaseAddress = new Uri(tramsApiEndpoint);
+			httpClientFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+			
+			var caseHistoryService = new CaseHistoryService(httpClientFactory.Object, logger.Object);
+			
+			// act
+			var actualListWrapperCasesHistory = await caseHistoryService.GetCasesHistory(CaseFactory.BuildCaseSearch());
+
+			// assert
+			Assert.That(actualListWrapperCasesHistory, Is.Not.Null);
+			Assert.That(actualListWrapperCasesHistory.Data, Is.Not.Null);
+			Assert.That(actualListWrapperCasesHistory.Paging, Is.Null);
+			Assert.That(actualListWrapperCasesHistory.Data.Count, Is.EqualTo(expectedCasesHistoryWrap.Data.Count));
+			
+			foreach (var caseHistoryDto in actualListWrapperCasesHistory.Data)
+			{
+				foreach (var expectedCaseHistory in expectedCasesHistoryWrap.Data.Where(expectedCase => caseHistoryDto.CaseUrn == expectedCase.CaseUrn))
+				{
+					Assert.That(caseHistoryDto.Description, Is.EqualTo(expectedCaseHistory.Description));
+					Assert.That(caseHistoryDto.Action, Is.EqualTo(expectedCaseHistory.Action));
+					Assert.That(caseHistoryDto.Title, Is.EqualTo(expectedCaseHistory.Title));
+					Assert.That(caseHistoryDto.Urn, Is.EqualTo(expectedCaseHistory.Urn));
+					Assert.That(caseHistoryDto.CaseUrn, Is.EqualTo(expectedCaseHistory.CaseUrn));
+					Assert.That(caseHistoryDto.CreatedAt, Is.EqualTo(expectedCaseHistory.CreatedAt));
+				}
+			}
+		}
+		
+		[Test]
+		public void WhenGetCasesHistory_ReturnsException()
+		{
+			// arrange
+			var configuration = new ConfigurationBuilder().ConfigurationUserSecretsBuilder().Build();
+			var tramsApiEndpoint = configuration["trams:api_endpoint"];
+			
+			var httpClientFactory = new Mock<IHttpClientFactory>();
+			var mockMessageHandler = new Mock<HttpMessageHandler>();
+			mockMessageHandler.Protected()
+				.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+				.ReturnsAsync(new HttpResponseMessage
+				{
+					StatusCode = HttpStatusCode.BadRequest
+				});
+
+			var httpClient = new HttpClient(mockMessageHandler.Object);
+			httpClient.BaseAddress = new Uri(tramsApiEndpoint);
+			httpClientFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+			
+			var logger = new Mock<ILogger<CaseHistoryService>>();
+			var caseHistoryService = new CaseHistoryService(httpClientFactory.Object, logger.Object);
+			
+			// act
+			Assert.ThrowsAsync<HttpRequestException>(() => caseHistoryService.GetCasesHistory(CaseFactory.BuildCaseSearch()));
 		}
 	}
 }
