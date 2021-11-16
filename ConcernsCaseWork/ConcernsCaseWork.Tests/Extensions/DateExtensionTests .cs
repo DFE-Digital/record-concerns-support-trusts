ddd036a662fe16825a9de4247ce959b239eb6053
Elijah Aremu <Elijah.AREMU@EDUCATION.GOV.UK>
2021-11-16 16:44:24 +0000

    Build UI timeline component

---
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Extensions/DateExtensionTests .cs
@@ -0,0 +1,19 @@
+﻿using ConcernsCaseWork.Extensions;
+using NUnit.Framework;
+using System;
+
+namespace ConcernsCaseWork.Tests.Extensions
+{
+	[Parallelizable(ParallelScope.All)]
+	public class DateExtensionTests
+	{
+		[TestCase("07/07/2021", "7 July 2021")]
+		[TestCase("11/16/2021", "16 November 2021")]
+
+		public void WhenToUserFriendlyDate_ReturnsExpected(DateTime actual, string expected)
+		{
+			// assert
+			Assert.That(actual.ToUserFriendlyDate(), Is.EqualTo(expected));
+		}
+	}
+}
---
+++ ConcernsCaseWork/ConcernsCaseWork/Extensions/DateExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConcernsCaseWork.Extensions
+{
+	public static class DateExtension
+	{
+		public static string ToUserFriendlyDate(this DateTime value)
+		{
+			return value.ToString("d MMMM yyyy");
+		}
+	}
+}
