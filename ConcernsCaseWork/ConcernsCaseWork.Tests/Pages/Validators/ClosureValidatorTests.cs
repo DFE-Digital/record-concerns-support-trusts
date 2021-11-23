cd21c2d52741b55b6c5a17ca4c23c44ae013c450
PauloLancao <paulolancao@hotmail.com>
2021-11-23 10:26:19 +0000

    add validator for closure

---
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Validators/ClosureValidatorTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ConcernsCaseWork.Tests.Pages.Validators
+{
+	[Parallelizable(ParallelScope.All)]
+	public class ClosureValidatorTests
+	{
+		[Test]
+		public void WhenClosureType_IsValid()
+		{
+			
+		}
+	}
+}
