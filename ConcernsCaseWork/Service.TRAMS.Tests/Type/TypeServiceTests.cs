747a23866623790b03e6b168452c2550d04aef12
PauloLancao <paulolancao@hotmail.com>
2021-11-23 10:59:00 +0000

    Add unit tests to type service

--- ConcernsCaseWork/Service.TRAMS.Tests/Type/TypeServiceTests.cs
+++ ConcernsCaseWork/Service.TRAMS.Tests/Type/TypeServiceTests.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using Moq.Protected;
 using NUnit.Framework;
+using Service.TRAMS.Base;
 using Service.TRAMS.Type;
 using System;
 using System.Linq;
@@ -23,6 +24,7 @@ namespace Service.TRAMS.Tests.Type
 		{
 			// arrange
 			var expectedTypes = TypeFactory.BuildListTypeDto();
+			var apiListWrapperTypes = new ApiListWrapper<TypeDto>(expectedTypes, null);
 			var configuration = new ConfigurationBuilder().ConfigurationUserSecretsBuilder().Build();
 			var tramsApiEndpoint = configuration["trams:api_endpoint"];
 			
@@ -33,7 +35,7 @@ namespace Service.TRAMS.Tests.Type
 				.ReturnsAsync(new HttpResponseMessage
 				{
 					StatusCode = HttpStatusCode.OK,
-					Content = new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(expectedTypes))
+					Content = new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(apiListWrapperTypes))
 				});
 
 			var httpClient = new HttpClient(mockMessageHandler.Object);
@@ -64,7 +66,7 @@ namespace Service.TRAMS.Tests.Type
 		}
 		
 		[Test]
-		public async Task WhenGetTypes_ThrowsException_ReturnsEmptyTypes()
+		public void WhenGetTypes_ThrowsException()
 		{
 			// arrange
 			var configuration = new ConfigurationBuilder().ConfigurationUserSecretsBuilder().Build();
@@ -86,14 +88,38 @@ namespace Service.TRAMS.Tests.Type
 			var logger = new Mock<ILogger<TypeService>>();
 			var typeService = new TypeService(httpClientFactory.Object, logger.Object);
 			
-			// act
-			var types = await typeService.GetTypes();
+			// act / assert
+			Assert.ThrowsAsync<HttpRequestException>(() => typeService.GetTypes());
+		}
+		
+		[Test]
+		public void WhenGetTypes_And_ResponseData_IsNull_ThrowsException()
+		{
+			// arrange
+			var apiListWrapperTypes = new ApiListWrapper<TypeDto>(null, null);
+			
+			var configuration = new ConfigurationBuilder().ConfigurationUserSecretsBuilder().Build();
+			var tramsApiEndpoint = configuration["trams:api_endpoint"];
+			
+			var httpClientFactory = new Mock<IHttpClientFactory>();
+			var mockMessageHandler = new Mock<HttpMessageHandler>();
+			mockMessageHandler.Protected()
+				.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+				.ReturnsAsync(new HttpResponseMessage
+				{
+					StatusCode = HttpStatusCode.OK,
+					Content = new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(apiListWrapperTypes))
+				});
 
-			// assert
-			Assert.That(types, Is.Not.Null);
-			// TODO uncomment when trams api is live
-			//Assert.That(types.Count, Is.EqualTo(0));
-			Assert.That(types.Count, Is.EqualTo(13));
+			var httpClient = new HttpClient(mockMessageHandler.Object);
+			httpClient.BaseAddress = new Uri(tramsApiEndpoint);
+			httpClientFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+			
+			var logger = new Mock<ILogger<TypeService>>();
+			var typeService = new TypeService(httpClientFactory.Object, logger.Object);
+			
+			// act / assert
+			Assert.ThrowsAsync<Exception>(() => typeService.GetTypes());
 		}
 	}
 }
