44b413cf0e8a61e5dea8260b0992abf27e929648
PauloLancao <paulolancao@hotmail.com>
2021-11-24 11:09:43 +0000

    add unit tests to ConcernTypeValidatorTests

--- ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Validators/ConcernTypeValidatorTests.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Validators/ConcernTypeValidatorTests.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using ConcernsCaseWork.Pages.Validators;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace ConcernsCaseWork.Tests.Pages.Validators
 {
@@ -6,9 +10,117 @@ namespace ConcernsCaseWork.Tests.Pages.Validators
 	public class ConcernTypeValidatorTests
 	{
 		[Test]
-		public void WhenConcernType_IsValid()
+		public void WhenConcernType_WhenType_Contains_TypeUrn_IsValid()
 		{
+			// arrange
+			var formCollection = new FormCollection(
+				new Dictionary<string, StringValues>
+				{
+					{ "type", new StringValues("123:type") },
+					{ "sub-type", new StringValues("subtype") },
+					{ "rating", new StringValues("rating") },
+					{ "trust-ukprn", new StringValues("trustukprn") }
+				});
 			
+			// act
+			var isValid = ConcernTypeValidator.IsValid(formCollection);
+
+			// assert
+			Assert.True(isValid);
+		}
+		
+		[Test]
+		public void WhenConcernType_WhenSubType_Contains_TypeUrn_IsValid()
+		{
+			// arrange
+			var formCollection = new FormCollection(
+				new Dictionary<string, StringValues>
+				{
+					{ "type", new StringValues("type") },
+					{ "sub-type", new StringValues("123:subtype") },
+					{ "rating", new StringValues("rating") },
+					{ "trust-ukprn", new StringValues("trustukprn") }
+				});
+			
+			// act
+			var isValid = ConcernTypeValidator.IsValid(formCollection);
+
+			// assert
+			Assert.True(isValid);
+		}
+		
+		[Test]
+		public void WhenConcernType_IsInValid()
+		{
+			// arrange
+			var formCollection = new FormCollection(
+				new Dictionary<string, StringValues>
+				{
+					{ "type", new StringValues("") },
+					{ "sub-type", new StringValues("") },
+					{ "rating", new StringValues("") },
+					{ "trust-ukprn", new StringValues("") }
+				});
+			
+			// act
+			var isValid = ConcernTypeValidator.IsValid(formCollection);
+
+			// assert
+			Assert.False(isValid);
+		}
+		
+		[Test]
+		public void WhenEditConcernType_WhenType_Contains_TypeUrn_IsValid()
+		{
+			// arrange
+			var formCollection = new FormCollection(
+				new Dictionary<string, StringValues>
+				{
+					{ "type", new StringValues("123:type") },
+					{ "sub-type", new StringValues("subtype") }
+				});
+			
+			// act
+			var isValid = ConcernTypeValidator.IsEditValid(formCollection);
+
+			// assert
+			Assert.True(isValid);
+		}
+		
+		[Test]
+		public void WhenEditConcernType_WhenSubType_Contains_TypeUrn_IsValid()
+		{
+			// arrange
+			var formCollection = new FormCollection(
+				new Dictionary<string, StringValues>
+				{
+					{ "type", new StringValues("type") },
+					{ "sub-type", new StringValues("123:subtype") }
+				});
+			
+			// act
+			var isValid = ConcernTypeValidator.IsEditValid(formCollection);
+
+			// assert
+			Assert.True(isValid);
+		}
+		
+		[Test]
+		public void WhenEditConcernType_IsInValid()
+		{
+			// arrange
+			var formCollection = new FormCollection(
+				new Dictionary<string, StringValues>
+				{
+					{ "type", new StringValues("") },
+					{ "sub-type", new StringValues("") }
+				});
+			
+			// act
+			var isValid = ConcernTypeValidator.IsEditValid(formCollection);
+
+			// assert
+			Assert.False(isValid);
 		}
 	}
 }
