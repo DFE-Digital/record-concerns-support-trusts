a636ec692542b8cd58bed4e76027243ff87d0481
PauloLancao <paulolancao@hotmail.com>
2021-11-24 09:49:32 +0000

    Add rating mapping unit tests

--- ConcernsCaseWork/ConcernsCaseWork.Tests/Mappers/RagMappingTests.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Mappers/RagMappingTests.cs
@@ -1,4 +1,5 @@
 ﻿using ConcernsCaseWork.Mappers;
+using ConcernsCaseWork.Shared.Tests.Factory;
 using NUnit.Framework;
 using System.Linq;
 
@@ -48,6 +49,24 @@ namespace ConcernsCaseWork.Tests.Mappers
 
 			// assert
 			CollectionAssert.AreEqual(rags, splitExpected);
+		}
+
+		[Test]
+		public void WhenMapDtoToModel_ReturnsRatingModel()
+		{
+			// arrange
+			var ratingDto = RatingFactory.BuildRatingDto();
+			
+			// act
+			var ratingModel = RatingMapping.MapDtoToModel(ratingDto);
+			
+			// assert
+			Assert.That(ratingModel, Is.Not.Null);
+			Assert.That(ratingModel.Checked, Is.Not.Null);
+			Assert.That(ratingModel.Name, Is.Not.Null);
+			Assert.That(ratingModel.Urn, Is.Not.Null);
+			Assert.That(ratingModel.RagRating, Is.Not.Null);
+			Assert.That(ratingModel.RagRatingCss, Is.Not.Null);
 		}
 	}
 }
