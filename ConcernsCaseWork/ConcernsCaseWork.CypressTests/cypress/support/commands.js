39eb6b0006598537d77af0e4d04db85758b3abfc
DALLY <Michael.DALLY@EDUCATION.GOV.UK>
2021-12-05 08:24:58 +0000

    pushed basic concern creation test

---
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/support/commands.js
@@ -0,0 +1,50 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import "cypress-localstorage-commands";
+
+Cypress.Commands.add("login",()=> {
+	cy.visit(Cypress.env('url')+"/login");
+	cy.get("#username").type(Cypress.env('username'));
+	cy.get("#password").type(Cypress.env('password')+"{enter}");
+	cy.saveLocalStorage();
+})
+
+Cypress.Commands.add('storeSessionData',()=>{
+    Cypress.Cookies.preserveOnce('.ConcernsCasework.Login')
+    let str = [];
+    cy.getCookies().then((cookie) => {
+        cy.log(cookie);
+        for (let l = 0; l < cookie.length; l++) {
+            if (cookie.length > 0 && l == 0) {
+                str[l] = cookie[l].name;
+                Cypress.Cookies.preserveOnce(str[l]);
+            } else if (cookie.length > 1 && l > 1) {
+                str[l] = cookie[l].name;
+                Cypress.Cookies.preserveOnce(str[l]);
+            }
+        }
+    });
+})
