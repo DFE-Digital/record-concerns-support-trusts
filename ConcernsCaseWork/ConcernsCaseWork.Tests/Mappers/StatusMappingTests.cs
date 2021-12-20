ef531b7dc307cd82640946d736ca14d3fc65e7c9
Elijah Aremu <Elijah.AREMU@EDUCATION.GOV.UK>
2021-12-20 16:05:26 +0000

    increase code coverage

---
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Mappers/StatusMappingTests.cs
@@ -0,0 +1,40 @@
+﻿using ConcernsCaseWork.Mappers;
+using ConcernsCaseWork.Shared.Tests.Factory;
+using NUnit.Framework;
+using Service.TRAMS.Trusts;
+using System.Linq;
+
+namespace ConcernsCaseWork.Tests.Mappers
+{
+	[Parallelizable(ParallelScope.All)]
+	public class StatusMappingTests
+	{
+		[Test]
+		public void WhenMapDtoToModel_ReturnsStatusModel()
+		{
+			//arrange
+			var statusesDto = StatusFactory.BuildListStatusDto();
+			var statusDto = statusesDto[1];
+
+			// act
+			var statusModel = StatusMapping.MapDtoToModel(statusesDto, statusDto.Urn);
+
+			//// assert
+			Assert.That(statusModel, Is.Not.Null);
+		}
+
+		[Test]
+		public void WhenMapDtoToModel_When_Urn_IsUknown_ReturnsStatusModel()
+		{
+			//arrange
+			var statusesDto = StatusFactory.BuildListStatusDto();
+
+			// act
+			var statusModel = StatusMapping.MapDtoToModel(statusesDto, 0);
+
+			//// assert
+			Assert.That(statusModel, Is.Not.Null);
+		}
+
+	}
+}
