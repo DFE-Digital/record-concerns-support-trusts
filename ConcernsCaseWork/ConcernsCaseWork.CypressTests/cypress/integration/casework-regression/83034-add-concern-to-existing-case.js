d2874f31fca5bd77ae8fb82d68b723a755b5a4bc
DALLY <Michael.DALLY@EDUCATION.GOV.UK>
2022-01-06 12:29:54 +0000

    removed assertion for case created - made assertion more generic to test that all expected concern types are reported back

--- ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/integration/casework-regression/83034-add-concern-to-existing-case.js
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/integration/casework-regression/83034-add-concern-to-existing-case.js
@@ -21,15 +21,15 @@ describe("User creates subsequent Concern to a case", () => {
 	});
 
 	it("Should allow a user to select a concern type", () => {
-		selectConcernType();
+		cy.selectConcernType();
 	});
 
 	it("Should allow a user to select the risk to the trust", () => {
-		selectRiskToTrust();
+		cy.selectRiskToTrust();
 	});
 
 	it("Should allow the user to enter Concern details", () => {
-		enterConcernDetails();
+		cy.enterConcernDetails();
 	});
 
 	it("Should be able to add a concern via the Add Concern Link", () => {
@@ -37,49 +37,16 @@ describe("User creates subsequent Concern to a case", () => {
 	});
 
 	it("Should allow a user to select a second concern type", () => {
-		selectConcernType();
+		cy.get(".govuk-summary-list__value").should(
+			"contain.text",
+			searchTerm.trim()
+		);
+		cy.selectConcernType();
 	});
 
 	it("Expected number of concern types should be displayed", () => {
 		isCorrectNumberOfStatuses(2, "Red plus");
 	});
-
-	function enterConcernDetails() {
-		let date = new Date();
-		cy.get("#issue").type("Data entered at " + date);
-		cy.get("#current-status").type("Data entered at " + date);
-		cy.get("#case-aim").type("Data entered at " + date);
-		cy.get("#de-escalation-point").type("Data entered at " + date);
-		cy.get("#next-steps").type("Data entered at " + date);
-		cy.get("#case-details-form  button").click();
-	}
-
-	function selectRiskToTrust() {
-		cy.debug();
-		cy.get('[href="/case/rating"').click();
-		cy.get(".ragtag").should("be.visible");
-		//Randomly select a RAG status
-		cy.get(".govuk-radios .ragtag:nth-of-type(1)")
-			.its("length")
-			.then((ragtagElements) => {
-				let num = Math.floor(Math.random() * ragtagElements);
-				cy.get(".govuk-radios .ragtag:nth-of-type(1)").eq(num).click();
-			});
-		cy.get("#case-rating-form > div.govuk-button-group > button").click();
-	}
-
-	//TODO: make this more dynamic - current usability issue raised
-	//under 83452
-	function selectConcernType() {
-		cy.get(".govuk-summary-list__value").should(
-			"contain.text",
-			searchTerm.trim()
-		);
-		cy.get(".govuk-radios__item [value=Financial]").click();
-		cy.get("[id=sub-type-3]").click();
-		cy.get("[id=rating-3]").click();
-		cy.get(".govuk-button").click();
-	}
 
 	function isCorrectNumberOfStatuses(expectedNumberOfRagStatus, ragStatus) {
 		switch (ragStatus) {
