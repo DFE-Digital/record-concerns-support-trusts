141607f7953d14335bb8261f1fe95a9bd06a51a7
Elijah Aremu <Elijah.AREMU@EDUCATION.GOV.UK>
2022-03-08 15:39:36 +0000

    add case action radio buttons, post to action page, add srma skeleton page

--- ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/Index.cshtml.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using ConcernsCaseWork.Pages.Base;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -10,10 +11,11 @@ namespace ConcernsCaseWork.Pages.Case.Management.Action
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 	public class IndexPageModel : AbstractPageModel
 	{
+		private readonly ILogger<IndexPageModel> _logger;
 
-		public IndexPageModel()
+		public IndexPageModel(ILogger<IndexPageModel> logger)
 		{
-
+			_logger = logger;
 		}
 
 		public async Task OnGetAsync()
@@ -24,9 +26,36 @@ namespace ConcernsCaseWork.Pages.Case.Management.Action
 			}
 			catch (Exception ex)
 			{
-				//_logger.LogError("Case::ManagementPageModel::OnGetAsync::Exception - {Message}", ex.Message);
 
-				//TempData["Error.Message"] = ErrorOnGetPage;
+			}
+		}
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			try
+			{
+				_logger.LogInformation("Case::Action::IndexPageModel::OnPostAsync");
+
+				// Fetch case urn
+				var caseUrnValue = RouteData.Values["urn"];
+				if (caseUrnValue == null || !long.TryParse(caseUrnValue.ToString(), out var caseUrn) || caseUrn == 0)
+					throw new Exception("CaseUrn is null or invalid to parse");
+
+				// Form
+				var action = Request.Form["action"].ToString();
+
+				if (string.IsNullOrEmpty(action))
+					throw new Exception("Missing form values");
+
+
+				return RedirectToPage($"{action}/add", new { urn = caseUrn });
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("Case::Action::IndexPageModel::OnPostAsync::Exception - {Message}", ex.Message);
+
+				TempData["Error.Message"] = ErrorOnPostPage;
+				return Page();
 			}
 		}
 	}
