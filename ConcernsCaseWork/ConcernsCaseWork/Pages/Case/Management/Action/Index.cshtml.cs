4bee71974412444fc22b2e3899a9250c47dc47f2
Elijah Aremu <Elijah.AREMU@EDUCATION.GOV.UK>
2022-03-08 09:42:25 +0000

    Add action index page and folder structure

---
+++ ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using ConcernsCaseWork.Pages.Base;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace ConcernsCaseWork.Pages.Case.Management.Action
+{
+	[Authorize]
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public class IndexPageModel : AbstractPageModel
+	{
+
+		public IndexPageModel()
+		{
+
+		}
+
+		public async Task OnGetAsync()
+		{
+			try
+			{
+
+			}
+			catch (Exception ex)
+			{
+				//_logger.LogError("Case::ManagementPageModel::OnGetAsync::Exception - {Message}", ex.Message);
+
+				//TempData["Error.Message"] = ErrorOnGetPage;
+			}
+		}
+	}
+}
