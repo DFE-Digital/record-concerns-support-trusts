33760cbdc9c4c5924a62f9925e5ff0e17e0aaedd
Menol <menol.razeek@education.gov.uk>
2022-03-17 12:35:58 +0000

    removed unused class

--- ConcernsCaseWork/Service.TRAMS/Cases/ICaseSearchService.cs
+++ ConcernsCaseWork/Service.TRAMS/Cases/ICaseSearchService.cs
@@ -11,29 +11,4 @@ namespace Service.TRAMS.Cases
 		Task<IList<CaseDto>> GetCasesByPageSearch(PageSearch pageSearch);
 		Task<IList<CaseHistoryDto>> GetCasesHistoryByCaseSearch(CaseSearch caseSearch);
 	}
-
-	public class TestDataCaseSearchService : ICaseSearchService
-	{
-		// in memory collection
-		public Task<IList<CaseDto>> GetCasesByCaseTrustSearch(CaseTrustSearch caseTrustSearch)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		public Task<IList<CaseDto>> GetCasesByCaseworkerAndStatus(CaseCaseWorkerSearch caseCaseWorkerSearch)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		public Task<IList<CaseDto>> GetCasesByPageSearch(PageSearch pageSearch)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		public Task<IList<CaseHistoryDto>> GetCasesHistoryByCaseSearch(CaseSearch caseSearch)
-		{
-			throw new System.NotImplementedException();
-		}
-	}
-
 }
