6e9b86760594df6dce358fda244256041710da92
Elijah Aremu <Elijah.AREMU@EDUCATION.GOV.UK>
2022-03-23 11:07:50 +0000

    add validation that checks whether there is an existing SRMA

---
+++ ConcernsCaseWork/ConcernsCaseWork.Shared.Tests/Factory/SrmaFactory.cs
@@ -0,0 +1,31 @@
+﻿using AutoFixture;
+using ConcernsCaseWork.Enums;
+using ConcernsCaseWork.Models.CaseActions;
+using System;
+using System.Collections.Generic;
+
+namespace ConcernsCaseWork.Shared.Tests.Factory
+{
+	public static class SrmaFactory
+	{
+		private readonly static Fixture Fixture = new Fixture();
+
+		public static List<SRMA> BuildListSrmaModel(SRMAStatus status)
+		{
+			return new List<SRMA>
+			{
+				BuildSrmaModel(status)
+			};
+		}
+
+		public static SRMA BuildSrmaModel(SRMAStatus status)
+		{
+			return new SRMA
+			{
+				DateOffered = Fixture.Create<DateTime>(),
+				Status = status,
+				Notes = Fixture.Create<string>()
+			};
+		}
+	}
+}
