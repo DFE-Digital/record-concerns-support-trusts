b5853642877339570337d03196de8d2993fb34b2
Elijah Aremu <Elijah.AREMU@EDUCATION.GOV.UK>
2022-04-13 11:03:29 +0100

    add ability to edit date report sent to trust

--- ConcernsCaseWork/ConcernsCaseWork/Models/CaseActions/SRMAModel.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Models/CaseActions/SRMAModel.cs
@@ -8,7 +8,7 @@ namespace ConcernsCaseWork.Models.CaseActions
 		public long Id { get; set; }
 		public DateTime	DateOffered { get; set; }
 		public DateTime? DateAccepted { get; set; }
-		public DateTime DateReportSentToTrust { get; set; }
+		public DateTime? DateReportSentToTrust { get; set; }
 		public DateTime? DateVisitStart { get; set; }
 		public DateTime? DateVisitEnd { get; set; }
 		public SRMAStatus Status { get; set; }
