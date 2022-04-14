de21b6ef270fb531586ddc348956f9352f430570
Elijah Aremu <Elijah.AREMU@EDUCATION.GOV.UK>
2022-04-14 11:13:34 +0100

    add enum helper tests

---
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Helpers/EnumHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using ConcernsCaseWork.Enums;
+using ConcernsCaseWork.Helpers;
+using NUnit.Framework;
+using System;
+
+namespace ConcernsCaseWork.Tests.Helpers
+{
+	[Parallelizable(ParallelScope.All)]
+	public class EnumHelperTests
+	{
+		[TestCase(SRMAStatus.TrustConsidering, "Trust considering")]
+		[TestCase(SRMAStatus.PreparingForDeployment, "Preparing for deployment")]
+		[TestCase(SRMAStatus.Deployed, "Deployed")]
+		public void WhenGetEnumDescription_ReturnsExpected(SRMAStatus value, string expectedResult)
+		{
+			// act
+			var result = EnumHelper.GetEnumDescription(value);
+
+			//assert
+			Assert.That(result, Is.EqualTo(expectedResult));
+		}
+
+
+
+	}
+}
