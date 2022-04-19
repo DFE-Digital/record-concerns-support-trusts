38314645838d54243050bdb18e90065652cf4cb0
Elijah Aremu <Elijah.AREMU@EDUCATION.GOV.UK>
2022-04-19 14:01:40 +0100

    add unit test for date offered

---
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Case/Management/Action/SRMA/Edit/EditDateOfferedPageModelTests.cs
@@ -0,0 +1,210 @@
+﻿using ConcernsCaseWork.Enums;
+using ConcernsCaseWork.Pages.Case.Management.Action.SRMA;
+using ConcernsCaseWork.Services.Cases;
+using ConcernsCaseWork.Shared.Tests.Factory;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConcernsCaseWork.Tests.Pages.Case.Management.Action.SRMA.Edit
+{
+	[Parallelizable(ParallelScope.All)]
+	public class EditDateOfferedPageModelTests
+	{
+		[Test]
+		public async Task WhenOnGetAsync_ReturnsPage()
+		{
+			// arrange 
+			var mockSRMAModelService = new Mock<ISRMAService>();
+			var mockLogger = new Mock<ILogger<EditDateOfferedPageModel>>();
+
+			var srmaModel = SrmaFactory.BuildSrmaModel(SRMAStatus.Deployed);
+
+			mockSRMAModelService.Setup(s => s.GetSRMAById(It.IsAny<long>()))
+				.ReturnsAsync(srmaModel);
+
+			var pageModel = SetupDateOfferedPageModel(mockSRMAModelService.Object, mockLogger.Object);
+			var routeData = pageModel.RouteData.Values;
+			routeData.Add("caseUrn", srmaModel.CaseUrn);
+			routeData.Add("srmaId", srmaModel.Id);
+
+			// act
+			var pageResponse = await pageModel.OnGetAsync();
+
+			// assert
+			Assert.That(pageResponse, Is.InstanceOf<PageResult>());
+			var page = pageResponse as PageResult;
+
+			Assert.That(page, Is.Not.Null);
+			Assert.That(pageModel.SRMAModel, Is.Not.Null);
+		}
+
+		[Test]
+		public async Task WhenOnGetAsync_MissingRouteData_CaseUrn_ThrowsException_ReturnsPage()
+		{
+			// arrange 
+			var mockSRMAModelService = new Mock<ISRMAService>();
+			var mockLogger = new Mock<ILogger<EditDateOfferedPageModel>>();
+
+			var pageModel = SetupDateOfferedPageModel(mockSRMAModelService.Object, mockLogger.Object);
+
+			// act 
+			var pageResponse = await pageModel.OnGetAsync();
+			var routeData = pageModel.RouteData.Values;
+			routeData.Add("SrmaId", 1);
+
+			// assert
+			Assert.That(pageResponse, Is.InstanceOf<PageResult>());
+			var page = pageResponse as PageResult;
+
+			Assert.That(page, Is.Not.Null);
+			Assert.IsNull(pageModel.SRMAModel);
+			Assert.That(pageModel.TempData, Is.Not.Null);
+			Assert.That(pageModel.TempData["Error.Message"], Is.EqualTo("An error occurred loading the page, please try again. If the error persists contact the service administrator."));
+
+			mockSRMAModelService.Verify(s =>
+				s.GetSRMAById(It.IsAny<long>()), Times.Never);
+		}
+
+		[Test]
+		public async Task WhenOnGetAsync_MissingRouteData_SrmaID_ThrowsException_ReturnsPage()
+		{
+			// arrange 
+			var mockSRMAModelService = new Mock<ISRMAService>();
+			var mockLogger = new Mock<ILogger<EditDateOfferedPageModel>>();
+
+			var pageModel = SetupDateOfferedPageModel(mockSRMAModelService.Object, mockLogger.Object);
+			var routeData = pageModel.RouteData.Values;
+			routeData.Add("caseUrn", 1);
+
+			// act 
+			var pageResponse = await pageModel.OnGetAsync();
+
+			// assert
+			Assert.That(pageResponse, Is.InstanceOf<PageResult>());
+			var page = pageResponse as PageResult;
+
+			Assert.That(page, Is.Not.Null);
+			Assert.IsNull(pageModel.SRMAModel);
+			Assert.That(pageModel.TempData, Is.Not.Null);
+			Assert.That(pageModel.TempData["Error.Message"], Is.EqualTo("An error occurred loading the page, please try again. If the error persists contact the service administrator."));
+
+			mockSRMAModelService.Verify(s =>
+				s.GetSRMAById(It.IsAny<long>()), Times.Never);
+		}
+
+		[Test]
+		public async Task WhenOnPostAsync_MissingRouteData_ThrowsException_ReturnsPage()
+		{
+			// arrange 
+			var mockSRMAModelService = new Mock<ISRMAService>();
+			var mockLogger = new Mock<ILogger<EditDateOfferedPageModel>>();
+
+			var pageModel = SetupDateOfferedPageModel(mockSRMAModelService.Object, mockLogger.Object);
+
+			// act 
+			var pageResponse = await pageModel.OnPostAsync();
+
+			// assert
+			Assert.That(pageResponse, Is.InstanceOf<PageResult>());
+			var page = pageResponse as PageResult;
+
+			Assert.That(page, Is.Not.Null);
+			Assert.IsNull(pageModel.SRMAModel);
+			Assert.That(pageModel.TempData, Is.Not.Null);
+			Assert.That(pageModel.TempData["Error.Message"], Is.EqualTo("An error occurred posting the form, please try again. If the error persists contact the service administrator."));
+
+			mockSRMAModelService.Verify(s =>
+				s.GetSRMAById(It.IsAny<long>()), Times.Never);
+		}
+
+		[Test]
+		public async Task WhenOnPostAsync_RouteData_RequestForm_Return_To_SRMA_Page()
+		{
+			// arrange 
+			var mockSRMAModelService = new Mock<ISRMAService>();
+			var mockLogger = new Mock<ILogger<EditDateOfferedPageModel>>();
+
+			var srmaModel = SrmaFactory.BuildSrmaModel(SRMAStatus.Deployed);
+
+			var pageModel = SetupDateOfferedPageModel(mockSRMAModelService.Object, mockLogger.Object);
+			var routeData = pageModel.RouteData.Values;
+			routeData.Add("caseUrn", srmaModel.CaseUrn);
+			routeData.Add("srmaId", srmaModel.Id);
+
+			pageModel.HttpContext.Request.Form = new FormCollection(
+				new Dictionary<string, StringValues>
+				{
+					{ "dtr-day", new StringValues("07") },
+					{ "dtr-month", new StringValues("04") },
+					{ "dtr-year", new StringValues("2022") }
+				});
+
+			var pageResponse = await pageModel.OnPostAsync();
+
+			// assert
+			Assert.That(pageResponse, Is.InstanceOf<RedirectResult>());
+			var page = pageResponse as RedirectResult;
+
+			Assert.That(page, Is.Not.Null);
+			Assert.That(page.Url, Is.EqualTo($"/case/{srmaModel.CaseUrn}/management/action/srma/{srmaModel.Id}"));
+		}
+
+		[Test]
+		public async Task WhenOnPostAsync_RouteData_RequestForm_InvalidDate_ThrowsInvalidOperationException_Page()
+		{
+			// arrange 
+			var mockSRMAModelService = new Mock<ISRMAService>();
+			var mockLogger = new Mock<ILogger<EditDateOfferedPageModel>>();
+
+			var srmaModel = SrmaFactory.BuildSrmaModel(SRMAStatus.Deployed);
+
+			var pageModel = SetupDateOfferedPageModel(mockSRMAModelService.Object, mockLogger.Object);
+			var routeData = pageModel.RouteData.Values;
+			routeData.Add("caseUrn", srmaModel.CaseUrn);
+			routeData.Add("srmaId", srmaModel.Id);
+
+			pageModel.HttpContext.Request.Form = new FormCollection(
+				new Dictionary<string, StringValues>
+				{
+					{ "dtr-day", new StringValues("00") },
+					{ "dtr-month", new StringValues("04") },
+					{ "dtr-year", new StringValues("2022") }
+				});
+
+			var pageResponse = await pageModel.OnPostAsync();
+
+			// assert
+			Assert.That(pageResponse, Is.InstanceOf<PageResult>());
+			var page = pageResponse as PageResult;
+
+			Assert.That(page, Is.Not.Null);
+			Assert.IsNull(pageModel.SRMAModel);
+			Assert.That(pageModel.TempData, Is.Not.Null);
+			Assert.That(pageModel.TempData["SRMA.Message"], Is.EqualTo("00-04-2022 is an invalid date"));
+		}
+
+		private static EditDateOfferedPageModel SetupDateOfferedPageModel(
+		ISRMAService mockSrmaModelService, ILogger<EditDateOfferedPageModel> mockLogger, bool isAuthenticated = false)
+		{
+			(PageContext pageContext, TempDataDictionary tempData, ActionContext actionContext) = PageContextFactory.PageContextBuilder(isAuthenticated);
+
+			return new EditDateOfferedPageModel(mockSrmaModelService, mockLogger)
+			{
+				PageContext = pageContext,
+				TempData = tempData,
+				Url = new UrlHelper(actionContext),
+				MetadataProvider = pageContext.ViewData.ModelMetadata
+			};
+
+		}
+	}
+}
--- ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/SRMA/Edit/DateOffered.cshtml.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/SRMA/Edit/DateOffered.cshtml.cs
@@ -1,6 +1,4 @@
-﻿using ConcernsCaseWork.Enums;
-using ConcernsCaseWork.Helpers;
-using ConcernsCaseWork.Models;
+﻿using ConcernsCaseWork.Helpers;
 using ConcernsCaseWork.Models.CaseActions;
 using ConcernsCaseWork.Pages.Base;
 using ConcernsCaseWork.Services.Cases;
@@ -8,25 +6,22 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ConcernsCaseWork.Pages.Case.Management.Action.SRMA
 {
 	[Authorize]
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-	public class EditOfferedDatePageModel : AbstractPageModel
+	public class EditDateOfferedPageModel : AbstractPageModel
 	{
-		private readonly ISRMAService srmaService;
-		private readonly ILogger<EditOfferedDatePageModel> _logger;
+		private readonly ISRMAService _srmaModelService;
+		private readonly ILogger<EditDateOfferedPageModel> _logger;
 
-		public SRMAModel SRMA { get; set; }
+		public SRMAModel SRMAModel { get; set; }
 
-		public EditOfferedDatePageModel(ISRMAService srmaService, ILogger<EditOfferedDatePageModel> logger)
+		public EditDateOfferedPageModel(ISRMAService srmaService, ILogger<EditDateOfferedPageModel> logger)
 		{
-			this.srmaService = srmaService;
+			_srmaModelService = srmaService;
 			_logger = logger;
 		}
 
@@ -36,15 +31,15 @@ namespace ConcernsCaseWork.Pages.Case.Management.Action.SRMA
 
 			try
 			{
-				_logger.LogInformation("Case::EditOfferedDatePageModel::OnGetAsync");
+				_logger.LogInformation("Case::EditDateOfferedPageModel::OnGetAsync");
 
 				(_, srmaId) = GetRouteData();
 
-				SRMA = await srmaService.GetSRMAById(srmaId);
+				SRMAModel = await _srmaModelService.GetSRMAById(srmaId);
 			}
 			catch (Exception ex)
 			{
-				_logger.LogError("Case::EditOfferedDatePageModel::OnGetAsync::Exception - {Message}", ex.Message);
+				_logger.LogError("Case::EditDateOfferedPageModel::OnGetAsync::Exception - {Message}", ex.Message);
 				TempData["Error.Message"] = ErrorOnGetPage;
 			}
 
@@ -58,7 +53,7 @@ namespace ConcernsCaseWork.Pages.Case.Management.Action.SRMA
 
 			try
 			{
-				_logger.LogInformation("Case::Action::SRMA::EditOfferedDatePageModel::OnPostAsync");
+				_logger.LogInformation("Case::Action::SRMA::EditDateOfferedPageModel::OnPostAsync");
 
 				(caseUrn, srmaId) = GetRouteData();
 
@@ -73,7 +68,7 @@ namespace ConcernsCaseWork.Pages.Case.Management.Action.SRMA
 					throw new InvalidOperationException($"{dtString} is an invalid date");
 				}
 
-				await srmaService.SetOfferedDate(srmaId, dt);
+				await _srmaModelService.SetOfferedDate(srmaId, dt);
 				return Redirect($"/case/{caseUrn}/management/action/srma/{srmaId}");
 			}
 			catch (InvalidOperationException ex)
@@ -83,37 +78,11 @@ namespace ConcernsCaseWork.Pages.Case.Management.Action.SRMA
 			}
 			catch (Exception ex)
 			{
-				_logger.LogError("Case::Action::SRMA::EditOfferedDatePageModel::OnPostAsync::Exception - {Message}", ex.Message);
+				_logger.LogError("Case::Action::SRMA::EditDateOfferedPageModel::OnPostAsync::Exception - {Message}", ex.Message);
 				TempData["Error.Message"] = ErrorOnPostPage;
 			}
 
 			return Page();
-		}
-
-		private (List<string> validationErrors, long srmaId, DateTime dateOffered, long caseId) ValidateInputsForPost()
-		{
-			var validationErrors = new List<string>();
-			
-			var caseUrnStr = Convert.ToString(RouteData.Values["caseUrn"]);
-			if (!long.TryParse(caseUrnStr, out long caseUrn))
-			{
-				validationErrors.Add("Invalid case Id");
-			}
-
-			var srmaIdStr = Convert.ToString(RouteData.Values["srmaId"]);
-			if (!long.TryParse(srmaIdStr, out long srmaId))
-			{
-				validationErrors.Add("SRMA Id not found");
-			}
-
-			var dtString = $"{Request.Form["dtr-day"]}-{Request.Form["dtr-month"]}-{Request.Form["dtr-year"]}";
-
-			if (!DateTimeHelper.TryParseExact(dtString, out DateTime dateOffered))
-			{
-				validationErrors.Add("SRMA offered date is not valid");
-			}
-
-			return (validationErrors, srmaId, dateOffered, caseUrn);
 		}
 
 		private (long caseUrn, long srmaId) GetRouteData()
