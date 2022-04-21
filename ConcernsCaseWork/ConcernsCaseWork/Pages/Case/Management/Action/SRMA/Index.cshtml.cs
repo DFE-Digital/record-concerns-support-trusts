561623e35cc2377323fd7d7b27c9052423612981
Elijah Aremu <Elijah.AREMU@EDUCATION.GOV.UK>
2022-04-21 13:58:29 +0100

    cancelled button goes to resolved page

--- ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/SRMA/Index.cshtml.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/SRMA/Index.cshtml.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Linq;
 using System.Threading.Tasks;
-using CaseActionModels = ConcernsCaseWork.Models.CaseActions;
+using ConcernsCaseWork.Models.CaseActions;
+using ConcernsCaseWork.Enums;
 
 namespace ConcernsCaseWork.Pages.Case.Management.Action.SRMA
 {
@@ -14,48 +14,39 @@ namespace ConcernsCaseWork.Pages.Case.Management.Action.SRMA
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 	public class IndexPageModel : AbstractPageModel
 	{
-		private readonly ISRMAService _srmaService;
+		private readonly ISRMAService _srmaModelService;
 		private readonly ILogger<IndexPageModel> _logger;
 
-		public long CaseUrn { get; set; }
-		public CaseActionModels.SRMAModel SRMA { get; set; }
+		public SRMAModel SRMAModel { get; set; }
 		public string DeclineCompleteButtonLabel { get; private set; }
 
 		public IndexPageModel(ISRMAService srmaService, ILogger<IndexPageModel> logger)
 		{
-			_srmaService = srmaService;
+			_srmaModelService = srmaService;
 			_logger = logger;
 		}
 
 		public async Task OnGetAsync()
 		{
-			var caseUrnValue = Convert.ToString(RouteData.Values["urn"]);
-			if (caseUrnValue == null || !long.TryParse(caseUrnValue, out long caseUrn) || caseUrn <= 0)
-			{
-				TempData["Error.Message"] = "Case Id not provided or invalid";
-				return;
-			}
+			long caseUrn = 0;
+			long srmaId = 0;
 
-			var srmaIdValue = Convert.ToString(RouteData.Values["srmaId"]);
-			if (srmaIdValue == null || !long.TryParse(srmaIdValue, out long srmaId) || srmaId <= 0)
+			try
 			{
-				TempData["Error.Message"] = "SRMA Id not provided or invalid";
-				return;
-			}
+				_logger.LogInformation("Case::Action::SRMA::IndexPageModel::OnGetAsync");
 
-			CaseUrn = caseUrn;
+				(caseUrn, srmaId) = GetRouteData();
+				// TODO - get SRMA by case ID and SRMA ID
+				SRMAModel = await _srmaModelService.GetSRMAById(srmaId);
 
-			SRMA = await _srmaService.GetSRMAById(srmaId);
-			if(SRMA == null)
-			{
-				TempData["SRMA.Message"] =  "Could not load this SRMA";
-				return;
-			}
-			else
-			{
-				switch (SRMA.Status)
+				if (SRMAModel == null)
 				{
-					case Enums.SRMAStatus.Deployed:
+					throw new InvalidOperationException("Could not load this SRMA");
+				}
+
+				switch (SRMAModel.Status)
+				{
+					case SRMAStatus.Deployed:
 						DeclineCompleteButtonLabel = "SRMA complete";
 						break;
 					default:
@@ -63,13 +54,48 @@ namespace ConcernsCaseWork.Pages.Case.Management.Action.SRMA
 						break;
 				}
 			}
-
+			catch (InvalidOperationException ex)
+			{
+				TempData["SRMA.Message"] = ex.Message;
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("Case::Action::SRMA::IndexPageModel::OnGetAsync::Exception - {Message}", ex.Message);
+				TempData["Error.Message"] = ErrorOnGetPage;
+			}
 		}
 
 		public async Task<IActionResult> OnPostAsync()
 		{
-
 			return RedirectToPage("index");
+		}
+
+		public async Task<ActionResult> OnGetCancel()
+		{
+			try
+			{
+				return Redirect("resolve");
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("Case::Action::SRMA::IndexPageModel::OnGetCancel::Exception - {Message}", ex.Message);
+
+				TempData["Error.Message"] = ErrorOnGetPage;
+				return Page();
+			}
+		}
+
+		private (long caseUrn, long srmaId) GetRouteData()
+		{
+			var caseUrnValue = RouteData.Values["urn"];
+			if (caseUrnValue == null || !long.TryParse(caseUrnValue.ToString(), out long caseUrn) || caseUrn == 0)
+				throw new Exception("CaseUrn is null or invalid to parse");
+
+			var srmaIdValue = RouteData.Values["srmaId"];
+			if (srmaIdValue == null || !long.TryParse(srmaIdValue.ToString(), out long srmaId) || srmaId == 0)
+				throw new Exception("srmaId is null or invalid to parse");
+
+			return (caseUrn, srmaId);
 		}
 	}
 }
---
+++ ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/SRMA/Resolve.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using ConcernsCaseWork.Enums;
+using ConcernsCaseWork.Helpers;
+using ConcernsCaseWork.Models;
+using ConcernsCaseWork.Pages.Base;
+using ConcernsCaseWork.Services.Cases;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConcernsCaseWork.Models.CaseActions;
+
+namespace ConcernsCaseWork.Pages.Case.Management.Action.Srma
+{
+	[Authorize]
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public class ResolvePageModel : AbstractPageModel
+	{
+		private readonly ILogger<ResolvePageModel> _logger;
+		private readonly ISRMAService _srmaModelService;
+
+		public ResolvePageModel(
+			ISRMAService srmaModelService, ILogger<ResolvePageModel> logger)
+		{
+			_srmaModelService = srmaModelService;
+			_logger = logger;
+		}
+
+		public async Task<IActionResult> OnGetAsync()
+		{
+			_logger.LogInformation("Case::Action::SRMA::ResolvePageModel::OnGetAsync");
+
+			try
+			{
+				GetRouteData();
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("Case::SRMA::ResolvePageModel::OnGetAsync::Exception - {Message}", ex.Message);
+
+				TempData["Error.Message"] = ErrorOnGetPage;
+			}
+
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			try
+			{
+				var caseUrn = GetRouteData();
+
+				return Redirect($"/case/{caseUrn}/management");
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("Case::SRMA::ResolvePageModel::OnPostAsync::Exception - {Message}", ex.Message);
+
+				TempData["Error.Message"] = ErrorOnPostPage;
+				return Page();
+			}
+		}
+
+		private (long caseUrn, long srmaId) GetRouteData()
+		{
+			var caseUrnValue = RouteData.Values["urn"];
+			if (caseUrnValue == null || !long.TryParse(caseUrnValue.ToString(), out long caseUrn) || caseUrn == 0)
+				throw new Exception("CaseUrn is null or invalid to parse");
+
+			var srmaIdValue = RouteData.Values["srmaId"];
+			if (srmaIdValue == null || !long.TryParse(srmaIdValue.ToString(), out long srmaId) || srmaId == 0)
+				throw new Exception("srmaId is null or invalid to parse");
+
+			return (caseUrn, srmaId);
+		}
+	}
+}
