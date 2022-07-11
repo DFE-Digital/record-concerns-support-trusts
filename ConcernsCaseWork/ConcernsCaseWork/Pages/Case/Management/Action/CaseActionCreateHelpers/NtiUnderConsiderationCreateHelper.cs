1677fd28c0eab1ab58b626c7a9314e8c6d2b7294
Menol <menol.razeek@education.gov.uk>
2022-07-11 15:16:28 +0100

    refactored case type selection

---
+++ ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/CaseActionCreateHelpers/NtiUnderConsiderationCreateHelper.cs
@@ -0,0 +1,31 @@
+﻿using ConcernsCaseWork.Models.CaseActions;
+using ConcernsCaseWork.Services.Cases;
+using Service.TRAMS.Cases;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConcernsCaseWork.Pages.Case.Management.Action.CaseActionCreateHelpers
+{
+	public class NtiUnderConsiderationCreateHelper : CaseActionCreateHelper
+	{
+		private readonly INtiModelService _ntiModelService;
+
+		public NtiUnderConsiderationCreateHelper(INtiModelService ntiModelService)
+		{
+			_ntiModelService = ntiModelService;
+		}
+
+		public override bool CanHandle(CaseActionEnum caseActionEnum)
+		{
+			return caseActionEnum == CaseActionEnum.NtiUnderConsideration;
+		}
+
+		public override async Task<bool> NewCaseActionAllowed(long caseUrn, string caseWorker)
+		{
+			var ntis = await _ntiModelService.GetNtiUnderConsiderationsForCase(caseUrn);
+			return base.HasOpenCaseAction(ntis) ? throw new InvalidOperationException("There is already an open NTI: Under consideration action linked to this case. Please resolve that before opening another one.")
+				: true;
+		}
+	}
+}
---
+++ ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/CaseActionCreateHelpers/SrmaCreateHelper.cs
@@ -0,0 +1,32 @@
+﻿using ConcernsCaseWork.Models.CaseActions;
+using ConcernsCaseWork.Services.Cases;
+using Service.TRAMS.Cases;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConcernsCaseWork.Pages.Case.Management.Action.CaseActionCreateHelpers
+{
+	public class SrmaCreateHelper : CaseActionCreateHelper
+	{
+		private readonly ISRMAService _srmaService;
+
+		public SrmaCreateHelper(ISRMAService srmaService)
+		{
+			_srmaService = srmaService;
+		}
+
+		public override bool CanHandle(CaseActionEnum caseActionEnum)
+		{
+			return caseActionEnum == CaseActionEnum.Srma;
+		}
+
+		public override async Task<bool> NewCaseActionAllowed(long caseUrn, string caseWorker)
+		{
+			var srmas = await _srmaService.GetSRMAsForCase(caseUrn);
+
+			return base.HasOpenCaseAction(srmas) ? throw new InvalidOperationException("There is already an open SRMA action linked to this case. Please resolve that before opening another one.")
+				: true;
+		}
+	}
+}
