6369dab60a3ada5b8e94f2baac60d91204753f2b
Menol <menol.razeek@education.gov.uk>
2022-08-23 12:24:35 +0100

    tests

---
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Mappers/NtiMappingTests.cs
@@ -0,0 +1,145 @@
+﻿using ConcernsCaseWork.Mappers;
+using ConcernsCaseWork.Models.CaseActions;
+using ConcernsCaseWork.Shared.Tests.Factory;
+using NUnit.Framework;
+using Service.TRAMS.Nti;
+using Service.TRAMS.NtiUnderConsideration;
+using Service.TRAMS.NtiWarningLetter;
+using Service.TRAMS.Trusts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConcernsCaseWork.Tests.Mappers
+{
+	[Parallelizable(ParallelScope.All)]
+	public class NtiMappingTests
+	{
+		[Test]
+		public void WhenMapDtoToServiceModel_ReturnsCorrectModel()
+		{
+			//arrange
+			var testData = new
+			{
+				Id = 1L,
+				CaseUrn = 123,
+				CreatedAt = DateTime.Now.AddDays(-5),
+				ClosedAt = DateTime.Now,
+				Notes = "Test notes",
+				Reasons = new KeyValuePair<int, string>[] { new KeyValuePair<int, string>(1, "Reason1") },
+				SentDate = DateTime.Now.AddDays(-1),
+				Status = new KeyValuePair<int, string>(1, "Status 1"),
+				UpdatedAt = DateTime.Now.AddDays(-1)
+			};
+
+			var ntiDto = new NtiDto
+			{
+				Id = testData.Id,
+				CaseUrn = testData.CaseUrn,
+				CreatedAt = testData.CreatedAt,
+				ClosedAt = testData.ClosedAt,
+				Notes = testData.Notes,
+				DateLetterSent = testData.SentDate,
+				StatusId = testData.Status.Key,
+				UpdatedAt = testData.UpdatedAt,
+				ReasonsMapping = testData.Reasons.Select(r => r.Key).ToArray()
+			};
+
+
+			// act
+			var serviceModel = NtiMappers.ToServiceModel(ntiDto);
+
+			// assert
+			Assert.That(serviceModel, Is.Not.Null);
+			Assert.That(serviceModel.CaseUrn, Is.EqualTo(testData.CaseUrn));
+			Assert.That(serviceModel.Id, Is.EqualTo(testData.Id));
+			Assert.That(serviceModel.Reasons, Is.Not.Null);
+			Assert.That(serviceModel.Reasons.Count, Is.EqualTo(testData.Reasons.Length));
+			Assert.That(serviceModel.Reasons.ElementAt(0).Id, Is.EqualTo(testData.Reasons.ElementAt(0).Key));
+		}
+
+		[Test]
+		public void WhenMapDtoToDbModel_ReturnsCorrectModel()
+		{
+			//arrange
+			var testData = new
+			{
+				Id = 1L,
+				CaseUrn = 123L,
+				CreatedAt = DateTime.Now.AddDays(-5),
+				ClosedAt = DateTime.Now,
+				Notes = "Test notes",
+				Reasons = new KeyValuePair<int, string>[] { new KeyValuePair<int, string>(1, "Reason1") },
+				SentDate = DateTime.Now.AddDays(-1),
+				Status = new KeyValuePair<int, string>(1, "Status 1"),
+				UpdatedAt = DateTime.Now.AddDays(-1)
+			};
+
+			var serviceModel = new NtiModel
+			{
+				Id = testData.Id,
+				CaseUrn = testData.CaseUrn,
+				CreatedAt = testData.CreatedAt,
+				ClosedAt = testData.ClosedAt,
+				Notes = testData.Notes,
+				Reasons = new NtiReasonModel[] { new NtiReasonModel { Id = testData.Reasons.First().Key, Name = testData.Reasons.First().Value } },
+				Status = new NtiStatusModel { Id = testData.Status.Key, Name = testData.Status.Value },
+				SentDate = testData.SentDate,
+				UpdatedAt = testData.UpdatedAt
+			};
+
+			// act
+			var dbModel = NtiMappers.ToDBModel(serviceModel);
+
+			// assert
+			Assert.That(dbModel, Is.Not.Null);
+			Assert.That(dbModel.CaseUrn, Is.EqualTo(testData.CaseUrn));
+			Assert.That(dbModel.Id, Is.EqualTo(testData.Id));
+
+			Assert.That(dbModel.StatusId, Is.Not.Null);
+			Assert.That(dbModel.StatusId, Is.EqualTo(testData.Status.Key));
+		}
+
+		[Test]
+		public void NtiReason_Dto_To_ServiceModel()
+		{
+			// arrange
+			var dto = new NtiReasonDto
+			{
+				Id = 1,
+				Name = "Name Name",
+				CreatedAt = new DateTime(2022, 02, 05),
+				UpdatedAt = new DateTime(2022, 02, 05)
+			};
+
+			// act
+			var model = NtiMappers.ToServiceModel(dto);
+
+			// assert
+			Assert.That(model, Is.Not.Null);
+			Assert.That(model.Id, Is.EqualTo(dto.Id));
+			Assert.That(model.Name, Is.EqualTo(dto.Name));
+		}
+
+		[Test]
+		public void NtiStatus_Dto_To_ServiceModel()
+		{
+			// arrange
+			var dto = new NtiStatusDto
+			{
+				Id = 1,
+				Name = "Name Name",
+				CreatedAt = new DateTime(2022, 02, 05),
+				UpdatedAt = new DateTime(2022, 02, 05)
+			};
+
+			// act
+			var model = NtiMappers.ToServiceModel(dto);
+
+			// assert
+			Assert.That(model, Is.Not.Null);
+			Assert.That(model.Id, Is.EqualTo(dto.Id));
+			Assert.That(model.Name, Is.EqualTo(dto.Name));
+		}
+	}
+}
---
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Case/Management/Action/Nti/AddPageModelTests.cs
@@ -0,0 +1,114 @@
+﻿using ConcernsCaseWork.Pages.Case.Management.Action.Nti;
+using ConcernsCaseWork.Services.Cases;
+using ConcernsCaseWork.Services.Nti;
+using ConcernsCaseWork.Shared.Tests.Factory;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using NUnit.Framework;
+using Service.Redis.Nti;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConcernsCaseWork.Tests.Pages.Case.Management.Action.Nti
+{
+	[Parallelizable(ParallelScope.All)]
+	public class AddPageModelTests
+	{
+		[Test]
+		public async Task WhenOnGetAsync_MissingCaseUrn_ThrowsException_ReturnPage()
+		{
+			// arrange
+
+			var mockNtiModelService = new Mock<INtiModelService>();
+			var mockNtiReasonsService = new Mock<INtiReasonsCachedService>();
+			var mockNtiStatusesService = new Mock<INtiStatusesCachedService>();
+			var mockLogger = new Mock<ILogger<AddPageModel>>();
+
+			var pageModel = SetupAddPageModel(mockNtiModelService, mockNtiReasonsService, mockNtiStatusesService, mockLogger);
+
+			// act
+			await pageModel.OnGetAsync();
+
+			// assert
+			Assert.That(pageModel.TempData["Error.Message"], Is.EqualTo("An error occurred loading the page, please try again. If the error persists contact the service administrator."));
+		}
+
+		[Test]
+		public async Task WhenOnGetAsync_ReturnsPageModel()
+		{
+			// arrange
+			var mockNtiModelService = new Mock<INtiModelService>();
+			var mockNtiReasonsService = new Mock<INtiReasonsCachedService>();
+			var mockNtiStatusesService = new Mock<INtiStatusesCachedService>();
+			var mockLogger = new Mock<ILogger<AddPageModel>>();
+
+			var pageModel = SetupAddPageModel(mockNtiModelService, mockNtiReasonsService, mockNtiStatusesService, mockLogger);
+
+			var routeData = pageModel.RouteData.Values;
+			routeData.Add("urn", 1);
+
+			// act
+			await pageModel.OnGetAsync();
+
+			// assert
+			mockLogger.Verify(
+				m => m.Log(
+					LogLevel.Information,
+					It.IsAny<EventId>(),
+					It.Is<It.IsAnyType>((v, _) => v.ToString().Contains("Case::Action::NTI::AddPageModel::OnGetAsync")),
+					null,
+					It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+				Times.Once);
+		}
+
+		[Test]
+		public async Task WhenOnPostAsync_MissingRouteData_ThrowsException_ReturnsPage()
+		{
+			// arrange
+			var mockNtiModelService = new Mock<INtiModelService>();
+			var mockNtiReasonsService = new Mock<INtiReasonsCachedService>();
+			var mockNtiStatusesService = new Mock<INtiStatusesCachedService>();
+			var mockLogger = new Mock<ILogger<AddPageModel>>();
+
+			var pageModel = SetupAddPageModel(mockNtiModelService, mockNtiReasonsService, mockNtiStatusesService, mockLogger);
+
+			// act
+			var pageResponse = await pageModel.OnPostAsync(String.Empty);
+
+			// assert
+			Assert.That(pageResponse, Is.InstanceOf<PageResult>());
+			var page = pageResponse as PageResult;
+
+			Assert.That(page, Is.Not.Null);
+			Assert.That(pageModel.TempData, Is.Not.Null);
+			Assert.That(pageModel.TempData["Error.Message"], Is.EqualTo("An error occurred posting the form, please try again. If the error persists contact the service administrator."));
+		}
+
+		private static AddPageModel SetupAddPageModel(Mock<INtiModelService> mockNtiModelService,
+			Mock<INtiReasonsCachedService> mockNtiReasonsCachedService,
+			Mock<INtiStatusesCachedService> mockNtiStatusesCachedService,
+			Mock<ILogger<AddPageModel>> mockLogger,
+			bool isAuthenticated = false)
+		{
+			(PageContext pageContext, TempDataDictionary tempData, ActionContext actionContext) = PageContextFactory.PageContextBuilder(isAuthenticated);
+
+			return new AddPageModel(mockNtiStatusesCachedService.Object, mockNtiReasonsCachedService.Object,
+				mockNtiModelService.Object, mockLogger.Object)
+			{
+				PageContext = pageContext,
+				TempData = tempData,
+				Url = new UrlHelper(actionContext),
+				MetadataProvider = pageContext.ViewData.ModelMetadata
+			};
+		}
+	}
+
+
+}
