8760cf16f58f94782d3cd1f675ac7e022095d39b
athomasdfe <97464316+athomasdfe@users.noreply.github.com>
2022-09-01 00:45:31 +0100

    Refactor and new tests
    **What is the change?**
    Fixes for change impact
    Implemented changes to support API testing
    Lots of abstraction from test specs into page classes
    Adds Finance Plan tests
    Adds Means of referral tests
    Adds NTI tests
    
    **Why do we need the change?**
    Increase E2E test coverage
    Fix pipeline
    
    **What is the impact?**
    (very high impact)
    
    **Azure DevOps Ticket**
    AB#104705

---
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/pages/caseActions/srmaEditPage.js
@@ -0,0 +1,76 @@
+import caseActionsBase from "/cypress/pages/caseActions/caseActionsBasePage";
+
+class SRMAEditPage {
+
+
+    constructor() {
+        //this.something = 
+        this.arrDate =[];
+    }
+
+
+    //locators
+    //SRMAAddPage.getDateOfferedDay();
+
+    getHeadingText() {
+        return     cy.get('h1[class="govuk-heading-l"]');
+    }
+
+    getSubHeadingText() {
+        return     cy.get('h2[class="govuk-heading-m"]');
+    }
+
+    //SRMA TABLE
+    //
+    getSrmaTable() {
+        return    cy.get('[class="govuk-table__cell"]', { timeout: 30000 });
+    }
+
+    getSrmaTableRow() {
+        return caseActionsBase.getTableRow();
+    }
+
+    getSrmaTableRowEmpty() {
+       // return    cy.get('.govuk-tag.ragtag.ragtag__grey', { timeout: 30000 });
+        return    cy.get('tr.govuk-table__row', { timeout: 30000 }).contains('Empty');
+    }
+
+    getNotesInfo() {
+        return    cy.get('[id="srma-notes-info"]', { timeout: 30000 });
+    }
+
+    getTableAddEditLink() {
+        return    cy.get('[class="govuk-link"]', { timeout: 30000 });
+    }
+
+    //Option accepts the following args: DfESupport | FinancialForecast | FinancialPlan | FinancialReturns |
+    //FinancialSupport| ForcedTermination | Nti| RecoveryPlan | Srma | Tff |
+    getCaseActionRadio(option) {
+        return     cy.get('[value="'+option+'"]');
+    }   
+    
+    getNotesBox() {
+        return    cy.get('[id="srma-notes"]', { timeout: 30000 });
+    }
+    
+    getNotesInfo() {
+        return    cy.get('[id="srma-notes-info"]', { timeout: 30000 });
+    }
+
+
+    //Methods
+
+    checkForTableEntry() {
+
+        //let $elem = Cypress.$('.govuk-list.govuk-error-summary__list');
+        let $elem = Cypress.$('.govuk-tag.ragtag.ragtag__grey');
+        cy.log(($elem).length)
+        
+        return ($elem.length);
+    }
+
+        
+}
+
+    export default new SRMAEditPage();
+
