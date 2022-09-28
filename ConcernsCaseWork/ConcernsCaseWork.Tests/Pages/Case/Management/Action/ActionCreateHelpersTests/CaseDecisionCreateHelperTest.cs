626b9a537f0bf1fd344e06c2bd0b39323d4cc512
aremusoftware <aremusoftware>
2022-09-28 15:30:28 +0100

    Add to unit tests

--- ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Case/Management/Action/ActionCreateHelpersTests/CaseDecisionCreateHelperTest.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Case/Management/Action/ActionCreateHelpersTests/CaseDecisionCreateHelperTest.cs
@@ -21,17 +21,14 @@ namespace ConcernsCaseWork.Tests.Pages.Case.Management.Action.ActionCreateHelper
 
 
 		[Test]
-		[TestCase(CaseActionEnum.Decision, true)]
-		[TestCase(CaseActionEnum.FinancialPlan, true)]
-		[TestCase(CaseActionEnum.Srma, true)]
-		public void CaseDecisionCreateHelper_CanHandle_ResponseCorrectly(CaseActionEnum action, bool expectedResult)
+		public void CaseDecisionCreateHelper_CanHandle_ResponseCorrectly([Values]CaseActionEnum action)
 		{
 			var builder = new TestBuilder();
 			var sut = builder.BuildSut();
 
 			var result = sut.CanHandle(action);
 
-			Assert.That(result, Is.EqualTo(expectedResult));
+			Assert.That(result, Is.EqualTo(true));
 		}
 
 
