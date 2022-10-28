59aa885a89c3c9d3b8d209d7dd2843fcbedc73ee
Emma Whitcroft <109589232+emma-w-nimble@users.noreply.github.com>
2022-10-28 11:16:22 +0100

    Add edit page checks to srma edit pages - redirects to closed page if the srma is open and a user tries to access an editable page

--- ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/SRMA/Edit/DateAccepted.cshtml.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/SRMA/Edit/DateAccepted.cshtml.cs
@@ -37,6 +37,11 @@ namespace ConcernsCaseWork.Pages.Case.Management.Action.SRMA
 				(caseUrn, srmaId) = GetRouteData();
 
 				SRMAModel = await _srmaModelService.GetSRMAById(srmaId);
+				
+				if (SRMAModel.IsClosed)
+				{
+					return Redirect($"/case/{caseUrn}/management/action/srma/{srmaId}/closed");
+				}
 			}
 			catch (Exception ex)
 			{
