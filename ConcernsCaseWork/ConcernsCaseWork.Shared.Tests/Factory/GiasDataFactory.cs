f49ca59c29956dd91b34e5e3a52e656778e24071
Emma Whitcroft <109589232+emma-w-nimble@users.noreply.github.com>
2022-11-18 16:07:59 +0000

    Fix missing trust type on overview page (#484)

--- ConcernsCaseWork/ConcernsCaseWork.Shared.Tests/Factory/GiasDataFactory.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Shared.Tests/Factory/GiasDataFactory.cs
@@ -6,26 +6,28 @@ namespace ConcernsCaseWork.Shared.Tests.Factory
 {
 	public static class GiasDataFactory
 	{
-		private readonly static Fixture Fixture = new Fixture();
+		private readonly static Fixture _fixture = new Fixture();
 		
 		public static GiasDataDto BuildGiasDataDto(string ukPrn = "trust-ukprn")
 		{
 			return new GiasDataDto(ukPrn, 
-				Fixture.Create<string>(), 
-				Fixture.Create<string>(), 
-				Fixture.Create<string>(), 
-				Fixture.Create<string>(),
-				GroupContactAddressFactory.BuildGroupContactAddressDto());
+				_fixture.Create<string>(), 
+				_fixture.Create<string>(), 
+				_fixture.Create<string>(), 
+				_fixture.Create<string>(),
+				GroupContactAddressFactory.BuildGroupContactAddressDto(),
+				_fixture.Create<string>());
 		}
 		
 		public static GiasDataModel BuildGiasDataModel()
 		{
-			return new GiasDataModel(Fixture.Create<string>(), 
-				Fixture.Create<string>(), 
-				Fixture.Create<string>(), 
-				Fixture.Create<string>(), 
-				Fixture.Create<string>(), 
-				GroupContactAddressFactory.BuildGroupContactAddressModel());
+			return new GiasDataModel(_fixture.Create<string>(), 
+				_fixture.Create<string>(), 
+				_fixture.Create<string>(), 
+				_fixture.Create<string>(), 
+				_fixture.Create<string>(), 
+				GroupContactAddressFactory.BuildGroupContactAddressModel(),
+				_fixture.Create<string>());
 		}
 	}
 }
