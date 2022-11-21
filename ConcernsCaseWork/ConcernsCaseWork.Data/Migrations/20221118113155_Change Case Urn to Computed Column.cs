6dba163a9696032f17bd6c0110cc7a46c12283ad
Emma Whitcroft <109589232+emma-w-nimble@users.noreply.github.com>
2022-11-21 10:10:30 +0000

    Feature/107054 implement sql lite (#483)
    * Update ConcernsCase to make Urn a computed column, so we can do away with the Global sequence, which is not required and is blocking the use of sql lite (or similar) for integration tests.
    
    * Add integration test project for the Data project, using sql lite. This is a starter for ten to be built on - we could do with checking it will run in the pipelines maybe before progressing too far with it.
    
    * Add missing reference to data.tests project
    
    Make concernscase.urn settable for tests :/

---
+++ ConcernsCaseWork/ConcernsCaseWork.Data/Migrations/20221118113155_Change Case Urn to Computed Column.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConcernsCaseWork.Data.Migrations
+{
+    public partial class ChangeCaseUrntoComputedColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropSequence(
+                name: "GlobalSequence",
+                schema: "concerns");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Urn",
+                schema: "concerns",
+                table: "ConcernsCase",
+                type: "int",
+                nullable: false,
+                computedColumnSql: "[Id]",
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValueSql: "NEXT VALUE FOR Concerns.GlobalSequence");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence<int>(
+                name: "GlobalSequence",
+                schema: "concerns",
+                minValue: 1L);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Urn",
+                schema: "concerns",
+                table: "ConcernsCase",
+                type: "int",
+                nullable: false,
+                defaultValueSql: "NEXT VALUE FOR Concerns.GlobalSequence",
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldComputedColumnSql: "[Id]");
+        }
+    }
+}
