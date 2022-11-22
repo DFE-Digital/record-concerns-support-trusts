48dab6261afd7628d4a95d1ccf968f9e1f4a5967
mikestock-nimble <mike.stock@nimbleapproach.com>
2022-11-22 16:27:44 +0000

    defining the contract for the decision outcome apis

---
+++ ConcernsCaseWork/ConcernsCaseWork.API.Contracts/Decisions/Outcomes/DecisionOutcomeAuthorizer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcernsCaseWork.API.Contracts.Decisions.Outcomes
+{
+	public enum DecisionOutcomeAuthorizer
+	{
+		G7 = 1,
+		G6 = 2,
+		RegionalSchoolsCommissioner = 3,
+		DeputyDirector = 4,
+		CountersigningDeputyDirector = 5,
+		Director= 6,
+		Minister = 7
+	}
+}
