b6c3903c669520eace7a97122b1c4382aa69fb48
Chris Wright <chris@dxw.com>
2022-11-23 12:42:31 +0000

    Bump container-apps-hosting module to v0.9.0
    * Sets Redis to version 6 by default

--- terraform.gpaas-azure-migration/container-apps-hosting.tf
+++ terraform.gpaas-azure-migration/container-apps-hosting.tf
@@ -1,5 +1,5 @@
 module "azure_container_apps_hosting" {
-  source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v0.7.1"
+  source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v0.9.0"
 
   environment    = local.environment
   project_name   = local.project_name
