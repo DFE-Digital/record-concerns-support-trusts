37827acc0de32418451639f4edd554a49f83f60c
Emma Whitcroft <109589232+emma-w-nimble@users.noreply.github.com>
2022-11-25 10:13:47 +0000

    Add api key check to startup middleware and fix incorrect api key name (#496)
    * Add api key check to startup middleware and fix incorrect api key name
    
    * Fix incorrect api key name in swagger config
    
    Co-authored-by: Chris Dexter <109349584+chrisdexnimble@users.noreply.github.com>

--- ConcernsCaseWork/ConcernsCaseWork.API/Middleware/ApiKeyMiddleware.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API/Middleware/ApiKeyMiddleware.cs
@@ -5,30 +5,35 @@ namespace ConcernsCaseWork.API.Middleware
     public class ApiKeyMiddleware
     {
         private readonly RequestDelegate _next;
-        private const string APIKEYNAME = "ConcernsApiKey";
+        private const string APIKEYNAME = "ApiKey";
         public ApiKeyMiddleware(RequestDelegate next)
         {
             _next = next;
         }
         public async Task InvokeAsync(HttpContext context, IUseCase<string, bool> apiKeyValidationService)
         {
-            if (!context.Request.Headers.TryGetValue(APIKEYNAME, out var extractedApiKey))
-            {
-                context.Response.StatusCode = 401;
-                await context.Response.WriteAsync("Api Key was not provided.");
-                return;
-            }
- 
-            var isKeyValid = apiKeyValidationService.Execute(extractedApiKey);
-            
-            if (!isKeyValid)
-            {
-                context.Response.StatusCode = 401;
-                await context.Response.WriteAsync("Unauthorized client.");
-                return;
-            }
+	        if (IsApiCall(context))
+	        {
+		        if (!context.Request.Headers.TryGetValue(APIKEYNAME, out var extractedApiKey))
+		        {
+			        context.Response.StatusCode = 401;
+			        await context.Response.WriteAsync("Api Key was not provided.");
+			        return;
+		        }
 
-            await _next(context);
+		        var isKeyValid = apiKeyValidationService.Execute(extractedApiKey);
+
+		        if (!isKeyValid)
+		        {
+			        context.Response.StatusCode = 401;
+			        await context.Response.WriteAsync("Unauthorized client.");
+			        return;
+		        }
+	        }
+
+	        await _next(context);
         }
+
+        private bool IsApiCall(HttpContext context) => context.Request.Path.HasValue && context.Request.Path.Value!.StartsWith("/v2");
     }
 }
