8c3784011dda59cd464b4cfdb2766e29f65a0762
mikestock-nimble <mike.stock@nimbleapproach.com>
2022-12-06 12:39:33 +0000

    114364: updated test to share existing page object models
    check you cannot re-edit or close decisions

--- ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/pages/caseActions/decision/closeDecisionPage.ts
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/pages/caseActions/decision/closeDecisionPage.ts
@@ -1,14 +1,5 @@
 export class CloseDecisionPage
 {
- 
-	public withSupportingNotes(supportingNotes: string): this {
-		cy.task("log", `With Supporting Notes - add notes ${supportingNotes}`);
-
-		cy.getById("case-decision-notes").clear().type(supportingNotes);
-
-		return this;
-	}
-
     public withFinaliseSupportingNotes(finaliseSupportingNotes: string): this {
 		cy.task("log", `With Supporting Notes - add notes ${finaliseSupportingNotes}`);
 
@@ -30,11 +21,6 @@ export class CloseDecisionPage
 		return this;
 	}
 
-	public continueToCloseDecision(): this {
-		cy.get('#close-decision-button').click();
-
-		return this;
-	}
 
 	public closeDecision(): this {
 		cy.getByTestId('close-decision-button').click();
@@ -42,62 +28,7 @@ export class CloseDecisionPage
 		return this;
 	}
 
-	
-	public continueRecordDecisionOutcome(): this {
-		cy.getByTestId('continue-record-decision-button').click();
-
-		return this;
-	}
-	
-	public saveAndContinueOutcome(): this {
-		cy.getByTestId('add-decision-outcome-button').click();
-
-
-
-		return this;
-	}
-	
-		public selectDecisionOutcome(): this {
-		cy.getByTestId('Approved').click();
-
-		return this;
-	}
-	
-	public selectCreatedDecision(): this {
-		cy.get("#open-case-actions td")
-		.should("contain.text", "Decision: No Decision Types")
-		.eq(-3)
-		.children("a")
-		.click();
-
-		return this;
-	}
-	
-	
-
-
-	public selectClosedActionDecision(): this {
-		cy.get("#close-case-actions td")
-	.should("contain.text", "Decision: No Decision Types")
-	.eq(-4)
-	.children("a")
-	.click();
-
-		return this;
-	}
-	
 	public hasValidationError(message: string): this {
-		cy.task("log", `Has Validation error ${message}`);
-
-		cy.get("#decision-error-list").should(
-			"contain.text",
-			message
-		);
-
-		return this;
-	}
-
-	public hasValidationErrorForNotes(message: string): this {
 		cy.task("log", `Has Validation error ${message}`);
 
 		cy.get("#errorSummary").should(
@@ -107,19 +38,4 @@ export class CloseDecisionPage
 
 		return this;
 	}
-
-	
-  
-
-    public hasSupportingNotes(supportingNotes: string): this {
-		cy.task("log", `Has Supporting Notes ${supportingNotes}`);
-
-		cy.getByTestId("supporting-notes-text").should(
-			"contain.text",
-			supportingNotes
-		);
-
-		return this;
-	}
-	
 }
