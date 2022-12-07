8dae8d4295fe7b4abe6062e72beb2ae05be87a2c
mikestock-nimble <mike.stock@nimbleapproach.com>
2022-12-07 15:11:39 +0000

    115081: generated a migration for the updated decision authorizer text

---
+++ ConcernsCaseWork/ConcernsCaseWork.Data/Migrations/20221207151013_DecisionAuthorizerText.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConcernsCaseWork.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DecisionAuthorizerText : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "concerns",
+                table: "DecisionOutcomeAuthorizer",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "RegionalDirector");
+
+            migrationBuilder.UpdateData(
+                schema: "concerns",
+                table: "DecisionOutcomeAuthorizer",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "CounterSigningDeputyDirector");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "concerns",
+                table: "DecisionOutcomeAuthorizer",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "RegionalSchoolsCommissioner");
+
+            migrationBuilder.UpdateData(
+                schema: "concerns",
+                table: "DecisionOutcomeAuthorizer",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "CountersigningDeputyDirector");
+        }
+    }
+}
