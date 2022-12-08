fbf806f30a881adafdd224f6e17196447801d65b
mikestock-nimble <mike.stock@nimbleapproach.com>
2022-12-08 10:48:16 +0000

    fixing the script

--- ConcernsCaseWork/setup-certs.sh
+++ ConcernsCaseWork/setup-certs.sh
@@ -22,11 +22,11 @@ cp localhost.crt ~/.mozilla/certificates/aspnetcore-localhost-https.crt
 
 echo "Setting up chrome"
 # Trust Edge/Chrome
-mkdir -p sql:$HOME/.pki/nssdb
 certutil -d sql:$HOME/.pki/nssdb -N
 certutil -d sql:$HOME/.pki/nssdb -A -t "P,," -n localhost -i ./localhost.crt
 certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n localhost -i ./localhost.crt
 
+echo "setting up .net trust"
 # Trust dotnet-to-dotnet (.pem extension is important here)
 sudo cp localhost.crt /usr/lib/ssl/certs/aspnetcore-https-localhost.pem
 
