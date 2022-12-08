59a3afb4685b0e6b8a883cf498bf8887ce0033d1
mikestock-nimble <mike.stock@nimbleapproach.com>
2022-12-08 10:55:18 +0000

    more debugging the script

--- ConcernsCaseWork/setup-certs.sh
+++ ConcernsCaseWork/setup-certs.sh
@@ -20,15 +20,16 @@ sudo mv policies.json /usr/lib/firefox/distribution/
 mkdir -p ~/.mozilla/certificates
 cp localhost.crt ~/.mozilla/certificates/aspnetcore-localhost-https.crt
 
-echo "Setting up chrome "
+echo "Setting up chrome"
 # Trust Edge/Chrome
 certutil -d sql:$HOME/.pki/nssdb -N
-certutil -d sql:$HOME/.pki/nssdb -A -t "P,," -n localhost -i ./localhost.crt
-certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n localhost -i ./localhost.crt
+#certutil -d sql:$HOME/.pki/nssdb -A -t "P,," -n localhost -i ./localhost.crt
+#certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n localhost -i ./localhost.crt
 
 echo "setting up .net trust"
 # Trust dotnet-to-dotnet (.pem extension is important here)
 sudo cp localhost.crt /usr/lib/ssl/certs/aspnetcore-https-localhost.pem
 
 # Cleanup
+echo "Cleanup"
 rm localhost.crt
