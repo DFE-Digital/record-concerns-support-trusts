35e5bc22e8d74c869c0bf253bf69123890838130
Chris Dexter <109349584+chrisdexnimble@users.noreply.github.com>
2022-12-12 17:55:06 +0000

    validation on select trust and select case type for non-concerns

--- ConcernsCaseWork/ConcernsCaseWork/Models/FindTrustModel.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Models/FindTrustModel.cs
@@ -1,9 +1,11 @@
 using System.ComponentModel.DataAnnotations;
 
+namespace ConcernsCaseWork.Models;
+
 public class FindTrustModel
 {
 	public string Nonce { get; set; }
 
-	[Required(ErrorMessage = "A trust is required")]
+	[Required(ErrorMessage = "A trust is required", AllowEmptyStrings = false)]
 	public string SelectedTrustUkprn { get; set; }
 }
