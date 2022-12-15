06c73c462d9cc762e2a763b7fd1334f21b3b50a8
mikestock-nimble <mike.stock@nimbleapproach.com>
2022-12-15 09:08:23 +0000

    adding logs to puppeteer

--- ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/plugins/azureLogin.js
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/plugins/azureLogin.js
@@ -5,6 +5,7 @@ const puppeteer = require('puppeteer');
 // Use puppeteer to log in to azure AD
 // Currently cypress has a problem loading the login page
 module.exports.azureLogin = async function azureLogin(url, username, password) {
+    console.log("Launching browser");
     const browser = await puppeteer.launch({
         headless: false,
         ignoreHTTPSErrors: true,
@@ -15,6 +16,7 @@ module.exports.azureLogin = async function azureLogin(url, username, password) {
 
     const [page] = await browser.pages();
 
+    console.log("Navigating to Azure AD")
     await page.goto(url);
 
     const submitSelector = "input[type=submit]";
@@ -22,27 +24,34 @@ module.exports.azureLogin = async function azureLogin(url, username, password) {
     const passwordSelector = "input[name=passwd]";
     const timeout = 2000;
 
+    console.log("Entering username");
     await page.waitForSelector(usernameSelector);
     await page.waitForTimeout(timeout);
     await page.type(usernameSelector, username, { delay: 50 });
     await page.click(submitSelector);
 
+    console.log("Entering password");
     await page.waitForSelector(passwordSelector);
     await page.waitForTimeout(timeout);
     await page.type(passwordSelector, password, { delay: 50 });
     await page.click(submitSelector);
 
+    console.log("Selecting stay signed in")
     // // Stay signed in
     await page.waitForTimeout(timeout);
     await page.click(submitSelector);
 
+    console.log("Waiting for website header");
     await page.waitForSelector(".moj-header__logo");
 
+    console.log("Storing cookie")
     const cookies = await page.cookies();
 
     const loginCookie = cookies.find(c => c.name === ".ConcernsCasework.Login");
 
+    console.log("Closing browser");
     await browser.close();
 
+    console.log("Returning cookie!");
     return loginCookie.value;
 }
