8d9886556a3d3a9aed402515ada99974b3a3e084
mikestock-nimble <mike.stock@nimbleapproach.com>
2022-12-15 10:35:43 +0000

    handling puppeteer login

--- ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/plugins/azureLogin.js
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/plugins/azureLogin.js
@@ -16,57 +16,60 @@ module.exports.azureLogin = async function azureLogin(url, username, password) {
           ]
     });
 
-    const usernameExists = username.length > 0;
-    const passwordExists = password.length > 0;
-    console.log("Username exists " + usernameExists);
-    console.log("Password exists " + passwordExists);
-
-
     const [page] = await browser.pages();
 
-    console.log("Navigating to Azure AD")
-    await page.goto(url);
-
-    const submitSelector = "input[type=submit]";
-    const usernameSelector = "input[name=loginfmt]";
-    const passwordSelector = "input[name=passwd]";
-    const timeout = 2000;
-
-    await page.waitForSelector(usernameSelector, { visible: true });
-    await page.waitForTimeout(timeout);
-
-    await page.type(usernameSelector, username, { delay: 50 });
-    await page.click(submitSelector);
-
-    console.log("Entering password");
-    await page.waitForSelector(passwordSelector);
-    await page.waitForTimeout(timeout);
-    await page.type(passwordSelector, password, { delay: 50 });
-    await page.click(submitSelector);
-
-    await page.waitForTimeout(10000);
-
-    await page.screenshot(
+    try
     {
-        path: join(__dirname, "../screenshots/capture.jpg")
-    });
+        console.log("Navigating to Azure AD")
+        await page.goto(url);
+    
+        const submitSelector = "input[type=submit]";
+        const usernameSelector = "input[name=loginfmt]";
+        const passwordSelector = "input[name=passwd]";
+        const timeout = 2000;
+    
+        await page.waitForSelector(usernameSelector, { visible: true });
+        await page.waitForTimeout(timeout);
+    
+        await page.type(usernameSelector, username, { delay: 50 });
+        await page.click(submitSelector);
+    
+        console.log("Entering password");
+        await page.waitForSelector(passwordSelector);
+        await page.waitForTimeout(timeout);
+        await page.type(passwordSelector, password, { delay: 50 });
+        await page.click(submitSelector);
+    
+        console.log("Selecting stay signed in")
+        // // Stay signed in
+        await page.waitForTimeout(timeout);
+        await page.click(submitSelector);
+    
+        console.log("Waiting for website header");
+        await page.waitForSelector(".moj-header__logo");
+    
+        console.log("Storing cookie")
+        const cookies = await page.cookies();
+    
+        const loginCookie = cookies.find(c => c.name === ".ConcernsCasework.Login");
+    
+        console.log("Closing browser");
+        await browser.close();
+    
+        console.log("Returning cookie!");
+        return loginCookie.value;
+    }
+    catch (ex)
+    {
+        console.log("Capturing failure");
+        await page.screenshot(
+        {
+            path: join(__dirname, "../screenshots/capture.jpg")
+        });
 
-    console.log("Selecting stay signed in")
-    // // Stay signed in
-    await page.waitForTimeout(timeout);
-    await page.click(submitSelector);
+        console.log("Closing browser");
+        await browser.close();
 
-    console.log("Waiting for website header");
-    await page.waitForSelector(".moj-header__logo");
-
-    console.log("Storing cookie")
-    const cookies = await page.cookies();
-
-    const loginCookie = cookies.find(c => c.name === ".ConcernsCasework.Login");
-
-    console.log("Closing browser");
-    await browser.close();
-
-    console.log("Returning cookie!");
-    return loginCookie.value;
+        throw ex;
+    }
 }
