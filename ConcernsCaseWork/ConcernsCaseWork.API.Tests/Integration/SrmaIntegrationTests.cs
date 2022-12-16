15fd6506667a7656e70d80e680e5d009828d78e4
mikestock-nimble <mike.stock@nimbleapproach.com>
2022-12-16 12:32:03 +0000

    115186: added validation tests to the case action post and patch endpoints

---
+++ ConcernsCaseWork/ConcernsCaseWork.API.Tests/Integration/SrmaIntegrationTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoFixture;
+using ConcernsCaseWork.API.RequestModels.CaseActions.NTI.WarningLetter;
+using ConcernsCaseWork.API.RequestModels.CaseActions.SRMA;
+using ConcernsCaseWork.API.Tests.Fixtures;
+using ConcernsCaseWork.API.Tests.Helpers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConcernsCaseWork.API.Tests.Integration
+{
+	public class SrmaIntegrationTests : IClassFixture<ApiTestFixture>
+	{
+		private Fixture _fixture;
+		private HttpClient _client;
+
+		public SrmaIntegrationTests(ApiTestFixture apiTestFixture)
+		{
+			_client = apiTestFixture.Client;
+			_fixture = new();
+		}
+
+		[Fact]
+		public async Task When_Post_InvalidRequest_Returns_ValidationErrors()
+		{
+			var request = _fixture.Create<CreateSRMARequest>();
+			request.CreatedBy = new string('a', 301);
+			request.Notes = new string('a', 2001);
+
+			var result = await _client.PostAsync($"/v2/case-actions/nti-warning-letter", request.ConvertToJson());
+			result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+
+			var error = await result.Content.ReadAsStringAsync();
+			error.Should().Contain("The field CreatedBy must be a string with a maximum length of 300.");
+			error.Should().Contain("The field Notes must be a string with a maximum length of 2000.");
+		}
+	}
+}
