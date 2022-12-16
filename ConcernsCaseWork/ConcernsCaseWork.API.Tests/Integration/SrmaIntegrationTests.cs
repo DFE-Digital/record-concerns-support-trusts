e77b7e6a0fb169e3bc6804f27dba1b3ec63d30e0
mikestock-nimble <mike.stock@nimbleapproach.com>
2022-12-16 12:50:30 +0000

    115186: added limit to srma notes api

--- ConcernsCaseWork/ConcernsCaseWork.API.Tests/Integration/SrmaIntegrationTests.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API.Tests/Integration/SrmaIntegrationTests.cs
@@ -1,15 +1,10 @@
 ﻿using AutoFixture;
-using ConcernsCaseWork.API.RequestModels.CaseActions.NTI.WarningLetter;
 using ConcernsCaseWork.API.RequestModels.CaseActions.SRMA;
 using ConcernsCaseWork.API.Tests.Fixtures;
 using ConcernsCaseWork.API.Tests.Helpers;
 using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -33,13 +28,26 @@ namespace ConcernsCaseWork.API.Tests.Integration
 			request.CreatedBy = new string('a', 301);
 			request.Notes = new string('a', 2001);
 
-			var result = await _client.PostAsync($"/v2/case-actions/nti-warning-letter", request.ConvertToJson());
+			var result = await _client.PostAsync($"/v2/case-actions/srma", request.ConvertToJson());
 			result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
 
 
 			var error = await result.Content.ReadAsStringAsync();
 			error.Should().Contain("The field CreatedBy must be a string with a maximum length of 300.");
 			error.Should().Contain("The field Notes must be a string with a maximum length of 2000.");
+		}
+
+		[Fact]
+		public async Task When_Patch_InvalidNotes_Returns_ValidationErrors()
+		{
+			var notes = new string('a', 2001);
+
+			var result = await _client.PatchAsync($"/v2/case-actions/srma/1/update-notes?notes={notes}", null);
+			result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+			var error = await result.Content.ReadAsStringAsync();
+			error.Should().Contain("The field notes must be a string with a maximum length of 2000.");
+
 		}
 	}
 }
