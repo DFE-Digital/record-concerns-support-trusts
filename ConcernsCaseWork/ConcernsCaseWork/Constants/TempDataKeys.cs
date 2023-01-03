79992445df912fc0c3036d492aab9c3ba952a902
Emma Whitcroft <109589232+emma-w-nimble@users.noreply.github.com>
2023-01-03 11:27:10 +0000

    Remove unused class

--- ConcernsCaseWork/ConcernsCaseWork/Constants/TempDataKeys.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.Routing;
-using System.Security.Policy;
-
-namespace ConcernsCaseWork.Constants;
-
-public static class TempDataKeys
-{
-	public static string ReferrerUrl => "ReferrerUrl";
-}
