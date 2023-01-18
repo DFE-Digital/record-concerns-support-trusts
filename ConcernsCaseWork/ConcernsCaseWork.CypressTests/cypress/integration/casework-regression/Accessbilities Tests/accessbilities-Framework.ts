5d459ee0e47f7fd224eaa5417219e9f2673f3382
FahadDarw <101198418+FahadDarw@users.noreply.github.com>
2023-01-18 09:36:20 +0000

    Update accessbilities-Framework.ts

--- ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/integration/casework-regression/Accessbilities Tests/accessbilities-Framework.ts
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/integration/casework-regression/Accessbilities Tests/accessbilities-Framework.ts
@@ -1,15 +1,13 @@
 /// <reference types ='Cypress'/>
 import accessibilitiesTestPages from '../../../fixtures/accessibilitiesTestPages.json'
-const wcagStandards = [ "wcag22aa"];
-const impactLevel = ["critical", "minor", "moderate", "serious"];
-const continueOnFail = false;
+
 
 	describe('Check accessibility of the different pages', function () {
 		beforeEach(() => {
 			cy.login();
 		});
         accessibilitiesTestPages.forEach((link) => {
-         it('Validate accessibility on different pages', function () {
+         it(`Validate accessibility on ${link}`, function () {
             let url = Cypress.env('url')
             cy.visit(url)
             cy.visit(url+link)
