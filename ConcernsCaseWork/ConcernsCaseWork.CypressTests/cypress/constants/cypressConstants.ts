65021c72284ec58c5851bbb203abd1d2e36b17d9
Chris Dexter <109349584+chrisdexnimble@users.noreply.github.com>
2023-01-23 17:45:09 +0000

    Added name and role to the headers sent

--- ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/constants/cypressConstants.ts
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/constants/cypressConstants.ts
@@ -1,3 +1,6 @@
 export const EnvApi = "api";
 export const EnvApiKey = "apiKey";
 export const EnvUsername = "username";
+export const CaseworkerClaim = "concerns-casework.caseworker";
+export const TeamLeaderClaim = "concerns-casework.teamleader";
+export const AdminClaim = "concerns-casework.admin";
