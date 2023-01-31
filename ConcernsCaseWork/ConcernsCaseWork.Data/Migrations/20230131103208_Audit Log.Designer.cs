6989cf9f537296cefc3757911b7a5aa7c79306e9
Emma Whitcroft <109589232+emma-w-nimble@users.noreply.github.com>
2023-01-31 10:35:57 +0000

    Add audit table and implement override to save changes to populate it

--- ConcernsCaseWork/ConcernsCaseWork.Data/Migrations/20230131103208_Audit Log.Designer.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Data/Migrations/20230131103208_Audit Log.Designer.cs
@@ -4,6 +4,7 @@ using ConcernsCaseWork.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ConcernsCaseWork.Data.Migrations
 {
     [DbContext(typeof(ConcernsDbContext))]
-    partial class ConcernsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230131103208_Audit Log")]
+    partial class AuditLog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
