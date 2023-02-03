863c944a3484c5c14ba73998e0577a30adb38cc8
Chris Dexter <109349584+chrisdexnimble@users.noreply.github.com>
2023-02-03 13:22:12 +0000

    fixing tests so that they provide user context and correlation id

--- ConcernsCaseWork/ConcernsCaseWork.API.Tests/Fixtures/ApiTestFixture.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API.Tests/Fixtures/ApiTestFixture.cs
@@ -1,12 +1,18 @@
 ﻿using ConcernsCaseWork.Data;
+using ConcernsCaseWork.Logging;
+using ConcernsCaseWork.Service.Base;
+using ConcernsCaseWork.UserContext;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Moq;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Net.Http;
+using System.Net.Mime;
 using Xunit;
 
 namespace ConcernsCaseWork.API.Tests.Fixtures
@@ -15,9 +21,11 @@ namespace ConcernsCaseWork.API.Tests.Fixtures
 	{
 		private readonly WebApplicationFactory<Startup> _application;
 
-		public HttpClient Client { get; private set; }
+		public HttpClient Client { get; init; }
 
 		private DbContextOptions<ConcernsDbContext> _dbContextOptions { get; init; }
+
+		private ServerUserInfoService _serverUserInfoService { get; init; }
 
 		private static readonly object _lock = new();
 		private static bool _isInitialised = false;
@@ -51,8 +59,11 @@ namespace ConcernsCaseWork.API.Tests.Fixtures
 							});
 						});
 
-					Client = _application.CreateClient();
-					Client.DefaultRequestHeaders.Add("ApiKey", "app-key");
+					var fakeUserInfo = new UserInfo()
+						{ Name = "API.TestFixture@test.gov.uk", Roles = new[] { Claims.CaseWorkerRoleClaim } };
+					_serverUserInfoService = new ServerUserInfoService() { UserInfo = fakeUserInfo };
+
+					Client = CreateHttpClient(fakeUserInfo);
 
 					_dbContextOptions = new DbContextOptionsBuilder<ConcernsDbContext>()
 						.UseSqlServer(connectionString)
@@ -64,6 +75,25 @@ namespace ConcernsCaseWork.API.Tests.Fixtures
 					_isInitialised = true;
 				}
 			}
+		}
+
+		private HttpClient CreateHttpClient(UserInfo userInfo)
+		{
+			var client = _application.CreateClient();
+			client.DefaultRequestHeaders.Add("ApiKey", "app-key");
+			client.DefaultRequestHeaders.Add("ContentType", MediaTypeNames.Application.Json);
+
+			// add standard headers for correlation and user context.
+			var clientUserInfoService = new ClientUserInfoService();
+			clientUserInfoService.SetPrincipal(userInfo);
+			clientUserInfoService.AddUserInfoRequestHeaders(client);
+
+			var correlationContext = new CorrelationContext();
+			correlationContext.SetContext(Guid.NewGuid().ToString());
+
+			AbstractService.AddDefaultRequestHeaders(client, correlationContext, clientUserInfoService, null);
+
+			return client;
 		}
 
 		private static string BuildDatabaseConnectionString(IConfigurationBuilder config)
@@ -78,7 +108,7 @@ namespace ConcernsCaseWork.API.Tests.Fixtures
 			return result;
 		}
 
-		public ConcernsDbContext GetContext() => new ConcernsDbContext(_dbContextOptions);
+		public ConcernsDbContext GetContext() => new ConcernsDbContext(_dbContextOptions, _serverUserInfoService);
 
 		public void Dispose()
 		{
--- ConcernsCaseWork/ConcernsCaseWork.API/Program.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API/Program.cs
@@ -2,17 +2,22 @@ namespace ConcernsCaseWork.API
 {
 	public class Program
 	{
+		/// <summary>
+		/// This is a bit misleading, the api doesn't actually host itself (though it probably should), so this method will never be invoked
+		/// </summary>
+		/// <param name="args"></param>
+
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
+			//CreateHostBuilder(args).Build().Run();
 		}
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureLogging(c => {
-					c.ClearProviders();
-					c.AddConsole();
-				})
-				.ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+		//public static IHostBuilder CreateHostBuilder(string[] args) =>
+		//	Host.CreateDefaultBuilder(args)
+		//		.ConfigureLogging(c => {
+		//			c.ClearProviders();
+		//			c.AddConsole();
+		//		})
+		//		.ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
 	}
 }
