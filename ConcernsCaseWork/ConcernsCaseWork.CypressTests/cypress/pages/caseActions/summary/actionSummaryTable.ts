12007dc29b0a638a7c619db9c2c37a2ceae40137
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-02-20 14:46:07 +0000

    121547: decision test improvements
    check the opened and closed date on view decision
    check the decision summary on the action summary table for closed and open decisions

---
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/pages/caseActions/summary/actionSummaryTable.ts
@@ -0,0 +1,36 @@
+import { Logger } from "cypress/common/logger";
+import { ActionSummaryRow } from "./actionSummaryRow";
+
+export class ActionSummaryTable {
+    public getOpenAction(value: string): Cypress.Chainable<ActionSummaryRow> {
+        Logger.Log(`Getting open action summary row ${value}`);
+
+        return cy.getById("open-case-actions")
+        .within(() =>
+        {
+            cy.getByTestId(`row-${value}`);
+        })
+        .then((el) =>
+        {
+            return new ActionSummaryRow(el);
+        });
+    }
+
+    public getClosedAction(value: string): Cypress.Chainable<ActionSummaryRow> {
+        Logger.Log(`Getting closed action summary row ${value}`);
+
+        return cy.getById("close-case-actions")
+        .within(() =>
+        {
+            cy.getByTestId(`row-${value}`);
+        })
+        .then((el) =>
+        {
+            return new ActionSummaryRow(el);
+        });
+    }
+}
+
+const actionSummaryTable = new ActionSummaryTable();
+
+export default actionSummaryTable;
