25679adf25d395283ad5716904c16d6333002fc7
Chris Dexter <109349584+chrisdexnimble@users.noreply.github.com>
2023-02-28 12:31:37 +0000

    Added a type for TrustUkprn which checks the valid range of values and rejects any that do not comply

---
+++ ConcernsCaseWork/ConcernsCaseWork.CoreTypes.Tests/TrustUkprnTests.cs
@@ -0,0 +1,39 @@
+﻿namespace ConcernsCaseWork.CoreTypes.Tests;
+
+public class TrustUkprnTests
+{
+	[Test]
+	public void CanCreateTrustUkprnTests()
+	{
+		var sut = new TrustUkprn(1);
+		Assert.That((long)sut, Is.EqualTo(1));
+	}
+
+	[Test]
+	public void Cannot_Create_Negative_TrustUkprn()
+	{
+		Assert.Throws<ArgumentException>(() => new TrustUkprn(-1));
+	}
+
+	[Test]
+	public void Can_Explicitly_Convert_Long_To_TrustUkprn()
+	{
+		long urnValue = 5;
+		var sut = (TrustUkprn)urnValue;
+		Assert.That((long)sut, Is.EqualTo(urnValue));
+	}
+
+	[Test]
+	public void Can_Implicitly_Convert_TrustUkprn_To_Long()
+	{
+		long result = new TrustUkprn(5);
+		Assert.That(result, Is.EqualTo(5));
+	}
+
+	[Test]
+	public void TrustUkprn_Must_Be_Between_10000000_And_19999999()
+	{
+		Assert.Throws<ArgumentOutOfRangeException>(() => new TrustUkprn(9999999));
+		Assert.Throws<ArgumentOutOfRangeException>(() => new TrustUkprn(20000000));
+	}
+}
---
+++ ConcernsCaseWork/ConcernsCaseWork.CoreTypes/TrustUkprn.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.GuardClauses;
+
+namespace ConcernsCaseWork.CoreTypes;
+
+public class TrustUkprn
+{
+	public TrustUkprn(long caseUrnValue)
+	{
+		Guard.Against.NegativeOrZero(caseUrnValue);
+		Guard.Against.OutOfRange(caseUrnValue, nameof(caseUrnValue), 10000000, 19999999);
+		Value = Guard.Against.NegativeOrZero(caseUrnValue);
+	}
+
+	private long Value { get; }
+
+	public static explicit operator TrustUkprn(long caseUrnValue) => new(caseUrnValue);
+	public static implicit operator long(TrustUkprn caseUrn) => caseUrn.Value;
+}
