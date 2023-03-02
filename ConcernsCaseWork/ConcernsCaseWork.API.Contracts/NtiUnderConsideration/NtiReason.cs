e70de1b556c449329f3faa2a671907abfd350deb
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-03-02 16:58:08 +0000

    112813: removing redis from nti under consideration
    remove the cached reason provider
    replace with an enum that does the same thing

---
+++ ConcernsCaseWork/ConcernsCaseWork.API.Contracts/NtiUnderConsideration/NtiReason.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace ConcernsCaseWork.API.Contracts.NtiUnderConsideration
+{
+	public enum NtiUnderConsiderationReason
+	{
+		[Description("Cash flow problems")]
+		CashFlowProblems = 1,
+		[Description("Cumulative deficit (actual)")]
+		CumulativeDeficitActual = 2,
+		[Description("Cumulative deficit (projected)")]
+		CumulativeDeficitProjected = 3,
+		[Description("Governance concerns")]
+		GovernanceConcerns = 4,
+		[Description("Non-Compliance with Academies Financial/Trust Handbook")]
+		NonComplianceWithAcademiesFinancialTrustHandbook = 5,
+		[Description("Non-Compliance with financial returns")]
+		NonComplianceWithFinancialReturns = 6,
+		[Description("Risk of insolvency")]
+		RiskOfInsolvency = 7,
+		[Description("Safeguarding")]
+		Safeguarding = 8
+	}
+}
