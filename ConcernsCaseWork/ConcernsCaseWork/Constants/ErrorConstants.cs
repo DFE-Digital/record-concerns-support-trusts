fcbc8f9217457613f15d6bc85dbe843b9ad51096
elielijah321 <45821029+elielijah321@users.noreply.github.com>
2023-03-08 12:18:45 +0000

    update error message

--- ConcernsCaseWork/ConcernsCaseWork/Constants/ErrorConstants.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Constants/ErrorConstants.cs
@@ -2,7 +2,7 @@
 {
 	public static class ErrorConstants
 	{
-		public const string ErrorOnGetPage = "An error occurred loading the page, please try again. If the error persists contact the service administrator.";
+		public const string ErrorOnGetPage = "There was an error. Refresh the page or try again later.\n\nIf the problem still continues, email the Record concerns and support for trusts team at regionalservices.rg@education.go.uk  ";
 		public const string ErrorOnPostPage = "There was an error and your changes were not saved. Refresh the page and if the problem continues, try again later. Email the Record concerns and support for trusts team at regionalservices.rg@education.gov.uk if this problem continues.";
 		public const string ErrorMessageKey = "Error.Message";
 		public const string ErrorDetailsKey = "Error.Details";
