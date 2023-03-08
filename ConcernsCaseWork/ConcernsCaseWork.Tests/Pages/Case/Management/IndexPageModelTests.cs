0f6d1e70a077928ee01f2eb7255e63feb9fdf61a
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-03-08 11:20:21 +0000

    breaking test on purpose to check output

--- ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Case/Management/IndexPageModelTests.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Case/Management/IndexPageModelTests.cs
@@ -193,7 +193,7 @@ namespace ConcernsCaseWork.Tests.Pages.Case.Management
 			var showEditActions = pageModel.IsEditableCase;
 
 			// assert
-			Assert.False(showEditActions);
+			Assert.True(showEditActions);
 		}
 
 		[Test]
