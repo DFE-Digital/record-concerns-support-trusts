015cf53f0b9fc5a9fa623b8550df24d404db0d11
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-03-09 16:27:09 +0000

    121376: removed interceptor from e2e

--- ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/support/e2e.ts
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/support/e2e.ts
@@ -53,23 +53,3 @@ declare global {
 
 	
 }
-
-// beforeEach(() => {
-// 	cy.intercept(
-// 		{
-// 			url: Cypress.env(EnvUrl) + "/**",
-// 			middleware: true,
-// 		},
-// 		(req) =>
-// 		{
-// 			// Set an auth header on every request made by the browser
-// 			req.headers['Authorization'] = `Bearer ${Cypress.env(EnvAuthKey)}`;
-//             req.headers = {
-//                 ...req.headers,
-//                 "ApiKey": Cypress.env(EnvApiKey),
-//                 "x-user-context-role-0" : CaseworkerClaim,
-//                 "x-user-context-name" : Cypress.env(EnvUsername)
-//             };
-// 		}
-// 	).as("@AuthInterceptor")
-// })
