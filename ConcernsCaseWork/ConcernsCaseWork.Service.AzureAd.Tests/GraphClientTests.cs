755483ef0056c0be234a42be47dd97739c962b43
Chris Dexter <109349584+chrisdexnimble@users.noreply.github.com>
2023-03-14 11:30:09 +0000

    Tweaked so that the test can assert the right interface is implemented, but excluded it from coverage because it deals directly with graphapi

--- ConcernsCaseWork/ConcernsCaseWork.Service.AzureAd.Tests/GraphClientTests.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Service.AzureAd.Tests/GraphClientTests.cs
@@ -1,13 +1,13 @@
-﻿// using ConcernsCaseWork.Service.AzureAd.Client;
-// using FluentAssertions;
-//
-// namespace ConcernsCaseWork.Service.AzureAd.Tests;
-//
-// public class GraphClientTests
-// {
-// 	[Fact]
-// 	public void GraphClient_Implements_IGraphClient()
-// 	{
-// 		typeof(GraphClient).Should().Implement<IGraphClient>();
-// 	}
-// }
+﻿using ConcernsCaseWork.Service.AzureAd.Client;
+using FluentAssertions;
+
+namespace ConcernsCaseWork.Service.AzureAd.Tests;
+
+public class GraphClientTests
+{
+	[Fact]
+	public void GraphClient_Implements_IGraphClient()
+	{
+		typeof(GraphClient).Should().Implement<IGraphClient>();
+	}
+}
