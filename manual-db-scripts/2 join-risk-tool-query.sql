7b859f9cc57239c59e1a4f7b8b101462f86e6e95
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-04-26 14:52:27 +0100

    125818: updated the script to include the concern sub type

--- manual-db-scripts/2 join-risk-tool-query.sql
+++ manual-db-scripts/2 join-risk-tool-query.sql
@@ -6,7 +6,7 @@ SELECT
 	cc.UpdatedAt AS CaseLastUpdated,
 	cc.NextSteps AS NextSteps,
 	cc.Issue AS Issue,
-	STRING_AGG(ct.Name + ': ' + concerns_risk.Name, ', ') AS ConcernRisks,
+	STRING_AGG(ct.Name + COALESCE(' ' + ct.Description, '') + ': ' + concerns_risk.Name, ', ') AS ConcernRisks,
 	COALESCE(nti_status.Name, nti_wl_status.Name) AS NtiStatus,
 	COALESCE(nti.DateStarted, nti_uc.CreatedAt, nti_wl.DateLetterSent) AS DateIssued
 FROM
@@ -64,69 +64,3 @@ GROUP BY
 	nti.DateStarted,
 	nti_uc.CreatedAt,
 	nti_wl.DateLetterSent
-	
-SELECT
-	cc.Id AS CaseId,
-	cc.CreatedAt AS CaseCreated,
-	trust_risk.Name AS TrustRisk,
-	cc.TrustCompaniesHouseNumber AS CompaniesHouseNumber,
-	cc.UpdatedAt AS CaseLastUpdated,
-	cc.NextSteps AS NextSteps,
-	cc.Issue AS Issue,
-	STRING_AGG(concern_risk.Name, ', ') AS ConcernRisks
-FROM
-	concerns.ConcernsCase cc WITH(NOLOCK)
-LEFT OUTER JOIN
-	concerns.ConcernsRating AS trust_risk WITH(NOLOCK)
-ON
-	cc.RatingId = trust_risk.Id
-LEFT OUTER JOIN
-	concerns.ConcernsRecord concern_record WITH(NOLOCK)
-ON
-	cc.Id = concern_record.CaseId
-LEFT OUTER JOIN
-	concerns.ConcernsRating AS concern_risk WITH(NOLOCK)
-ON
-	concern_record.RatingId = concern_risk.Id
-GROUP BY
-	cc.Id,
-	cc.CreatedAt,
-	trust_risk.Name,
-	cc.TrustCompaniesHouseNumber,
-	cc.UpdatedAt,
-	cc.NextSteps,
-	cc.Issue
-	
-	
-SELECT
-	cc.Id AS CaseId,
-	nti_status.Name AS Nti,
-	CASE WHEN nti_uc.Id IS NOT NULL THEN 'Open' 
-	END AS Nti_Uc,
-	nti_wl_status.Name AS Nti_Wl,
-	COALESCE(nti.DateStarted, nti_uc.CreatedAt, nti_wl.DateLetterSent) AS DateIssued
-FROM
-	concerns.ConcernsCase cc WITH(NOLOCK)
-LEFT OUTER JOIN 
-	concerns.NoticeToImproveCase nti WITH(NOLOCK) 
-ON 
-	cc.Id = nti.CaseUrn
-	AND nti.ClosedAt IS NULL
-LEFT OUTER JOIN 
-	concerns.NoticeToImproveStatus nti_status WITH(NOLOCK) 
-ON 
-	nti.StatusId = nti_status.Id
-LEFT OUTER JOIN 
-	concerns.NTIUnderConsiderationCase nti_uc WITH(NOLOCK) 
-ON 
-	cc.Id = nti_uc.CaseUrn
-	AND nti_uc.ClosedAt IS NULL
-LEFT OUTER JOIN 
-	concerns.NTIWarningLetterCase nti_wl WITH(NOLOCK) 
-ON 
-	cc.Id = nti_wl.CaseUrn
-	AND nti_wl.ClosedAt IS NULL
-LEFT OUTER JOIN 
-	concerns.NTIWarningLetterStatus nti_wl_status WITH(NOLOCK) 
-ON 
-	nti_wl.StatusId = nti_wl_status.Id
