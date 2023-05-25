41539dbc5c55c882586096ee88bdd9d83bef4d20
FahadDarw <fahad.darwish@nimbleapproach.com>
2023-05-25 14:35:22 +0100

    Written test for non concern journey

--- ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/e2e/casework-regression/create-non-concerns-case.cy.ts
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/e2e/casework-regression/create-non-concerns-case.cy.ts
@@ -5,6 +5,7 @@ import caseManagementPage from "cypress/pages/caseMangementPage";
 import { SelectCaseTypePage } from "cypress/pages/createCase/selectCaseTypePage";
 import { EnvUsername } from "cypress/constants/cypressConstants";
 import { CreateCasePage } from "cypress/pages/createCase/createCasePage";
+import CreateConcernPage from "cypress/pages/createCase/createConcernPage";
 
 describe("Creating a case", () => {
     let email: string;
@@ -13,6 +14,8 @@ describe("Creating a case", () => {
     const addTerritoryPage = new AddTerritoryPage();
     const addConcernDetailsPage = new AddConcernDetailsPage();
     const createCasePage = new CreateCasePage();
+    const createConcernPage = new CreateConcernPage();
+
     beforeEach(() => {
         cy.login();
         cy.visit(Cypress.env('url') + '/case/create');
@@ -68,6 +71,46 @@ describe("Creating a case", () => {
             .hasCaseHistory(caseHistoryData)
             .hasCaseOwner(name);
 
+        Logger.Log("Click add concern")
+        caseManagementPage.
+        getAddConcernBtn();
+
+        Logger.Log("Checking accessibility on adding concern page");
+        cy.excuteAccessibilityTests();
+
+        Logger.Log("Create a valid concern");
+        createConcernPage
+            .withConcernType("Financial")
+            .withSubConcernType("Financial: Deficit")
+            .withRating("Red-Amber")
+            .withMeansOfRefferal("External")
+            .addConcern();
+
+        Logger.Log("Click next to take you to risk page and Select risk");
+        addConcernDetailsPage.nextStep();
+        createConcernPage
+   
+        .withRating("Red-Amber");
+        addConcernDetailsPage.nextStep();
+            Logger.Log("Checking accessibility on create case concern page");
+            cy.excuteAccessibilityTests();
+
+            Logger.Log("Populate territory");
+            addTerritoryPage
+                .withTerritory(territory)
+                .nextStep();
+                Logger.Log("Add concern details with valid text limit");
+                addConcernDetailsPage
+                    .withIssue("This is an issue")
+                    .withCurrentStatus("This is the current status")
+                    .withCaseAim("This is the case aim")
+                    .withDeEscalationPoint("This is the de-escalation point")
+                    .withNextSteps("This is the next steps")
+                    .withCaseHistory("This is the case history")
+                    .createCase();
+           
 
     });
+
+    
 });
