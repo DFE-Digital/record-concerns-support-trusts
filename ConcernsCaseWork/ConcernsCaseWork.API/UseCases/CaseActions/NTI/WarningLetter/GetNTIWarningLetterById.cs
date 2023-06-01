bcd99290b04ee942d81095f2b01d4e0509ce8352
blakebyron-nimble <130747129+blakebyron-nimble@users.noreply.github.com>
2023-06-01 09:14:11 +0100

    Remove Commented out code & update test

--- ConcernsCaseWork/ConcernsCaseWork.API/UseCases/CaseActions/NTI/WarningLetter/GetNTIWarningLetterById.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API/UseCases/CaseActions/NTI/WarningLetter/GetNTIWarningLetterById.cs
@@ -28,58 +28,8 @@ namespace ConcernsCaseWork.API.UseCases.CaseActions.NTI.WarningLetter
 			if (warningLetter ==null)
 			{
 				return null;
-				//return Task.FromResult<NTIWarningLetterResponse>(null).Result;
 			}
 			return NTIWarningLetterFactory.CreateResponse(warningLetter);
 		}
-
-		//public async NTIWarningLetterResponse Execute(long warningLetterId)
-		//{
-		//	var result = await _gateway.GetNTIWarningLetterById(warningLetterId);
-
-		//	return result == null ? null : NTIWarningLetterFactory.CreateResponse(result);
-
-
-		//}
-
-		//public async Task<NTIWarningLetterResponse> ExecuteAsync(long warningLetterId)
-		//{
-		//	var result = await _gateway.GetNTIWarningLetterById(warningLetterId);
-
-		//	return result == null ? null : NTIWarningLetterFactory.CreateResponse(result);
-		//}
-
-		//     public async Task<NTIWarningLetterResponse> ExecuteAsync(long warningLetterId)
-		//     {
-		//var warningLetter = await _gateway.GetNTIWarningLetterById(warningLetterId);
-		//if (warningLetter == null)
-		//{
-		//	return Task.FromResult<NTIWarningLetterResponse>(null);
-		//}
-		//return NTIWarningLetterFactory.CreateResponse(warningLetter);
-
-
-		////async Task<NTIWarningLetterResponse> DoWork()
-		////{
-		////	var warningLetter = await _gateway.GetNTIWarningLetterById(warningLetterId);
-
-		////	return warningLetter != null ? NTIWarningLetterFactory.CreateResponse(warningLetter): null;
-		////}
-
-		////return await DoWork();
-		//}
-
-
-
-		//public NTIWarningLetterResponse Execute(long warningLetterId)
-		//{
-		//	return ExecuteAsync(warningLetterId).Result;
-		//}
-
-		//public async Task<NTIWarningLetterResponse> ExecuteAsync(long warningLetterId)
-		//{
-		//	var warningLetter = await _gateway.GetNTIWarningLetterById(warningLetterId);
-		//	return NTIWarningLetterFactory.CreateResponse(warningLetter);
-		//}
 	}
 }
