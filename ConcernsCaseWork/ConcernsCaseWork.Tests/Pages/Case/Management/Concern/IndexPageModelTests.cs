27b1c553637ed0c8d046c2b268492acb9a13e8aa
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-06-02 08:59:16 +0100

    129035: fixed broken tests

--- ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Case/Management/Concern/IndexPageModelTests.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Case/Management/Concern/IndexPageModelTests.cs
@@ -73,7 +73,7 @@ namespace ConcernsCaseWork.Tests.Pages.Case.Management.Concern
 			Assert.That(pageModel.CreateRecordsModel.Count, Is.EqualTo(createRecordsModel.Count));
 			Assert.Null(pageModel.TempData["Error.Message"]);
 			
-			mockCaseModelService.Verify(c => c.GetCaseByUrn(It.IsAny<long>()), Times.Once);
+			mockCaseModelService.Verify(c => c.GetCaseByUrn(It.IsAny<long>()), Times.Exactly(2));
 			mockRecordModelService.Verify(r => r.GetCreateRecordsModelByCaseUrn(It.IsAny<long>()), Times.Once);
 			mockTrustModelService.Verify(t => t.GetTrustByUkPrn(It.IsAny<string>()), Times.Once);
 			mockRatingModelService.Verify(r => r.GetRatingsModel(), Times.Once);
