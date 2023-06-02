09281c93713e3e7ba24a078f7b3b8fae11c21011
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-06-02 09:05:11 +0100

    129035: uncommented test

--- ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Case/Management/IndexPageModelTests.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Pages/Case/Management/IndexPageModelTests.cs
@@ -1,8 +1,7 @@
-﻿/*using AutoFixture;
-using ConcernsCaseWork.Constants;
+﻿using AutoFixture;
 using ConcernsCaseWork.API.Contracts.Permissions;
 using ConcernsCaseWork.Authorization;
-using ConcernsCaseWork.Models;
+using ConcernsCaseWork.Constants;
 using ConcernsCaseWork.Models.CaseActions;
 using ConcernsCaseWork.Pages.Case.Management;
 using ConcernsCaseWork.Redis.NtiUnderConsideration;
@@ -26,7 +25,6 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ConcernsCaseWork.Tests.Pages.Case.Management
@@ -255,4 +253,4 @@ namespace ConcernsCaseWork.Tests.Pages.Case.Management
 			pageModel.TempData["Error.Message"].Should().BeNull();
 		}
 	}
-}*/
+}
