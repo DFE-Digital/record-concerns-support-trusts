9706d2faaa79708e66201d3be6e42c29ba125bc3
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-06-09 14:52:36 +0100

    131508: added pagination to the active cases by trust api
    added integration tests for active and closed cases

--- ConcernsCaseWork/ConcernsCaseWork.API/ResponseModels/PagingResponse.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API/ResponseModels/PagingResponse.cs
@@ -5,5 +5,8 @@ namespace ConcernsCaseWork.API.ResponseModels
         public int Page { get; set; }
         public int RecordCount { get; set; }
         public string NextPageUrl { get; set; }
+		public bool HasNext { get; set; }
+
+		public bool HasPrevious { get; set; }
     }
 }
