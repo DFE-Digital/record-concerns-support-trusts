df3c0ce8077b9c66da5f8c10dbef3939c3298552
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-06-12 12:27:21 +0100

    131508: tests now passing after having a technical issue
    tests were not running together when they were in the same collection, which meant the setup code was breaking
    
    no idea why its working now, will monitor it, it may have just been my environment

--- ConcernsCaseWork/ConcernsCaseWork.API/ResponseModels/PagingResponse.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API/ResponseModels/PagingResponse.cs
@@ -2,7 +2,14 @@ namespace ConcernsCaseWork.API.ResponseModels
 {
     public class PagingResponse
     {
+		/// <summary>
+		/// The current page we are on
+		/// </summary>
         public int Page { get; set; }
+
+		/// <summary>
+		/// This is the total record count
+		/// </summary>
         public int RecordCount { get; set; }
         public string NextPageUrl { get; set; }
 		public bool HasNext { get; set; }
