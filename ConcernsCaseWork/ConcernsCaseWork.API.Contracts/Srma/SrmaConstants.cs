755ebd7fe0355dbb4e566b69298aff92b3cf632d
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-06-22 11:28:13 +0100

    132389: increased the size of the srma notes field from 2000 to 5000 characters

--- ConcernsCaseWork/ConcernsCaseWork.API.Contracts/Srma/SrmaConstants.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API.Contracts/Srma/SrmaConstants.cs
@@ -1,7 +1,8 @@
-﻿namespace ConcernsCaseWork.Constants
+﻿namespace ConcernsCaseWork.API.Contracts.Srma
 {
 	public class SrmaConstants
 	{
+		public const int NotesLength = 5000;
 		public const string ResolutionComplete = "complete";
 		public const string ResolutionCancelled = "cancel";
 		public const string ResolutionDeclined = "decline";
