605d8f4373164567d9e545bae499acffde8a9517
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-06-30 12:42:13 +0100

    132005: changed automation tests to create cases in uk local time
    with pagination had an issue where the cases created by the site were in a different timezone from those created through the automation using the api

--- ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/support/formatDate.ts
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/support/formatDate.ts
@@ -1,3 +1,14 @@
+import { DateTime } from 'luxon';
+
 export function toDisplayDate(date: Date): string {
     return date.toLocaleDateString('en-gb', {day:"numeric", month: "long", year: "numeric"});
+}
+
+export function getUkLocalDate(): string {
+    // Dates are stored in local time in the app
+    // Cases are sorted by date, the timezone matters
+    // In the future, the app should use ISO
+    const result = DateTime.local().setZone('Europe/London');
+
+    return <string>result.toISO();
 }
