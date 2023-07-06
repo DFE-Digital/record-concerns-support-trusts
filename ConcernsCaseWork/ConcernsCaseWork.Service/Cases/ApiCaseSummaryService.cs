cd32a1ebfaba24602928869eac710024a4fcc6e7
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-07-06 15:06:46 +0100

    131023: added feature flag for turning off trust pagination

--- ConcernsCaseWork/ConcernsCaseWork.Service/Cases/ApiCaseSummaryService.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Service/Cases/ApiCaseSummaryService.cs
@@ -1,34 +1,79 @@
+using ConcernsCaseWork.API.Contracts.Configuration;
 using ConcernsCaseWork.Logging;
 using ConcernsCaseWork.Service.Base;
 using ConcernsCaseWork.UserContext;
 using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
 
 namespace ConcernsCaseWork.Service.Cases;
 
 public class ApiCaseSummaryService : ConcernsAbstractService, IApiCaseSummaryService
 {
-	public ApiCaseSummaryService(ILogger<ApiCaseSummaryService> logger, ICorrelationContext correlationContext, IHttpClientFactory clientFactory, IClientUserInfoService userInfoService) :
-		base(clientFactory, logger, correlationContext, userInfoService) { }
+	private IFeatureManager _featureManager;
+
+	public ApiCaseSummaryService(
+		ILogger<ApiCaseSummaryService> logger, 
+		ICorrelationContext correlationContext, 
+		IHttpClientFactory clientFactory, 
+		IClientUserInfoService userInfoService,
+		IFeatureManager featureManager) : base(clientFactory, logger, correlationContext, userInfoService) 
+	{
+		_featureManager = featureManager;
+	}
+
 	public async Task<IEnumerable<ActiveCaseSummaryDto>> GetActiveCaseSummariesForUsersTeam(string caseworker)
 		=> await Get<IEnumerable<ActiveCaseSummaryDto>>($"/{EndpointsVersion}/concerns-cases/summary/{caseworker}/active/team");
-	
+
 	public async Task<ApiListWrapper<ActiveCaseSummaryDto>> GetActiveCaseSummariesByCaseworker(
-		string caseworker, 
+		string caseworker,
 		int? page = 1)
-		=> await GetByPagination<ActiveCaseSummaryDto>($"/{EndpointsVersion}/concerns-cases/summary/{caseworker}/active?page={page}&count=5");
+	{
+		var result = await GetByPagination<ActiveCaseSummaryDto>($"/{EndpointsVersion}/concerns-cases/summary/{caseworker}/active?page={page}&count=5");
+
+		return result;
+	}
 
 	public async Task<ApiListWrapper<ClosedCaseSummaryDto>> GetClosedCaseSummariesByCaseworker(
 		string caseworker,
 		int? page = 1)
-		=> await GetByPagination<ClosedCaseSummaryDto>($"/{EndpointsVersion}/concerns-cases/summary/{caseworker}/closed?page={page}&count=5");
+	{
+		var result = await GetByPagination<ClosedCaseSummaryDto>($"/{EndpointsVersion}/concerns-cases/summary/{caseworker}/closed?page={page}&count=5");
+
+		return result;
+	}
 
 	public async Task<ApiListWrapper<ActiveCaseSummaryDto>> GetActiveCaseSummariesByTrust(
 		string trustUkPrn,
 		int? page = 1)
-		=> await GetByPagination<ActiveCaseSummaryDto>($"/{EndpointsVersion}/concerns-cases/summary/bytrust/{trustUkPrn}/active?page={page}&count=5");
+	{
+		var endpoint = await AppendPagination($"/{EndpointsVersion}/concerns-cases/summary/bytrust/{trustUkPrn}/active", page);
+
+		var result = await GetByPagination<ActiveCaseSummaryDto>(endpoint);
+
+		return result;
+	}
 
 	public async Task<ApiListWrapper<ClosedCaseSummaryDto>> GetClosedCaseSummariesByTrust(
 		string trustUkPrn,
 		int? page = 1)
-		=> await GetByPagination<ClosedCaseSummaryDto>($"/{EndpointsVersion}/concerns-cases/summary/bytrust/{trustUkPrn}/closed?page={page}&count=5");
+	{
+		var endpoint = await AppendPagination($"/{EndpointsVersion}/concerns-cases/summary/bytrust/{trustUkPrn}/closed", page);
+
+		var result = await GetByPagination<ClosedCaseSummaryDto>(endpoint);
+        return result;
+    }
+
+	private async Task<string> AppendPagination(string endpoint, int? page)
+	{
+		var isPaginationEnabled = await _featureManager.IsEnabledAsync(FeatureFlags.IsTrustCasesTabsEnabled);
+
+		var result = endpoint;
+
+		if (isPaginationEnabled)
+		{
+			result += $"?page={page}&count=5";
+		}
+
+		return result;
+	}
 }
