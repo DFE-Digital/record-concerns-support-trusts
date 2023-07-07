3796c8a3c55206f52867174ecdf2f2ac91de1dbd
blakebyron-nimble <130747129+blakebyron-nimble@users.noreply.github.com>
2023-07-07 11:51:49 +0100

    CTC Api Endpoints and tests

---
+++ ConcernsCaseWork/ConcernsCaseWork.API/Features/CityTechnicalCollege/CityTechnologyCollegeController.cs
@@ -0,0 +1,52 @@
+﻿using ConcernsCaseWork.API.ResponseModels;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ConcernsCaseWork.API.Features.CityTechnicalCollege
+{
+	[ApiVersion("2.0")]
+	[ApiController]
+	[Route("v{version:apiVersion}/citytechnologycolleges")]
+	public class CityTechnologyCollegeController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public CityTechnologyCollegeController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		/// <summary>
+		/// Get a list of city technology colleges
+		/// </summary>
+		/// <returns>A list of city technology colleges containing the ID, Name, UKPRN, Companies House Number and Address</returns>
+		[HttpGet(Name = nameof(List))]
+		[MapToApiVersion("2.0")]
+		[ProducesResponseType((int)HttpStatusCode.OK)]
+		public async Task<IActionResult> List(List.Query query)
+		{
+			var model = await _mediator.Send(query);
+			return Ok(model.Items);
+		}
+
+		/// <summary>
+		/// Get an individual city technology college based on the UKPRN
+		/// </summary>
+		/// <param name="query">Property Reference parameter</param>
+		/// <returns>A city technology college containing the ID, Name, UKPRN, Companies House Number and Address</returns>
+		[HttpGet("ukprn/{ukprn}", Name = nameof(GetByUKPRN))]
+		[ProducesResponseType((int)HttpStatusCode.OK)]
+		[ProducesResponseType((int)HttpStatusCode.NotFound)]
+		public async Task<IActionResult> GetByUKPRN(GetByUKPRN.Query query)
+		{
+			var model = await _mediator.Send(query);
+			if (model == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(model);
+		}
+	}
+}
