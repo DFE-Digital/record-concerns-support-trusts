feb5250ab844753209dfd2222d1347bd8fd411f9
Michael Stock <115985723+mikestock-nimble@users.noreply.github.com>
2023-07-21 14:27:23 +0100

    134367: fixed minor styling issues on trust overview sub nav (#951)
    removed feature flag for sub navs

--- ConcernsCaseWork/ConcernsCaseWork.Service/Cases/ApiCaseSummaryService.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Service/Cases/ApiCaseSummaryService.cs
@@ -1,24 +1,19 @@
-using ConcernsCaseWork.API.Contracts.Configuration;
 using ConcernsCaseWork.Logging;
 using ConcernsCaseWork.Service.Base;
 using ConcernsCaseWork.UserContext;
 using Microsoft.Extensions.Logging;
-using Microsoft.FeatureManagement;
 
 namespace ConcernsCaseWork.Service.Cases;
 
 public class ApiCaseSummaryService : ConcernsAbstractService, IApiCaseSummaryService
 {
-	private IFeatureManager _featureManager;
-
 	public ApiCaseSummaryService(
 		ILogger<ApiCaseSummaryService> logger, 
 		ICorrelationContext correlationContext, 
 		IHttpClientFactory clientFactory, 
-		IClientUserInfoService userInfoService,
-		IFeatureManager featureManager) : base(clientFactory, logger, correlationContext, userInfoService) 
+		IClientUserInfoService userInfoService) : base(clientFactory, logger, correlationContext, userInfoService) 
 	{
-		_featureManager = featureManager;
+
 	}
 
 	public async Task<IEnumerable<ActiveCaseSummaryDto>> GetActiveCaseSummariesForUsersTeam(string caseworker)
@@ -46,9 +41,7 @@ public class ApiCaseSummaryService : ConcernsAbstractService, IApiCaseSummarySer
 		string trustUkPrn,
 		int? page = 1)
 	{
-		var endpoint = await AppendPagination($"/{EndpointsVersion}/concerns-cases/summary/bytrust/{trustUkPrn}/active", page);
-
-		var result = await GetByPagination<ActiveCaseSummaryDto>(endpoint);
+		var result = await GetByPagination<ActiveCaseSummaryDto>($"/{EndpointsVersion}/concerns-cases/summary/bytrust/{trustUkPrn}/active?page={page}&count=5");
 
 		return result;
 	}
@@ -57,23 +50,8 @@ public class ApiCaseSummaryService : ConcernsAbstractService, IApiCaseSummarySer
 		string trustUkPrn,
 		int? page = 1)
 	{
-		var endpoint = await AppendPagination($"/{EndpointsVersion}/concerns-cases/summary/bytrust/{trustUkPrn}/closed", page);
+		var result = await GetByPagination<ClosedCaseSummaryDto>($"/{EndpointsVersion}/concerns-cases/summary/bytrust/{trustUkPrn}/closed?page={page}&count=5");
 
-		var result = await GetByPagination<ClosedCaseSummaryDto>(endpoint);
         return result;
     }
-
-	private async Task<string> AppendPagination(string endpoint, int? page)
-	{
-		var isPaginationEnabled = await _featureManager.IsEnabledAsync(FeatureFlags.IsTrustCasesTabsEnabled);
-
-		var result = endpoint;
-
-		if (isPaginationEnabled)
-		{
-			result += $"?page={page}&count=5";
-		}
-
-		return result;
-	}
 }
