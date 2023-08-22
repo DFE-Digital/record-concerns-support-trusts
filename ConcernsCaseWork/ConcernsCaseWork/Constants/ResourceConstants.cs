9a51b6d53756e41e80dab2e51ec6f0c79346dea2
elielijah321 <45821029+elielijah321@users.noreply.github.com>
2023-08-22 15:16:23 +0100

    update RMF link

--- ConcernsCaseWork/ConcernsCaseWork/Constants/ResourceConstants.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Constants/ResourceConstants.cs
@@ -2,6 +2,6 @@
 {
 	public class ResourceConstants
 	{
-		public const string RiskManagementFrameworkLink = "https://educationgovuk.sharepoint.com/:p:/s/RiskSpecialisedProjects/EakqY0aFk9RHkAfCNjatCF4Bf0ip1TFOzLkh3QMMfgAQGg?e=ualpKZ&PreviousSessionID=57c2d265-2f87-2e08-53ec-d75c062ef9a3&wdLOR=c5D3DE9AC-A670-D34B-AC9E-4958311212E8";
+		public const string RiskManagementFrameworkLink = "https://educationgovuk.sharepoint.com/:p:/s/lveefa00003/ERdAuIIBtxNKrt-QHPcwEdEBWUWUj993tjSa8CleF5prOw?e=hTuwVW";
 	}
 }
