e7e426ed891fa91ed03325f91eac1c93e017e994
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-09-22 12:45:26 +0100

    fixed swagger for financial plan

--- ConcernsCaseWork/ConcernsCaseWork.API/Features/FinancialPlan/FinancialPlanController.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API/Features/FinancialPlan/FinancialPlanController.cs
@@ -1,67 +1,69 @@
-﻿//using ConcernsCaseWork.API.ResponseModels;
-//using ConcernsCaseWork.API.ResponseModels.CaseActions.FinancialPlan;
-//using MediatR;
-//using Microsoft.AspNetCore.Mvc;
-//using System.Net;
+﻿using ConcernsCaseWork.API.RequestModels.CaseActions.FinancialPlan;
+using ConcernsCaseWork.API.ResponseModels;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
-//namespace ConcernsCaseWork.API.Features.FinancialPlan
-//{
-//	[ApiVersion("2.0")]
-//	[Route("v{version:apiVersion}/case-actions/financial-plan")]
-//	[ApiController]
-//	public class FinancialPlanController : Controller
-//	{
-//		private readonly IMediator _mediator;
-//		public FinancialPlanController(IMediator mediator)
-//		{
-//			_mediator = mediator;
-//		}
+namespace ConcernsCaseWork.API.Features.FinancialPlan
+{
+	[ApiVersion("2.0")]
+	[Route("v{version:apiVersion}/case-actions/financial-plan")]
+	[ApiController]
+	public class FinancialPlanController : Controller
+	{
+		private readonly IMediator _mediator;
+		public FinancialPlanController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
 
-//		[HttpGet("{Id}")]
-//		[ProducesResponseType((int)HttpStatusCode.OK)]
-//		[ProducesResponseType((int)HttpStatusCode.NotFound)]
-//		public async Task<IActionResult> GetByID([FromRoute]GetByID.Query query)
-//		{
-//			var model = await _mediator.Send(query);
-//			if (model == null)
-//			{
-//				return NotFound();
-//			}
+		[HttpGet("{Id}")]
+		[ProducesResponseType((int)HttpStatusCode.OK)]
+		[ProducesResponseType((int)HttpStatusCode.NotFound)]
+		public async Task<IActionResult> GetByID([FromRoute] GetByID.Query query)
+		{
+			var model = await _mediator.Send(query);
+			if (model == null)
+			{
+				return NotFound();
+			}
 
-//			return Ok(new ApiSingleResponseV2<GetByID.Result>(model));
-//		}
+			return Ok(new ApiSingleResponseV2<GetByID.Result>(model));
+		}
 
-//		[HttpPost()]
-//		[MapToApiVersion("2.0")]
-//		[ProducesResponseType((int)HttpStatusCode.Created)]
-//		[ProducesResponseType((int)HttpStatusCode.BadRequest)]
-//		public async Task<IActionResult> Create([FromBody] Create.Command command, CancellationToken cancellationToken = default)
-//		{
-//			var commandResult = await _mediator.Send(command);
-//			var model = await _mediator.Send(new GetByID.Query() { Id = commandResult });
-//			return CreatedAtAction(nameof(GetByID), new { Id = model.Id }, new ApiSingleResponseV2<GetByID.Result>(model));
-//		}
+		[HttpPost()]
+		[MapToApiVersion("2.0")]
+		[ProducesResponseType((int)HttpStatusCode.Created)]
+		[ProducesResponseType((int)HttpStatusCode.BadRequest)]
+		public async Task<IActionResult> Create([FromBody] CreateFinancialPlanRequest request, CancellationToken cancellationToken = default)
+		{
+			var command = new Create.Command(request);
+			var commandResult = await _mediator.Send(command);
+			var model = await _mediator.Send(new GetByID.Query() { Id = commandResult });
+			return CreatedAtAction(nameof(GetByID), new { Id = model.Id }, new ApiSingleResponseV2<GetByID.Result>(model));
+		}
 
 
-//		[HttpPatch]
-//		[MapToApiVersion("2.0")]
-//		[ProducesResponseType((int)HttpStatusCode.Created)]
-//		[ProducesResponseType((int)HttpStatusCode.BadRequest)]
-//		public async Task<IActionResult> Update([FromBody] Update.Command command, CancellationToken cancellationToken = default)
-//		{
-//			var commandResult = await _mediator.Send(command);
-//			var model = await _mediator.Send(new GetByID.Query() { Id = commandResult });
-//			return Ok(new ApiSingleResponseV2<GetByID.Result>(model));
-//		}
+		[HttpPatch]
+		[MapToApiVersion("2.0")]
+		[ProducesResponseType((int)HttpStatusCode.Created)]
+		[ProducesResponseType((int)HttpStatusCode.BadRequest)]
+		public async Task<IActionResult> Update([FromBody] PatchFinancialPlanRequest request, CancellationToken cancellationToken = default)
+		{
+			var command = new Update.Command(request);
+			var commandResult = await _mediator.Send(command);
+			var model = await _mediator.Send(new GetByID.Query() { Id = commandResult });
+			return Ok(new ApiSingleResponseV2<GetByID.Result>(model));
+		}
 
-//		[HttpDelete("{Id}")]
-//		[MapToApiVersion("2.0")]
-//		[ProducesResponseType((int)HttpStatusCode.BadRequest)]
-//		public async Task<IActionResult> Delete([FromRoute] Delete.Command command, CancellationToken cancellationToken = default)
-//		{
-//			await _mediator.Send(command);
-		
-//			return NoContent();
-//		}
-//	}
-//}
+		[HttpDelete("{Id}")]
+		[MapToApiVersion("2.0")]
+		[ProducesResponseType((int)HttpStatusCode.BadRequest)]
+		public async Task<IActionResult> Delete([FromRoute] Delete.Command command, CancellationToken cancellationToken = default)
+		{
+			await _mediator.Send(command);
+
+			return NoContent();
+		}
+	}
+}
--- ConcernsCaseWork/ConcernsCaseWork.API/Features/FinancialPlan/Update.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API/Features/FinancialPlan/Update.cs
@@ -1,11 +1,8 @@
 ﻿using ConcernsCaseWork.API.RequestModels.CaseActions.FinancialPlan;
-using ConcernsCaseWork.API.UseCases.CaseActions.FinancialPlan;
 using ConcernsCaseWork.Data;
 using ConcernsCaseWork.Data.Models;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-using static ConcernsCaseWork.API.Features.ConcernsRecord.Update;
 
 namespace ConcernsCaseWork.API.Features.FinancialPlan
 {
@@ -14,25 +11,12 @@ namespace ConcernsCaseWork.API.Features.FinancialPlan
 
 		public class Command : IRequest<long>
 		{
-			[Required]
-			public long Id { get; set; }
-			[Required]
-			public int CaseUrn { get; set; }
+			public PatchFinancialPlanRequest Request { get; }
 
-			[StringLength(300)]
-			public string Name { get; set; }
-			public long? StatusId { get; set; }
-			public DateTime? DatePlanRequested { get; set; }
-			public DateTime? DateViablePlanReceived { get; set; }
-			public DateTime CreatedAt { get; set; }
-
-			[StringLength(300)]
-			public string CreatedBy { get; set; }
-			public DateTime UpdatedAt { get; set; }
-			public DateTime? ClosedAt { get; set; }
-
-			[StringLength(2000)]
-			public string Notes { get; set; }
+			public Command(PatchFinancialPlanRequest request)
+			{
+				Request = request;
+			}
 		}
 
 		public class CommandHandler : IRequestHandler<Command, long>
@@ -46,8 +30,9 @@ namespace ConcernsCaseWork.API.Features.FinancialPlan
 				_mediator = mediator;
 			}
 
-			public async Task<long> Handle(Command request, CancellationToken cancellationToken)
+			public async Task<long> Handle(Command command, CancellationToken cancellationToken)
 			{
+				var request = command.Request;
 				var fp = new FinancialPlanCase
 				{
 					Id = request.Id,
@@ -63,7 +48,7 @@ namespace ConcernsCaseWork.API.Features.FinancialPlan
 					UpdatedAt = request.UpdatedAt,
 				};
 
-				var tracked = _context.Update<FinancialPlanCase>(fp);
+				var tracked = _context.Update(fp);
 				await _context.SaveChangesAsync();
 
 				var updatedNotification = new FinancialPlanUpdatedNotification() { Id = fp.Id, CaseId = fp.CaseUrn, };
