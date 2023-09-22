1e4fc0d02bff2cfa5ac9bcb0f97aeff2be414e58
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-09-22 14:17:05 +0100

    fixed swagger definitions for city technical colleges

---
+++ ConcernsCaseWork/ConcernsCaseWork.API.Contracts/Trusts/CityTechnicalCollege.cs
@@ -0,0 +1,16 @@
+﻿namespace ConcernsCaseWork.API.Contracts.Trusts
+{
+	public class CityTechnologyCollege
+	{
+		public string Name { get; set; }
+		public string UKPRN { get; set; }
+		public string CompaniesHouseNumber { get; set; }
+		public string AddressLine1 { get; set; }
+		public string AddressLine2 { get; set; }
+		public string AddressLine3 { get; set; }
+		public string County { get; set; }
+		public string Town { get; set; }
+		public string Postcode { get; set; }
+
+	}
+}
