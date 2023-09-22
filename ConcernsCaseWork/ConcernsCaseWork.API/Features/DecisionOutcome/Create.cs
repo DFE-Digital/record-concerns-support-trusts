a4b693eebcfbc8bd89ac2adde8b7c4144cbc39cb
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-09-22 12:22:09 +0100

    fixed the decision controller so it generates swagger correctly

--- ConcernsCaseWork/ConcernsCaseWork.API/Features/DecisionOutcome/Create.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API/Features/DecisionOutcome/Create.cs
@@ -38,7 +38,7 @@ namespace ConcernsCaseWork.API.Features.Decision.Outcome
 				_mediator = mediator;
 			}
 
-			public async Task<CreateDecisionOutcomeResponse> Handle(Command request, CancellationToken cancellationToken)
+			public async Task<CreateDecisionOutcomeResponse> Handle(Command command, CancellationToken cancellationToken)
 			{
 				var concernCase = await _context.ConcernsCase
 					.Include(x => x.Decisions)
@@ -46,35 +46,35 @@ namespace ConcernsCaseWork.API.Features.Decision.Outcome
 					.Include(x => x.Decisions)
 					.ThenInclude(x => x.Outcome)
 					.ThenInclude(x => x.BusinessAreasConsulted)
-					.FirstOrDefaultAsync(o => o.Urn == request.ConcernsCaseUrn);
+					.FirstOrDefaultAsync(o => o.Urn == command.ConcernsCaseUrn);
 
 				if (concernCase == null)
 				{
-					throw new NotFoundException($"Not Found: Concern with id {request.ConcernsCaseUrn}");
+					throw new NotFoundException($"Not Found: Concern with id {command.ConcernsCaseUrn}");
 				}
 
-				var decision = concernCase.Decisions.FirstOrDefault(d => d.DecisionId == request.DecisionId);
+				var decision = concernCase.Decisions.FirstOrDefault(d => d.DecisionId == command.DecisionId);
 
 				if (decision == null)
 				{
-					throw new NotFoundException($"Not Found: Decision with id {request.DecisionId}, Case {request.ConcernsCaseUrn}");
+					throw new NotFoundException($"Not Found: Decision with id {command.DecisionId}, Case {command.ConcernsCaseUrn}");
 				}
 
 				if (decision.Outcome != null)
 				{
-					throw new ResourceConflictException($"Conflict: Decision with id {request.DecisionId} already has an outcome, Case {request.ConcernsCaseUrn}");
+					throw new ResourceConflictException($"Conflict: Decision with id {command.DecisionId} already has an outcome, Case {command.ConcernsCaseUrn}");
 				}
 
 				var now = DateTime.Now;
 				var outcome = new Data.Models.Concerns.Case.Management.Actions.Decisions.Outcome.DecisionOutcome()
 				{
-					DecisionId = request.DecisionId,
-					Status = request.Request.Status,
-					TotalAmount = request.Request.TotalAmount,
-					DecisionMadeDate = request.Request.DecisionMadeDate,
-					DecisionEffectiveFromDate = request.Request.DecisionEffectiveFromDate,
-					Authorizer = request.Request.Authorizer,
-					BusinessAreasConsulted = request.Request.BusinessAreasConsulted.Select(b => new DecisionOutcomeBusinessAreaMapping()
+					DecisionId = command.DecisionId,
+					Status = command.Request.Status,
+					TotalAmount = command.Request.TotalAmount,
+					DecisionMadeDate = command.Request.DecisionMadeDate,
+					DecisionEffectiveFromDate = command.Request.DecisionEffectiveFromDate,
+					Authorizer = command.Request.Authorizer,
+					BusinessAreasConsulted = command.Request.BusinessAreasConsulted.Select(b => new DecisionOutcomeBusinessAreaMapping()
 					{
 						DecisionOutcomeBusinessId = b
 					}).ToList(),
@@ -85,13 +85,13 @@ namespace ConcernsCaseWork.API.Features.Decision.Outcome
 				var result = _context.DecisionOutcomes.Add(outcome);
 				await _context.SaveChangesAsync(cancellationToken);
 
-				var decisionCreatedNotification = new DecisionOutcomeCreatedNotification() { Id = result.Entity.DecisionOutcomeId, CaseId = request.ConcernsCaseUrn };
+				var decisionCreatedNotification = new DecisionOutcomeCreatedNotification() { Id = result.Entity.DecisionOutcomeId, CaseId = command.ConcernsCaseUrn };
 				await _mediator.Publish(decisionCreatedNotification);
 
 				return new CreateDecisionOutcomeResponse()
 				{
-					ConcernsCaseUrn = request.ConcernsCaseUrn,
-					DecisionId = request.DecisionId,
+					ConcernsCaseUrn = command.ConcernsCaseUrn,
+					DecisionId = command.DecisionId,
 					DecisionOutcomeId = result.Entity.DecisionOutcomeId,
 				};
 			}
--- ConcernsCaseWork/ConcernsCaseWork.API/Features/DecisionOutcome/Update.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API/Features/DecisionOutcome/Update.cs
@@ -40,7 +40,7 @@ namespace ConcernsCaseWork.API.Features.Decision.Outcome
 				_mediator = mediator;
 			}
 
-			public async Task<UpdateDecisionOutcomeResponse> Handle(Command request, CancellationToken cancellationToken)
+			public async Task<UpdateDecisionOutcomeResponse> Handle(Command command, CancellationToken cancellationToken)
 			{
 				var concernCase = await _context.ConcernsCase
 					.Include(x => x.Decisions)
@@ -48,31 +48,31 @@ namespace ConcernsCaseWork.API.Features.Decision.Outcome
 					.Include(x => x.Decisions)
 					.ThenInclude(x => x.Outcome)
 					.ThenInclude(x => x.BusinessAreasConsulted)
-					.FirstOrDefaultAsync(o => o.Urn == request.ConcernsCaseUrn);
+					.FirstOrDefaultAsync(o => o.Urn == command.ConcernsCaseUrn);
 
 				if (concernCase == null)
 				{
-					throw new NotFoundException($"Not Found: Concern with id {request.ConcernsCaseUrn}");
+					throw new NotFoundException($"Not Found: Concern with id {command.ConcernsCaseUrn}");
 				}
 				
-				var decision = concernCase.Decisions.FirstOrDefault(d => d.DecisionId == request.DecisionId);
+				var decision = concernCase.Decisions.FirstOrDefault(d => d.DecisionId == command.DecisionId);
 
 				if (decision == null)
 				{
-					throw new NotFoundException($"Not Found: Decision with id {request.DecisionId}, Case {request.ConcernsCaseUrn}");
+					throw new NotFoundException($"Not Found: Decision with id {command.DecisionId}, Case {command.ConcernsCaseUrn}");
 				}
 
 				if (decision.Outcome == null)
 				{
-					throw new NotFoundException($"Not Found: Decision with id {request.DecisionId} does not have an outcome, Case {request.ConcernsCaseUrn}");
+					throw new NotFoundException($"Not Found: Decision with id {command.DecisionId} does not have an outcome, Case {command.ConcernsCaseUrn}");
 				}
 
-				decision.Outcome.Status = request.Request.Status;
-				decision.Outcome.TotalAmount = request.Request.TotalAmount;
-				decision.Outcome.DecisionMadeDate = request.Request.DecisionMadeDate;
-				decision.Outcome.DecisionEffectiveFromDate = request.Request.DecisionEffectiveFromDate;
-				decision.Outcome.Authorizer = request.Request.Authorizer;
-				decision.Outcome.BusinessAreasConsulted = request.Request.BusinessAreasConsulted.Select(b => new DecisionOutcomeBusinessAreaMapping()
+				decision.Outcome.Status = command.Request.Status;
+				decision.Outcome.TotalAmount = command.Request.TotalAmount;
+				decision.Outcome.DecisionMadeDate = command.Request.DecisionMadeDate;
+				decision.Outcome.DecisionEffectiveFromDate = command.Request.DecisionEffectiveFromDate;
+				decision.Outcome.Authorizer = command.Request.Authorizer;
+				decision.Outcome.BusinessAreasConsulted = command.Request.BusinessAreasConsulted.Select(b => new DecisionOutcomeBusinessAreaMapping()
 				{
 					DecisionOutcomeBusinessId = b
 				}).ToList();
@@ -83,13 +83,13 @@ namespace ConcernsCaseWork.API.Features.Decision.Outcome
 
 				await _context.SaveChangesAsync();
 
-				var concernCreatedNotification = new DecisionOutcomeUpdatedNotification() { Id = decision.Outcome.DecisionOutcomeId, ConcernsCaseUrn = request.ConcernsCaseUrn, };
+				var concernCreatedNotification = new DecisionOutcomeUpdatedNotification() { Id = decision.Outcome.DecisionOutcomeId, ConcernsCaseUrn = command.ConcernsCaseUrn, };
 				await _mediator.Publish(concernCreatedNotification);
 
 				return new UpdateDecisionOutcomeResponse()
 				{
-					ConcernsCaseUrn = request.ConcernsCaseUrn,
-					DecisionId = request.DecisionId,
+					ConcernsCaseUrn = command.ConcernsCaseUrn,
+					DecisionId = command.DecisionId,
 					DecisionOutcomeId = decision.Outcome.DecisionOutcomeId
 				};
 			}
