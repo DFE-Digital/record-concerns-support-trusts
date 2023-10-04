0b9362d9c3dc50dc40cd07fd92cb63adaff88453
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-10-04 16:10:50 +0100

    removed factories that were not used

--- ConcernsCaseWork/ConcernsCaseWork.API/Factories/Concerns/Decisions/ICreateDecisionResponseFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using ConcernsCaseWork.API.Contracts.ResponseModels.Concerns.Decisions;
-
-namespace ConcernsCaseWork.API.Factories.Concerns.Decisions
-{
-	public interface ICreateDecisionResponseFactory
-	{
-		public CreateDecisionResponse Create(int concernsCaseUrn, int decisionId);
-	}
-}
