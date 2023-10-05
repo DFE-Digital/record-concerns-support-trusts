8768d2b6e9b6344bc6179c22d78d2f079b6d1fa9
elielijah321 <marinel.aremusoftwaresolutions@hotmail.com>
2023-10-05 12:34:04 +0100

    fix failing test

--- ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/e2e/casework-regression/create-non-concerns-case.cy.ts
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/e2e/casework-regression/create-non-concerns-case.cy.ts
@@ -66,11 +66,11 @@ describe("Creating a non concerns case", () => {
             .withCaseDivision("SFSO")
             .continue();
 
-		Logger.Log("Create a valid Non-concern case type");
-		selectCaseTypePage.withCaseType("NonConcerns").continue();
-
 		Logger.Log("Populate territory");
 		addTerritoryPage.withTerritory(territory).nextStep();
+
+		Logger.Log("Create a valid Non-concern case type");
+		selectCaseTypePage.withCaseType("NonConcerns").continue();
 
 		Logger.Log("Checking accessibility on non concerns confirmation page");
 		cy.excuteAccessibilityTests();
@@ -144,7 +144,7 @@ describe("Creating a non concerns case", () => {
 		});
 	});
 
-	it("Converting non conern to concern case", () => {
+	it.only("Converting non conern to concern case", () => {
 		Logger.Log("Create a case");
 		createCasePage.withTrustName(trustName).selectOption().confirmOption();
 
@@ -153,11 +153,11 @@ describe("Creating a non concerns case", () => {
             .withCaseDivision("SFSO")
             .continue();
 
-		Logger.Log("Create a valid Non-concern case type");
-		selectCaseTypePage.withCaseType("NonConcerns").continue();
-
 		Logger.Log("Populate territory");
 		addTerritoryPage.withTerritory(territory).nextStep();
+
+		Logger.Log("Create a valid Non-concern case type");
+		selectCaseTypePage.withCaseType("NonConcerns").continue();
 
 		Logger.Log("Add non concerns case");
 		addConcernDetailsPage.createCase();
