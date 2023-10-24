91a56f662380d5cac4777db359fd0e3c82f98ec0
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-10-24 13:42:07 +0100

    use enum when seeding nti under consideration reason

--- ConcernsCaseWork/ConcernsCaseWork.Data/Configurations/NtiUnderConsideration/NTIUnderConsiderationReasonConfiguration.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Data/Configurations/NtiUnderConsideration/NTIUnderConsiderationReasonConfiguration.cs
@@ -17,14 +17,62 @@ public class NTIUnderConsiderationReasonConfiguration : IEntityTypeConfiguration
 		builder.HasData(
 			new NTIUnderConsiderationReason[]
 			{
-				new NTIUnderConsiderationReason{ Id = 1, Name = "Cash flow problems", CreatedAt = createdAt, UpdatedAt = createdAt },
-				new NTIUnderConsiderationReason{ Id = 2, Name = "Cumulative deficit (actual)", CreatedAt = createdAt, UpdatedAt = createdAt },
-				new NTIUnderConsiderationReason{ Id = 3, Name = "Cumulative deficit (projected)", CreatedAt = createdAt, UpdatedAt = createdAt },
-				new NTIUnderConsiderationReason{ Id = 4, Name = "Governance concerns", CreatedAt = createdAt, UpdatedAt = createdAt },
-				new NTIUnderConsiderationReason{ Id = 5, Name = "Non-Compliance with Academies Financial/Trust Handbook", CreatedAt = createdAt, UpdatedAt = createdAt },
-				new NTIUnderConsiderationReason{ Id = 6, Name = "Non-Compliance with financial returns", CreatedAt = createdAt, UpdatedAt = createdAt },
-				new NTIUnderConsiderationReason{ Id = 7, Name = "Risk of insolvency", CreatedAt = createdAt, UpdatedAt = createdAt },
-				new NTIUnderConsiderationReason{ Id = 8, Name = "Safeguarding", CreatedAt = createdAt, UpdatedAt = createdAt }
+				new NTIUnderConsiderationReason
+				{ 
+					Id = (int)API.Contracts.NtiUnderConsideration.NtiUnderConsiderationReason.CashFlowProblems, 
+					Name = "Cash flow problems", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt 
+				},
+				new NTIUnderConsiderationReason
+				{ 
+					Id = (int)API.Contracts.NtiUnderConsideration.NtiUnderConsiderationReason.CumulativeDeficitActual, 
+					Name = "Cumulative deficit (actual)", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt 
+				},
+				new NTIUnderConsiderationReason
+				{ 
+					Id = (int)API.Contracts.NtiUnderConsideration.NtiUnderConsiderationReason.CumulativeDeficitProjected, 
+					Name = "Cumulative deficit (projected)", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt 
+				},
+				new NTIUnderConsiderationReason
+				{ 
+					Id = (int)API.Contracts.NtiUnderConsideration.NtiUnderConsiderationReason.GovernanceConcerns, 
+					Name = "Governance concerns", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt 
+				},
+				new NTIUnderConsiderationReason
+				{ 
+					Id = (int)API.Contracts.NtiUnderConsideration.NtiUnderConsiderationReason.NonComplianceWithAcademiesFinancialTrustHandbook, 
+					Name = "Non-Compliance with Academies Financial/Trust Handbook", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt 
+				},
+				new NTIUnderConsiderationReason
+				{ 
+					Id = (int)API.Contracts.NtiUnderConsideration.NtiUnderConsiderationReason.NonComplianceWithFinancialReturns, 
+					Name = "Non-Compliance with financial returns", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt 
+				},
+				new NTIUnderConsiderationReason
+				{ 
+					Id = (int)API.Contracts.NtiUnderConsideration.NtiUnderConsiderationReason.RiskOfInsolvency,
+					Name = "Risk of insolvency", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt 
+				},
+				new NTIUnderConsiderationReason
+				{ 
+					Id = (int)API.Contracts.NtiUnderConsideration.NtiUnderConsiderationReason.Safeguarding, 
+					Name = "Safeguarding", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt 
+				}
 			});
 	}
 }
