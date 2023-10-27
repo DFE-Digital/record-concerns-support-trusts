f95fbd297e1096f72021d4a4b323b807ceb5fed9
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-10-27 10:25:43 +0100

    updated the database configuration to use the enum

--- ConcernsCaseWork/ConcernsCaseWork.Data/Configurations/NTIWarningLetters/NTIWarningLetterStatusConfiguration.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Data/Configurations/NTIWarningLetters/NTIWarningLetterStatusConfiguration.cs
@@ -17,11 +17,54 @@ public class NTIWarningLetterStatusConfiguration : IEntityTypeConfiguration<NTIW
 		builder.HasData(
 			new NTIWarningLetterStatus[]
 			{
-				new NTIWarningLetterStatus{ Id = 1, Name = "Preparing warning letter", CreatedAt = createdAt, UpdatedAt = createdAt, IsClosingState = false, PastTenseName="" },
-				new NTIWarningLetterStatus{ Id = 2, Name = "Sent to trust", CreatedAt = createdAt, UpdatedAt = createdAt, IsClosingState = false, PastTenseName="" },
-				new NTIWarningLetterStatus{ Id = 3, Name = "Cancel warning letter", Description="The warning letter is no longer needed.", CreatedAt = createdAt, UpdatedAt = createdAt, IsClosingState = true, PastTenseName="Cancelled" },
-				new NTIWarningLetterStatus{ Id = 4, Name = "Conditions met", Description="You are satisfied that all the conditions have been, or will be, met as outlined in the letter", CreatedAt = createdAt, UpdatedAt = createdAt, IsClosingState = true, PastTenseName="Conditions met" },
-				new NTIWarningLetterStatus{ Id = 5, Name = "Escalate to Notice To Improve", Description="Conditions have not been met. Close NTI: Warning letter and begin NTI on case page using \"Add to case\".", CreatedAt = createdAt, UpdatedAt = createdAt, IsClosingState = true, PastTenseName="Escalated to Notice To Improve" }
+				new NTIWarningLetterStatus
+				{ 
+					Id = (int)API.Contracts.NtiWarningLetter.NtiWarningLetterStatus.PreparingWarningLetter, 
+					Name = "Preparing warning letter", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt, 
+					IsClosingState = false, 
+					PastTenseName="" 
+				},
+				new NTIWarningLetterStatus
+				{ 
+					Id = (int)API.Contracts.NtiWarningLetter.NtiWarningLetterStatus.SentToTrust, 
+					Name = "Sent to trust", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt, 
+					IsClosingState = false, 
+					PastTenseName="" 
+				},
+				new NTIWarningLetterStatus
+				{ 
+					Id = (int)API.Contracts.NtiWarningLetter.NtiWarningLetterStatus.CancelWarningLetter, 
+					Name = "Cancel warning letter", 
+					Description="The warning letter is no longer needed.", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt, 
+					IsClosingState = true, 
+					PastTenseName="Cancelled" 
+				},
+				new NTIWarningLetterStatus
+				{ 
+					Id = (int)API.Contracts.NtiWarningLetter.NtiWarningLetterStatus.ConditionsMet, 
+					Name = "Conditions met", 
+					Description="You are satisfied that all the conditions have been, or will be, met as outlined in the letter", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt, 
+					IsClosingState = true, 
+					PastTenseName="Conditions met" 
+				},
+				new NTIWarningLetterStatus
+				{ 
+					Id = (int)API.Contracts.NtiWarningLetter.NtiWarningLetterStatus.EscalateToNoticeToImprove, 
+					Name = "Escalate to Notice To Improve", 
+					Description="Conditions have not been met. Close NTI: Warning letter and begin NTI on case page using \"Add to case\".", 
+					CreatedAt = createdAt, 
+					UpdatedAt = createdAt, 
+					IsClosingState = true, 
+					PastTenseName="Escalated to Notice To Improve" 
+				}
 			});
 	}
 }
