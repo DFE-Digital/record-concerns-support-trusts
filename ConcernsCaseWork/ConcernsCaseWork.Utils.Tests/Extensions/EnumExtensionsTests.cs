de85470895623936d51ef0abc3e28bb0c6a528ff
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-11-08 11:34:54 +0000

    fixed issue with autoformatting

--- ConcernsCaseWork/ConcernsCaseWork.Utils.Tests/Extensions/EnumExtensionsTests.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Utils.Tests/Extensions/EnumExtensionsTests.cs
@@ -15,23 +15,13 @@ namespace ConcernsCaseWork.Utils.Tests.Extensions
 		}
 
 		[Test]
-		public void Get_Description_When_ValidValue_WithoutDescription_Returns_Description()
+		public void GetDescription_When_ValidValue_WithoutDescription_Returns_Description()
 		{
 			var testEnum = TestEnum.Green;
 
 			var result = testEnum.Description();
 
 			result.Should().Be("Green");
-		}
-
-		[Test]
-		public void Get_Description_When_ValueOutsideEnum_Returns_Null()
-		{
-			var testEnum = (TestEnum)5;
-
-			var result = testEnum.Description();
-
-			result.Should().BeNull();
 		}
 	}
 
