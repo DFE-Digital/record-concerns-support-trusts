a850468edf5e5e68c4b31ed7cae258f5cdabbfad
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-11-08 11:06:51 +0000

    api project uses the new util project

--- ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/NtiUnderConsideration/EditNtiUnderConsiderationBaseModel.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/NtiUnderConsideration/EditNtiUnderConsiderationBaseModel.cs
@@ -1,9 +1,9 @@
 ﻿using ConcernsCaseWork.API.Contracts.NoticeToImprove;
 using ConcernsCaseWork.API.Contracts.NtiUnderConsideration;
-using ConcernsCaseWork.API.Extensions;
 using ConcernsCaseWork.Models;
 using ConcernsCaseWork.Models.CaseActions;
 using ConcernsCaseWork.Pages.Base;
+using ConcernsCaseWork.Utils.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +19,7 @@ namespace ConcernsCaseWork.Pages.Case.Management.Action.NtiUnderConsideration
 			return reasonValues.Select(r => new RadioItem
 			{
 				Id = Convert.ToString((int)r),
-				Text = r.GetDescription(),
+				Text = r.Description(),
 				IsChecked = ntiModel?.NtiReasonsForConsidering?.Any(ntiR => ntiR == r) == true,
 			});
 		}
