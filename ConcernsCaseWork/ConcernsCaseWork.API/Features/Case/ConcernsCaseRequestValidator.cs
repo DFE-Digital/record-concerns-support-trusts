e6b15e030064cd8e1efcec76a46181971f00966b
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-11-17 13:09:47 +0000

    147776: added validation to the create and update case apis
    ensure division, territory/region, trust uk prn and rating id are not null
    
    removed the case-without-division test as all cases must have a division, it cannot be null and none of our migrated data has a null division

--- ConcernsCaseWork/ConcernsCaseWork.API/Features/Case/ConcernsCaseRequestValidator.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API/Features/Case/ConcernsCaseRequestValidator.cs
@@ -8,11 +8,20 @@ namespace ConcernsCaseWork.API.Features.Case
 		public ConcernsCaseRequestValidator()
 		{
 			RuleFor(x => x.RatingId).GreaterThanOrEqualTo(1)
-				.WithMessage("Ratings Urn can not be 0");
+				.WithMessage("'RatingId' must be a value within range");
 
-			RuleFor(x => x.Division).IsInEnum().WithMessage("Division must have value 1 or 2");
+			RuleFor(x => x.Division).NotEmpty().IsInEnum().WithMessage("'Division' must be a value within range");
 
-			RuleFor(x => x.Region).IsInEnum();
+			RuleFor(x => x).Must(HasTerritoryOrRegion).WithMessage("'Territory' or 'Region' must be provided");
+
+			RuleFor(x => x.Region).IsInEnum().WithMessage("'Region' must be a value within range");
+			RuleFor(x => x.Territory).IsInEnum().WithMessage("'Territory' must be a value within range");
+			RuleFor(x => x.TrustUkprn).NotEmpty().WithMessage("'TrustUkprn' must not be empty");
+		}
+
+		public bool HasTerritoryOrRegion(ConcernCaseRequest request)
+		{
+			return request.Territory != null || request.Region != null;
 		}
 	}
 }
