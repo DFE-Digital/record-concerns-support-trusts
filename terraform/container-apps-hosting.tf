5cd2a15daa9e4cb0630e11bfe54919b4a1fe3125
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-04-16 15:40:55 +0100

    Set MX Records using Terraform

--- terraform/container-apps-hosting.tf
+++ terraform/container-apps-hosting.tf
@@ -57,6 +57,7 @@ module "azure_container_apps_hosting" {
   dns_zone_domain_name = local.dns_zone_domain_name
   dns_ns_records       = local.dns_ns_records
   dns_txt_records      = local.dns_txt_records
+  dns_mx_records       = local.dns_mx_records
 
   enable_event_hub                          = local.enable_event_hub
   enable_logstash_consumer                  = local.enable_logstash_consumer
