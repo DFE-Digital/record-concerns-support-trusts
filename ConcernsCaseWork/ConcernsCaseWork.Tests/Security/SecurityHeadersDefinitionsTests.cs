a9f5b9549bdd37e2d62f38c9e312c5f8b15eba1a
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-07-04 16:10:48 +0100

    Formatted file

--- ConcernsCaseWork/ConcernsCaseWork.Tests/Security/SecurityHeadersDefinitionsTests.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Tests/Security/SecurityHeadersDefinitionsTests.cs
@@ -21,39 +21,39 @@ namespace ConcernsCaseWork.Tests.Security
 			Assert.That(headerPolicyCollection.ContainsKey("X-Frame-Options"), Is.True);
 			headerPolicyCollection.TryGetValue("X-Frame-Options", out var xFrameOptions);
 			Assert.That(xFrameOptions, Is.AssignableFrom<XFrameOptionsHeader>());
-			
+
 			Assert.That(headerPolicyCollection.ContainsKey("X-XSS-Protection"), Is.True);
 			headerPolicyCollection.TryGetValue("X-XSS-Protection", out var xssProtection);
 			Assert.That(xssProtection, Is.AssignableFrom<XssProtectionHeader>());
-			
+
 			Assert.That(headerPolicyCollection.ContainsKey("X-Content-Type-Options"), Is.True);
 			headerPolicyCollection.TryGetValue("X-Content-Type-Options", out var xContentTypeOptions);
 			Assert.That(xContentTypeOptions, Is.AssignableFrom<XContentTypeOptionsHeader>());
-			
+
 			Assert.That(headerPolicyCollection.ContainsKey("Referrer-Policy"), Is.True);
 			headerPolicyCollection.TryGetValue("Referrer-Policy", out var referrerPolicy);
 			Assert.That(referrerPolicy, Is.AssignableFrom<ReferrerPolicyHeader>());
-			
+
 			Assert.That(headerPolicyCollection.ContainsKey("Server"), Is.True);
 			headerPolicyCollection.TryGetValue("Server", out var server);
 			Assert.That(server, Is.AssignableFrom<ServerHeader>());
-			
+
 			Assert.That(headerPolicyCollection.ContainsKey("Cross-Origin-Opener-Policy"), Is.True);
 			headerPolicyCollection.TryGetValue("Cross-Origin-Opener-Policy", out var crossOriginOpenerPolicy);
 			Assert.That(crossOriginOpenerPolicy, Is.InstanceOf<CrossOriginOpenerPolicyHeader>());
-			
+
 			Assert.That(headerPolicyCollection.ContainsKey("Cross-Origin-Embedder-Policy"), Is.True);
 			headerPolicyCollection.TryGetValue("Cross-Origin-Embedder-Policy", out var crossOriginEmbedderPolicy);
 			Assert.That(crossOriginEmbedderPolicy, Is.InstanceOf<CrossOriginEmbedderPolicyHeader>());
-			
+
 			Assert.That(headerPolicyCollection.ContainsKey("Cross-Origin-Resource-Policy"), Is.True);
 			headerPolicyCollection.TryGetValue("Cross-Origin-Resource-Policy", out var crossOriginResourcePolicy);
 			Assert.That(crossOriginResourcePolicy, Is.InstanceOf<CrossOriginResourcePolicyHeader>());
-			
+
 			Assert.That(headerPolicyCollection.ContainsKey("Content-Security-Policy"), Is.True);
 			headerPolicyCollection.TryGetValue("Content-Security-Policy", out var contentSecurityPolicy);
 			Assert.That(contentSecurityPolicy, Is.InstanceOf<ContentSecurityPolicyHeader>());
-			
+
 			Assert.That(headerPolicyCollection.ContainsKey("Permissions-Policy"), Is.True);
 			headerPolicyCollection.TryGetValue("Permissions-Policy", out var permissionsPolicy);
 			Assert.That(permissionsPolicy, Is.InstanceOf<PermissionsPolicyHeader>());
