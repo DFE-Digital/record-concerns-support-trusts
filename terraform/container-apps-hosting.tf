7ea1522f0c34b925d1d304cc3252a940206211b2
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-07-24 11:33:05 +0100

    Set custom waf rules against the Azure Front Door (#1320)
    * This enables us to set exclusions for certain types of requests

--- terraform/container-apps-hosting.tf
+++ terraform/container-apps-hosting.tf
@@ -47,6 +47,7 @@ module "azure_container_apps_hosting" {
   cdn_frontdoor_host_add_response_headers         = local.cdn_frontdoor_host_add_response_headers
   cdn_frontdoor_health_probe_path                 = local.cdn_frontdoor_health_probe_path
   cdn_frontdoor_enable_rate_limiting              = local.cdn_frontdoor_enable_rate_limiting
+  cdn_frontdoor_waf_custom_rules                  = local.cdn_frontdoor_waf_custom_rules
   cdn_frontdoor_rate_limiting_duration_in_minutes = local.cdn_frontdoor_rate_limiting_duration_in_minutes
   cdn_frontdoor_rate_limiting_threshold           = local.cdn_frontdoor_rate_limiting_threshold
   cdn_frontdoor_origin_fqdn_override              = local.cdn_frontdoor_origin_fqdn_override
