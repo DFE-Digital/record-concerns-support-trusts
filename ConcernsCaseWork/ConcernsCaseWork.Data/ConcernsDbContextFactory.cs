c6b57b4116f88e79677ed16a72912c1ec709bb4f
Farshad DASHTI <Farshad.DASHTI@EDUCATION.GOV.UK>
2024-08-02 11:41:24 +0100

    Added design time dbcontext factory

---
+++ ConcernsCaseWork/ConcernsCaseWork.Data/ConcernsDbContextFactory.cs
@@ -0,0 +1,35 @@
+﻿using ConcernsCaseWork.UserContext;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace ConcernsCaseWork.Data
+{
+	public class ConcernsDbContextFactory : IDesignTimeDbContextFactory<ConcernsDbContext>
+	{
+		public ConcernsDbContext CreateDbContext(string[] args)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<ConcernsDbContext>();
+
+			var configPath = Path.Combine(Directory.GetCurrentDirectory(), "../ConcernsCaseWork/appsettings.json");
+
+			var configuration = new ConfigurationBuilder()
+				.AddJsonFile(configPath)
+				.Build();
+
+			var userInfoService = new ServerUserInfoService()
+			{
+				UserInfo = new UserInfo() { Name = "Design.Time@test.gov.uk", Roles = new[] { Claims.CaseWorkerRoleClaim } }
+			};
+
+			var connectionString = configuration.GetConnectionString("DefaultConnection");
+			optionsBuilder.UseSqlServer(connectionString);
+
+			var interceptors = new List<IInterceptor>();
+
+			return new ConcernsDbContext(optionsBuilder.Options, userInfoService, interceptors);
+		}
+	}
+}
