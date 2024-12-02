dd76d2f4b7e8c879cdeaf8adeec6e79347d23d46
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-12-02 15:54:35 +0000

    Updated to v1.16.0 of Container App Module (#1420)
    * This version allows us to deploy alarms for monitoring resource deletion events

--- terraform/container-apps-hosting.tf
+++ terraform/container-apps-hosting.tf
@@ -1,5 +1,5 @@
 module "azure_container_apps_hosting" {
-  source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v1.15.0"
+  source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v1.16.0"
 
   environment    = local.environment
   project_name   = local.project_name
