cb8ba69afe4fe85a3e31e8faeac4fcccd1f3b366
SHAKIR, Muhammad <Muhammad.SHAKIR@EDUCATION.GOV.UK>
2025-01-02 14:42:07 +0000

    Display closed concerns with correct status

--- ConcernsCaseWork/ConcernsCaseWork.Redis/Models/CreateRecordModel.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Redis/Models/CreateRecordModel.cs
@@ -12,12 +12,18 @@ namespace ConcernsCaseWork.Redis.Models
 		public long TypeId { get; set; }
 
 		public long RatingId { get; set; }
-		
+
+		public long StatusId { get; set; }
+
 		public long MeansOfReferralId { get; set; }
 
 		public string GetConcernTypeName()
 		{
 			return ((ConcernType?)TypeId)?.Description();
+		}
+		public bool IsClosed()
+		{
+			return StatusId == (long)ConcernStatus.Close;
 		}
 	}
 }
