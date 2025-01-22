913bf9e5e7b27c5c1fac45be5127bb814ec10b61
SHAKIR, Muhammad <Muhammad.SHAKIR@EDUCATION.GOV.UK>
2025-01-22 16:34:13 +0000

    Replaced auth key to token authorization for Cypress tests

--- ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/api/apiBase.ts
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/api/apiBase.ts
@@ -1,11 +1,11 @@
-import { EnvApiKey, EnvUsername, CaseworkerClaim } from "cypress/constants/cypressConstants";
+import { EnvApiKey, EnvUsername, CaseworkerClaim, jwtToken } from "cypress/constants/cypressConstants";
 
 export class ApiBase
 {
     protected getHeaders(): object
     {
         const result = {
-            ApiKey: Cypress.env(EnvApiKey),
+            'Authorization': `Bearer ${Cypress.env(jwtToken)}`,
             "Content-type": "application/json",
             "x-user-context-role-0" : CaseworkerClaim,
             "x-user-context-name" : Cypress.env(EnvUsername)
