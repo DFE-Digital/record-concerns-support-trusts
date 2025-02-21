f114d9aaeac5b9335468742bdedb0c87c278b2c6
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-02-21 16:17:19 +0000

    Fixed incorrected cypress env value

--- ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/constants/cypressConstants.ts
+++ ConcernsCaseWork/ConcernsCaseWork.CypressTests/cypress/constants/cypressConstants.ts
@@ -1,7 +1,7 @@
 export const EnvUrl = "url"; 
 export const EnvApi = "api";
 export const EnvApiKey = "apiKey";
-export const EnvUsername = "cypressUser";
+export const EnvUsername = "username";
 export const EnvAuthKey = "authKey";
 export const CaseworkerClaim = "concerns-casework.caseworker";
 export const TeamLeaderClaim = "concerns-casework.teamleader";
