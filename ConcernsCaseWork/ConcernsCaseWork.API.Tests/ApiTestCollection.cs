6f9b9cb13c158b3cbce8e2835ae864fda2147edc
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-02-27 13:37:14 +0000

    Rebased and fixed failing tests.

---
+++ ConcernsCaseWork/ConcernsCaseWork.API.Tests/ApiTestCollection.cs
@@ -0,0 +1,13 @@
+﻿using ConcernsCaseWork.API.Tests.Fixtures;
+using Xunit;
+
+namespace ConcernsCaseWork.API.Tests
+{
+	[CollectionDefinition("ApiTestCollection", DisableParallelization = true)]
+	public class ApiTestCollection : ICollectionFixture<ApiTestFixture>
+	{
+		public const string ApiTestCollectionName = "ApiTestCollection";
+
+		// No code needed here. It's just the xUnit pattern for shared fixture.
+	}
+}
