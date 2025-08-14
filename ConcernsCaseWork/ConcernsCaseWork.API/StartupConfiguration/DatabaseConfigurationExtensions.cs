3e0422497a8d1b02637812d9e0a5a5a375d205bd
SHAKIR, Muhammad <Muhammad.SHAKIR@EDUCATION.GOV.UK>
2025-08-14 09:08:35 +0100

    simplify code

--- ConcernsCaseWork/ConcernsCaseWork.API/StartupConfiguration/DatabaseConfigurationExtensions.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API/StartupConfiguration/DatabaseConfigurationExtensions.cs
@@ -7,7 +7,7 @@ public static class DatabaseConfigurationExtensions
 	public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
 	{
 		var connectionString = configuration.GetConnectionString("DefaultConnection");
-		var isEnabled = configuration.GetValue<bool>("ConcernsCasework:EnableSQLRetryOnFailure", false);
+		var isEnabled = configuration.GetValue("ConcernsCasework:EnableSQLRetryOnFailure", false);
 		services.AddDbContext<ConcernsDbContext>(options =>
 			options.UseConcernsSqlServer(connectionString, isEnabled)
 		);
