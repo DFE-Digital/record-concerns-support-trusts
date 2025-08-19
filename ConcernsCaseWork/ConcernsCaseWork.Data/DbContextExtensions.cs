6361ba7d34ad9515ff4d2903affed6ab641a844f
mshakirdfe <muhammad.shakir@education.gov.uk>
2025-08-19 10:53:26 +0100

    Implemented DB retry logic (#1548)

--- ConcernsCaseWork/ConcernsCaseWork.Data/DbContextExtensions.cs
+++ ConcernsCaseWork/ConcernsCaseWork.Data/DbContextExtensions.cs
@@ -4,11 +4,21 @@ namespace ConcernsCaseWork.Data;
 
 public static class DbContextExtensions
 {
-	public static DbContextOptionsBuilder UseConcernsSqlServer(this DbContextOptionsBuilder optionsBuilder, string connectionString)
+	public static DbContextOptionsBuilder UseConcernsSqlServer(this DbContextOptionsBuilder optionsBuilder, string connectionString, bool enableRetryOnFailure = false)
 	{
 		optionsBuilder.UseSqlServer(
 			connectionString,
-			opt => opt.MigrationsHistoryTable("__EfMigrationsHistory", "concerns"));
+			opt =>
+			{
+				opt.MigrationsHistoryTable("__EfMigrationsHistory", "concerns");
+				if (enableRetryOnFailure)
+				{
+					opt.EnableRetryOnFailure(
+						maxRetryCount: 2,
+						maxRetryDelay: TimeSpan.FromSeconds(5),
+						errorNumbersToAdd: null);
+				}
+			});
 		return optionsBuilder;
 	}
 }
