13046df490a50f5d425a4dfd18e08af2faabc4d7
AZIA, Stephane <Stephane.AZIA@EDUCATION.GOV.UK>
2025-10-14 15:03:29 +0100

    Cookie banner

--- ConcernsCaseWork/ConcernsCaseWork/Constants/CookieConstants.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Constants/CookieConstants.cs
@@ -3,5 +3,6 @@
 	public static class CookieConstants
 	{
 		public const string CookieConsentName = ".ConcernsCasework.Consent";
+		public const string CookieConsentHide = ".ConcernsCasework.Consent.Hide";
 	}
 }
