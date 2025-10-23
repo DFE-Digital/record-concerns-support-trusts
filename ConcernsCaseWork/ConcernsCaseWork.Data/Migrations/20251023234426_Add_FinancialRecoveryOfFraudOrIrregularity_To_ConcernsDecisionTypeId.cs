d99202ebbdacc56518b2bcfa54123745c0ac5e02
AZIA, Stephane <Stephane.AZIA@EDUCATION.GOV.UK>
2025-10-24 00:53:14 +0100

    Add FinancialRecoveryOfFraudOrIrregularity to ConcernsDecisionTypeId

---
+++ ConcernsCaseWork/ConcernsCaseWork.Data/Migrations/20251023234426_Add_FinancialRecoveryOfFraudOrIrregularity_To_ConcernsDecisionTypeId.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConcernsCaseWork.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_FinancialRecoveryOfFraudOrIrregularity_To_ConcernsDecisionTypeId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "concerns",
+                table: "ConcernsDecisionTypeId",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 12, "FinancialRecoveryOfFraudOrIrregularity" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "concerns",
+                table: "ConcernsDecisionTypeId",
+                keyColumn: "Id",
+                keyValue: 12);
+        }
+    }
+}
