edaf63eb227f574d9775099ba2cf72e85e92ba35
DEIFALLA <Mohamed.DEIFALLA@EDUCATION.GOV.UK>
2025-11-21 18:44:19 +0000

    add new fields

--- ConcernsCaseWork/ConcernsCaseWork.API.Contracts/Decisions/DrawdownFacilityAgreed.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API.Contracts/Decisions/DrawdownFacilityAgreed.cs
@@ -11,6 +11,12 @@ namespace ConcernsCaseWork.API.Contracts.Decisions
 		[Description("Payment under existing arrangement")]
 		PaymentUnderExistingArrangement = 3,
 		[Description("Final drawdown from this package")]
-		FinalDrawdownFromThisPackage = 4
+		FinalDrawdownFromThisPackage = 4,
+		[Description("A new package with drawdown")]
+		ANewPackageWithDrawdown = 5,
+		[Description("A new package with immediate payment")]
+		ANewPackageWithImmediatePayment = 6,
+		[Description("A drawdown from an existing package")]
+		ADrawdownFromAnExistingPackage = 7
 	}
 }
