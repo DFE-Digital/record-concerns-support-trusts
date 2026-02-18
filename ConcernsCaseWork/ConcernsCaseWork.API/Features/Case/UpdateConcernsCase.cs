e9941c5b19987641c0b545bc688561ff36251caf
AZIA, Stephane <Stephane.AZIA@EDUCATION.GOV.UK>
2026-02-18 22:47:38 +0000

    Inclusion of RAG Status Rational history

--- ConcernsCaseWork/ConcernsCaseWork.API/Features/Case/UpdateConcernsCase.cs
+++ ConcernsCaseWork/ConcernsCaseWork.API/Features/Case/UpdateConcernsCase.cs
@@ -11,16 +11,28 @@ namespace ConcernsCaseWork.API.Features.Case
 	public class UpdateConcernsCase : IUpdateConcernsCase
 	{
 		private readonly IConcernsCaseGateway _concernsCaseGateway;
+		private readonly IConcernsCaseRiskToTrustRatingHistoryGateway _concernsCaseRiskToTrustRagRationalHistoryGateway;
 
 		public UpdateConcernsCase(
-			IConcernsCaseGateway concernsCaseGateway)
+			IConcernsCaseGateway concernsCaseGateway, IConcernsCaseRiskToTrustRatingHistoryGateway concernsCaseRiskToTrustRagRationalHistoryGateway)
 		{
 			_concernsCaseGateway = concernsCaseGateway;
+			_concernsCaseRiskToTrustRagRationalHistoryGateway = concernsCaseRiskToTrustRagRationalHistoryGateway;
 		}
 
 		public ConcernsCaseResponse Execute(int urn, ConcernCaseRequest request)
 		{
 			var currentConcernsCase = _concernsCaseGateway.GetConcernsCaseByUrn(urn);
+
+			if (currentConcernsCase.RatingId != request.RatingId)
+			{
+				_concernsCaseRiskToTrustRagRationalHistoryGateway.CreateHistory(
+					currentConcernsCase.Id,
+					currentConcernsCase.RatingId,
+					currentConcernsCase.RatingRationalCommentary
+				);
+			}
+
 			var concernsCaseToUpdate = ConcernsCaseFactory.Update(currentConcernsCase, request);
 			var updatedConcernsCase = _concernsCaseGateway.Update(concernsCaseToUpdate);
 			return ConcernsCaseResponseFactory.Create(updatedConcernsCase);
