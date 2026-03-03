588e877819c139e3828ad16699c469251e82d1e8
AZIA, Stephane <Stephane.AZIA@EDUCATION.GOV.UK>
2026-03-03 01:07:01 +0000

    Added test cases

---
+++ ConcernsCaseWork/ConcernsCaseWork.API.Tests/Features/Case/GetConcernSearchCriteriasTests.cs
@@ -0,0 +1,31 @@
+﻿using ConcernsCaseWork.API.Features.Case;
+using ConcernsCaseWork.Data.Gateways;
+using Moq;
+using Xunit;
+
+namespace ConcernsCaseWork.API.Tests.Features.Case
+{
+    public class GetConcernSearchCriteriasTests
+    {
+		[Fact]
+		public void Execute_ReturnsExpectedCaseSearchParametersResponse()
+		{
+			// Arrange
+			var mockGateway = new Mock<IConcernsCaseGateway>();
+			var expectedOwners = new[] { "owner1", "owner2" };
+			var expectedTeamLeaders = new[] { "leader1", "leader2" };
+
+			mockGateway.Setup(g => g.GetOwnersOfCases()).Returns(expectedOwners);
+			mockGateway.Setup(g => g.GetTeamLeadersOfCases()).Returns(expectedTeamLeaders);
+
+			var sut = new GetConcernSearchCriterias(mockGateway.Object);
+
+			// Act
+			var result = sut.Execute();
+
+			// Assert
+			Assert.Equal(expectedOwners, result.CaseOwners);
+			Assert.Equal(expectedTeamLeaders, result.TeamLeaders);
+		}
+	}
+}
---
+++ ConcernsCaseWork/ConcernsCaseWork.API.Tests/Features/Case/GetConcernsCaseSummariesByFilterTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ConcernsCaseWork.API.Features.Case;
+using ConcernsCaseWork.Data.Gateways;
+using ConcernsCaseWork.Data.Models;
+using Moq;
+using Xunit;
+
+namespace ConcernsCaseWork.API.Tests.Features.Case
+{
+    public class GetConcernsCaseSummariesByFilterTests
+	{
+		[Fact]
+		public async Task Execute_ReturnsMappedResponsesAndRecordCount()
+		{
+			// Arrange
+			var mockGateway = new Mock<ICaseSummaryGateway>();
+			var parameters = new GetCaseSummariesByFilterParameters();
+			var caseSummaryVm = new ActiveCaseSummaryVm {
+				Rating = new ConcernsRating { 
+					Name = "High" ,
+					CreatedAt = DateTime.UtcNow.AddDays(-1),
+					UpdatedAt = DateTime.UtcNow,
+					Id = 1
+				},
+				TeamLedBy = "Leader" 
+			};
+			var caseSummaries = new List<ActiveCaseSummaryVm> { caseSummaryVm };
+			var recordCount = 1;
+
+			mockGateway
+				.Setup(g => g.GetCaseSummariesByFilter(It.IsAny<GetCaseSummariesByFilterParameters>()))
+				.ReturnsAsync((caseSummaries, recordCount));
+
+			var sut = new GetConcernsCaseSummariesByFilter(mockGateway.Object);
+
+			// Act
+			var (result, count) = await sut.Execute(parameters);
+
+			// Assert
+			Assert.Single(result);
+			Assert.Equal(recordCount, count);
+			Assert.Equal("Leader", result[0].TeamLedBy);
+		}
+	}
+}
